3e6fb9e643aa674b5116833107f9f35fd89c9e1f
Adrian Price-Whelan <adrian.prw@gmail.com>
2012-11-26 13:36:45 -0500

    update leapfrog integration tests

--- streams/integrate/tests/test_leapfrog.py
+++ streams/integrate/tests/test_leapfrog.py
@@ -16,6 +16,30 @@ from .._leapfrog import leapfrog2
 from ..leapfrog import *
 from ... import potential as pot
 
+def plot_energies(potential, ts, xs, vs, axes1=None):
+    E_kin = 0.5*np.sum(vs**2, axis=2)
+    E_pot = potential.value_at(xs)
+
+    energies = potential.energy_at(xs, vs)
+
+    if axes1 == None:
+        fig1, axes1 = plt.subplots(3,1,sharex=True,sharey=True)
+
+    axes1[0].plot(ts, E_kin)
+    axes1[0].set_ylabel(r"$E_{kin}$")
+
+    axes1[1].plot(ts, E_pot.T)
+    axes1[1].set_ylabel(r"$E_{pot}$")
+
+    axes1[2].plot(ts, (E_kin + E_pot.T))
+    axes1[2].set_ylabel(r"$E_{tot}$")
+
+    grid = np.linspace(-2., 2., 100)
+    fig2, axes2 = potential.plot(grid,grid,grid)
+    axes2[0,0].plot(xs[:,0,0], xs[:,0,1])
+    axes2[1,0].plot(xs[:,0,0], xs[:,0,2])
+    axes2[1,1].plot(xs[:,0,1], xs[:,0,2])
+
 class TestIntegrate(object):
 
     def test1(self):
@@ -39,31 +63,53 @@ class TestIntegrate(object):
 
         return
 
-    def test_energy(self):
-        potential = pot.HernquistPotential(M=1E10, c=0.7)
+    def test_energy1(self):
+        potential = pot.HernquistPotential(M=1E10*u.solMass, c=0.7)
 
-        initial_position = np.array([1.0, 0.0, 0.]) # kpc
+        initial_position = np.array([10.0, 0.0, 0.]) # kpc
         initial_velocity = np.array([0.0, (100.*u.km/u.s).to(u.kpc/u.Myr).value, 0.]) # kpc/Myr
 
         ts, xs, vs = leapfrog(potential.acceleration_at, initial_position, initial_velocity, 0., 1000., 1.)
+        plot_energies(potential,ts, xs, vs)
+        plt.show()
 
-        E_kin = 0.5*np.sum(vs**2, axis=2)
-        E_pot = potential.value_at(xs)
+    def test_energy2(self):
+        potential = pot.MiyamotoNagaiPotential(M=1E11*u.solMass, a=6.5, b=0.26)
 
-        energies = potential.energy_at(xs, vs)
+        initial_position = np.array([8.0, 0.0, 0.]) # kpc
+        initial_velocity = np.array([0.0, (200.*u.km/u.s).to(u.kpc/u.Myr).value, 0.]) # kpc/Myr
 
-        fig, axes = plt.subplots(3,1,sharex=True,sharey=True)
-        axes[0].plot(ts, E_kin)
-        axes[0].set_ylabel(r"$E_{kin}$")
+        ts, xs, vs = leapfrog(potential.acceleration_at, initial_position, initial_velocity, 0., 1000., 1.)
+        plot_energies(potential,ts, xs, vs)
+        plt.show()
 
-        axes[1].plot(ts, E_pot.T)
-        axes[1].set_ylabel(r"$E_{pot}$")
+    def test_energy3(self):
+        potential = pot.LogarithmicPotential(v_circ=(181.*u.km/u.s).to(u.kpc/u.Myr).value, p=1., q=0.75, c=12.)
 
-        axes[2].plot(ts, (E_kin + E_pot.T), 'k.')
-        axes[2].set_ylabel(r"$E_{tot}$")
+        initial_position = np.array([14.0, 0.0, 0.]) # kpc
+        initial_velocity = np.array([0.0, (160.*u.km/u.s).to(u.kpc/u.Myr).value, 0.]) # kpc/Myr
 
-        grid = np.linspace(-2., 2., 100)
-        fig, axes = potential.plot(grid,grid,grid)
-        axes[0,0].plot(xs[:,0,0], xs[:,0,1])
-        axes[1,0].plot(xs[:,0,0], xs[:,0,2])
-        axes[1,1].plot(xs[:,0,1], xs[:,0,2])
+        ts, xs, vs = leapfrog(potential.acceleration_at, initial_position, initial_velocity, 0., 6000., 1.)
+        plot_energies(potential,ts, xs, vs)
+        plt.show()
+
+    def test_energy4(self):
+        potential1 = pot.HernquistPotential(M=1E10*u.solMass, c=0.7)
+        potential2 = pot.MiyamotoNagaiPotential(M=1E11*u.solMass, a=6.5, b=0.26)
+        potential3 = pot.LogarithmicPotential(v_circ=(181.*u.km/u.s).to(u.kpc/u.Myr).value, p=1., q=1., c=12.)
+        #potential = potential1 + potential2 + potential3
+
+        initial_position = np.array([14.0, 0.0, 0.]) # kpc
+        initial_velocity = np.array([0.0, (160.*u.km/u.s).to(u.kpc/u.Myr).value, 0.]) # kpc/Myr
+
+        fig, axes = plt.subplots(3, 1, sharex=True, sharey=True)
+        ts, xs, vs = leapfrog(potential1.acceleration_at, initial_position, initial_velocity, 0., 6000., 1.)
+        plot_energies(potential1, ts, xs, vs, axes)
+
+        ts, xs, vs = leapfrog(potential2.acceleration_at, initial_position, initial_velocity, 0., 6000., 1.)
+        plot_energies(potential2, ts, xs, vs, axes)
+
+        ts, xs, vs = leapfrog(potential3.acceleration_at, initial_position, initial_velocity, 0., 6000., 1.)
+        plot_energies(potential3, ts, xs, vs, axes)
+
+        plt.show()
