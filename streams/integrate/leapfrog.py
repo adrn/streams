b02e1a4031d897b112647f5b84369d08d1ec967a
Adrian Price-Whelan <adrian.prw@gmail.com>
2012-12-27 15:34:25 -0500

    improve docstring for leapfrog integrator

--- streams/integrate/leapfrog.py
+++ streams/integrate/leapfrog.py
@@ -18,10 +18,26 @@ from ..util import _validate_coord
 __all__ = ["PotentialIntegrator", "leapfrog"]
 
 def leapfrog(acceleration_function, initial_position, initial_velocity, t1, t2, dt=None):
-    ''' '''
+    """ Given an acceleration function and initial conditions, integrate from t1 to t2
+        with a timestep dt using Leapfrog integration.
+        See: http://ursa.as.arizona.edu/~rad/phys305/ODE_III/node11.html
+
+        Parameters
+        ----------
+        acceleration_function : function
+            A function that accepts a position or an array of positions and computes
+            the acceleration at that position.
+        initial_position : array, list
+            A list or array of initial positions.
+        initial_velocity : array, list
+            A list or array of initial velocities.
+    """
 
     initial_position = np.array(initial_position)
     initial_velocity = np.array(initial_velocity)
+
+    if initial_position.shape != initial_velocity.shape:
+        raise ValueError("initial_position shape must match initial_velocity shape! Right now, they mismatch: {0} != {1}".format(initial_position.shape, initial_velocity.shape))
 
     if initial_position.ndim == 1:
         # x_i just stands for positions, it's actually a vector
