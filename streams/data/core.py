d52bc943b644c6e8bf2c6b6ae9b128c29bef5f79
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-01-31 18:09:08 -0500

    add data class for SGR_CEN

--- streams/data/core.py
+++ streams/data/core.py
@@ -11,7 +11,7 @@ import os, sys
 
 # Third-party
 import numpy as np
-import pyfits as pf
+import astropy.io.fits as fits
 import astropy.io.ascii as ascii
 import astropy.coordinates as coord
 import astropy.units as u
@@ -19,11 +19,11 @@ import astropy.units as u
 # Project
 from ..util import project_root
 
-__all__ = ["LM10", "LINEAR", "QUEST"]
+__all__ = ["LM10", "LINEAR", "QUEST", "SgrCen"]
 
 def _make_npy_file(ascii_file, overwrite=False, ascii_kwargs=dict()):
-    """ Make a .npy version of the given ascii file data. 
-        
+    """ Make a .npy version of the given ascii file data.
+
         Parameters
         ----------
         ascii_file : str
@@ -34,21 +34,21 @@ def _make_npy_file(ascii_file, overwrite=False, ascii_kwargs=dict()):
         ascii_kwargs : dict
             A dictionary of keyword arguments to be passed to ascii.read().
     """
-    
+
     filename, ext = os.path.splitext(ascii_file)
     npy_filename = filename + ".npy"
-    
+
     if os.path.exists(npy_filename) and overwrite:
         os.remove(npy_filename)
     elif os.path.exists(npy_filename) and not overwrite:
         return npy_filename
-    
+
     data = ascii.read(ascii_file, **ascii_kwargs)
     np.save(npy_filename, np.array(data))
     return npy_filename
 
 class StreamData(object):
-    
+
     def _set_coordinates(self, ra_name, dec_name):
         """ Given the name of the right ascension column and the name of the
             declination column, set the 'ra' and 'dec' attributes as astropy
@@ -56,63 +56,94 @@ class StreamData(object):
         """
         icrs = [coord.ICRSCoordinates(ra,dec,unit=(u.degree,u.degree)) \
                 for ra,dec in zip(self.data[ra_name], self.data[dec_name])]
-        
+
         self.ra = [c.ra for c in icrs]
         self.dec = [c.dec for c in icrs]
 
 class LM10(StreamData):
-    
+
     data = None
-    
+
     def __init__(self, overwrite=False):
-        """ Read in simulation data from Law & Majewski 2010. 
-        
+        """ Read in simulation data from Law & Majewski 2010.
+
             Parameters
             ----------
             overwrite : bool (optional)
                 If True, will overwrite any existing npy files and regenerate
                 using the latest ascii data.
         """
-        
+
         if LM10.data == None:
             dat_filename = os.path.join(project_root, "data", "simulation", \
                                     "SgrTriax_DYN.dat")
-            
+
             npy_filename = _make_npy_file(dat_filename, overwrite=overwrite)
             LM10.data = np.load(npy_filename)
-        
+
         self._set_coordinates("ra", "dec")
 
 class LINEAR(StreamData):
-    
+
     data = None
-    
+
     def __init__(self, overwrite=False):
         """ Read in LINEAR RR Lyrae catalog from Branimir. """
-        
+
         if LINEAR.data == None:
             txt_filename = os.path.join(project_root, "data", "catalog", \
                                         "LINEAR_RRab.txt")
-                                        
+
             npy_filename = _make_npy_file(txt_filename, overwrite=overwrite)
             LINEAR.data = np.load(npy_filename)
-        
+
         self._set_coordinates("ra", "dec")
 
 class QUEST(StreamData):
-    
+
     data = None
-    
+
     def __init__(self, overwrite=False):
         """ Read in QUEST RR Lyrae from Vivas et al. 2004 """
-        
+
         if QUEST.data == None:
             fits_filename = os.path.join(project_root, "data", "catalog", \
                                         "quest_vivas2004_RRL.fits")
-            
-            hdulist = pf.open(fits_filename)
+
+            hdulist = fits.open(fits_filename)
             QUEST.data = hdulist[1].data
-        
+
         self._set_coordinates("RAJ2000", "DEJ2000")
 
-        
+class SgrCen(StreamData):
+
+    data = None
+
+    def __init__(self, overwrite=False):
+        """ Read in Sgr satellite center simulation data from Kathryn """
+
+        # Scale Sgr simulation data to physical units
+        ru = 0.63 # kpc
+        vu = (41.27781037*u.km/u.s).to(u.kpc/u.Myr).value # kpc/Myr
+        tu = 14.9238134129 # Myr
+
+        if SgrCen.data == None:
+            txt_filename = os.path.join(project_root, "data", "simulation", \
+                                        "SGR_CEN")
+
+            npy_filename = _make_npy_file(txt_filename, overwrite=overwrite, ascii_kwargs=dict(names=["t","dt","x","y","z","vx","vy","vz"]))
+            SgrCen.data = np.load(npy_filename)
+
+            SgrCen.data["t"] = SgrCen.data["t"] * tu
+            SgrCen.data["dt"] = SgrCen.data["dt"] * tu
+
+            SgrCen.data["x"] = SgrCen.data["x"] * ru
+            SgrCen.data["y"] = SgrCen.data["y"] * ru
+            SgrCen.data["z"] = SgrCen.data["z"] * ru
+
+            SgrCen.data["vx"] = SgrCen.data["vx"] * vu
+            SgrCen.data["vy"] = SgrCen.data["vy"] * vu
+            SgrCen.data["vz"] = SgrCen.data["vz"] * vu
+
+
+
