21556ece952d3506b9dec09d0df71957b046fa39
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-03-04 15:43:25 -0500

    back and forth...

--- streams/integrate/leapfrog.py
+++ streams/integrate/leapfrog.py
@@ -47,8 +47,8 @@ def leapfrog(acceleration_function, initial_position, initial_velocity, t1, t2, 
         x_i = np.array(initial_position)
         v_i = np.array(initial_velocity)
 
-    times = np.arange(t1, t2+dt, dt)
-    #times = np.arange(t1, t2, dt)
+    #times = np.arange(t1, t2+dt, dt)
+    times = np.arange(t1, t2, dt)
     
     Ntimesteps = len(times)
 
