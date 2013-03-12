212a65bec94a3c29bad316c4102c0cc989da6027
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-03-12 10:43:56 -0400

    vectorize. also ^2 is bitwise xor dammit

--- streams/simulation/backintegrate.py
+++ streams/simulation/backintegrate.py
@@ -90,99 +90,98 @@ def back_integrate_with_errors(potential, sgr_snap, sgr_cen, dt):
 
     # Initialize particle simulation with full potential
     simulation = TestParticleSimulation(potential=potential)
-
+       
+    # Transform to heliocentric coordinates
+    rsun = 8. # kpc
+    
+    # Distances in kpc, velocities in kpc/Myr
+    x, y, z = sgr_snap.data["x"] + rsun, sgr_snap.data["y"], sgr_snap.data["z"]
+    vx, vy, vz = sgr_snap.data["vx"], sgr_snap.data["vy"], sgr_snap.data["vz"]
+    
+    d = np.sqrt(x**2 + y**2 + z**2)
+    vr = (x*vx + y*vy + z*vz) / d 
+    
+    # proper motions in km/s/kpc
+    rad = np.sqrt(x**2 + y**2)
+    vrad = (x*vx + y*vy) / rad
+    mu_l = (x*vy - y*vx) / rad / d
+    mu_b = (-z*vrad + rad*vz) / d**2
+    
+    # angular position
+    sinb = z/d
+    cosb = rad/d
+    cosl = x/rad
+    sinl = y/rad
+    
+    # DISTANCE ERROR -- assuming 2% distances from RR Lyrae mid-IR
+    d += np.random.normal(0., 0.02*d)
+    
+    # VELOCITY ERROR -- 5 km/s (TODO: ???)
+    vr += np.random.normal(0., (5.*u.km/u.s).to(u.kpc/u.Myr).value)
+    
+    # PROPER MOTION ERROR -- from GAIA
+    # http://www.rssd.esa.int/index.php?project=GAIA&page=Science_Performance#chapter1
+    #        25 muas at V=15 (RR Lyraes are ~A-F type :between B and G star)
+    #        300 muas at V=20
+    # with THANKS to Horace Smith
+    # V abs mag RR Lyrae's
+    #  M_v = (0.214 +/- 0.047)([Fe/H] + 1.5) + 0.45+/-0.05
+    # Benedict et al. (2011 AJ, 142, 187)
+    # assuming [Fe/H]=-0.5
+    Mabs = 0.65
+    
+    # Johnson/Cousins (V-IC)  
+    # (V-IC) color=vmic
+    # 0.1-0.58
+    # Guldenschuh et al. (2005 PASP 117, 721)
+    vmic = 0.3
+    V = Mabs + 5.*np.log10(d*100.)
+    
+    # GAIA G mag
+    g = V - 0.0257 - 0.0924*vmic- 0.1623*vmic**2 + 0.0090*vmic**3
+    zz = 10**(0.4*(g-15.)) # ???
+    p = g < 12.
+    
+    if sum(p) > 0:
+        zz[p] = 10**(0.4*(12. - 15.))
+    
+    # "end of mission parallax standard"
+    # σπ [μas] = sqrt(9.3 + 658.1 · z + 4.568 · z^2) · [0.986 + (1 - 0.986) · (V-IC)]
+    dp = np.sqrt(9.3 + 658.1*zz + 4.568*zz**2)*(0.986 + (1 - 0.986)*vmic)
+    
+    # assume 5 year baseline, mas/year
+    dmu = dp/5.
+    # too optimistic: following suggests factor 2 more realistic
+    #http://www.astro.utu.fi/~cflynn/galdyn/lecture10.html 
+    # - and Sanjib suggests factor 0.526
+    dmu = 0.526*dp
+    
+    # translate to radians/year
+    conv1 = np.pi/180./60./60./1.e6
+    # translate to km/s from  kpc/year 
+    kmpkpc =3.085678e16
+    secperyr = 3.1536e7 
+    conv2 = kmpkpc/secperyr
+    dmu = dmu*conv1*conv2
+    
+    mu_l += np.random.normal(0., dmu)
+    mu_b += np.random.normal(0., dmu)
+    
+    # CONVERT BACK
+    x = d*cosb*cosl - rsun
+    y = d*cosb*sinl
+    z = d*sinb
+    
+    vx = vr*cosb*cosl - d*mul*sinl - d*mub*sinb*cosl
+    vy = vr*cosb*sinl + d*mul*cosl - d*mub*sinb*sinl
+    vz = vr*sinb + d*mub*cosb
+    
     for ii in range(sgr_snap.num):
-        
-        # Transform to heliocentric coordinates
-        rsun = 8. # kpc
-        
-        # Distances in kpc, velocities in kpc/Myr
-        x, y, z = sgr_snap.data["x"][ii] + rsun, sgr_snap.data["y"][ii], sgr_snap.data["z"][ii]
-        vx, vy, vz = sgr_snap.data["vx"][ii], sgr_snap.data["vy"][ii], sgr_snap.data["vz"][ii]
-        
-        d = np.sqrt(x**2 + y**2 + z**2)
-        vr = (x*vx + y*vy + z*vz) / d 
-        
-        # proper motions in km/s/kpc
-        rad = np.sqrt(x**2 + y**2)
-        vrad = (x*vx + y*vy) / rad
-        mu_l = (x*vy - y*vx) / rad / d
-        mu_b = (-z*vrad + rad*vz) / d**2
-        
-        # angular position
-        sin_b = z/d
-        cos_b = rad/d
-        cos_l = x/rad
-        sin_l = y/rad
-        
-        # DISTANCE ERROR -- assuming 2% distances from RR Lyrae mid-IR
-        d += np.random.normal(0., 0.02*d)
-        
-        # VELOCITY ERROR -- 5 km/s (TODO: ???)
-        vr += np.random.normal(0., (5.*u.km/u.s).to(u.kpc/u.Myr).value)
-        
-        # PROPER MOTION ERROR -- from GAIA
-        # http://www.rssd.esa.int/index.php?project=GAIA&page=Science_Performance#chapter1
-        #        25 muas at V=15 (RR Lyraes are ~A-F type :between B and G star)
-        #        300 muas at V=20
-        # with THANKS to Horace Smith
-        # V abs mag RR Lyrae's
-        #  M_v = (0.214 +/- 0.047)([Fe/H] + 1.5) + 0.45+/-0.05
-        # Benedict et al. (2011 AJ, 142, 187)
-        # assuming [Fe/H]=-0.5
-        Mabs = 0.65
-        
-        # Johnson/Cousins (V-IC)  
-        # (V-IC) color=vmic
-        # 0.1-0.58
-        # Guldenschuh et al. (2005 PASP 117, 721)
-        vmic = 0.3
-        V = Mabs + 5.*np.log10(d*100.)
-        
-        # GAIA G mag
-        g = V - 0.0257 - 0.0924*vmic- 0.1623*vmic**2 + 0.0090*vmic**3
-        zz = 10**(0.4*(g-15.)) # ???
-        p = g < 12.
-        
-        if sum(p) > 0:
-            zz[p] = 10**(0.4*(12. - 15.))
-        
-        # "end of mission parallax standard"
-        # σπ [μas] = sqrt(9.3 + 658.1 · z + 4.568 · z^2) · [0.986 + (1 - 0.986) · (V-IC)]
-        dp= np.sqrt(9.3 + 658.1*zz + 4.568*zz^2)*(0.986 + (1 - 0.986)*vmic)
-        
-        # assume 5 year baseline, mas/year
-        dmu = dp/5.
-        # too optimistic: following suggests factor 2 more realistic
-        #http://www.astro.utu.fi/~cflynn/galdyn/lecture10.html 
-        # - and Sanjib suggests factor 0.526
-        dmu = 0.526*dp
-        
-        # translate to radians/year
-        conv1 = np.pi/180./60./60./1.e6
-        # translate to km/s from  kpc/year 
-        kmpkpc =3.085678e16
-        secperyr = 3.1536e7 
-        conv2 = kmpkpc/secperyr
-        dmu = dmu*conv1*conv2
-        
-        mu_l += np.random.normal(0., dmu)
-        mu_b += np.random.normal(0., dmu)
-        
-        # CONVERT BACK
-        x = d*cosb*cosl - rsun
-        y = d*cosb*sinl
-        z = d*sinb
-        
-        vx = vr*cosb*cosl - d*mul*sinl - d*mub*sinb*cosl
-        vy = vr*cosb*sinl + d*mul*cosl - d*mub*sinb*sinl
-        vz = vr*sinb + d*mub*cosb
-        
-        p = Particle(position=(x, y, z), # kpc
-                     velocity=(vx, vy ,vz), # kpc/Myr
+        p = Particle(position=(x[ii], y[ii], z[ii]), # kpc
+                     velocity=(vx[ii], vy[ii] ,vz[ii]), # kpc/Myr
                      mass=1.) # M_sol
         simulation.add_particle(p)
 
     ts, xs, vs = simulation.run(t1=max(sgr_cen.data["t"]), t2=min(sgr_cen.data["t"]), dt=-dt)
-
+    
     return _variance_statistic(potential, xs, vs, sgr_cen)
