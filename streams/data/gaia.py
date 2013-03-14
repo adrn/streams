ab641162b730a2733409a3f22dcb4cedc4fb15b9
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-03-14 11:45:07 -0400

    add helpers for stars

--- streams/data/gaia.py
+++ streams/data/gaia.py
@@ -14,45 +14,52 @@ import numpy as np
 import astropy.units as u
 from astropy.utils.misc import isiterable
 
-def parallax_error(d):
-    """ http://www.rssd.esa.int/index.php?project=GAIA&page=Science_Performance#chapter1
-            25 muas at V=15 (RR Lyraes are ~A-F type :between B and G star)
-            300 muas at V=20
-        with thanks to Horace Smith
-    """
-    if not isinstance(d, u.Quantity):
-        raise TypeError("Distance array must be an Astropy Quantity object!")
+__all__ = ["parallax_error", "proper_motion_error", "rr_lyrae_M_V", \
+           "apparent_magnitude", "add_observational_uncertainties"]
+
+def rr_lyrae_M_V(fe_h):
+    """ Given an RR Lyra metallicity, return the V-band absolute magnitude. """
     
     # V abs mag RR Lyrae's
     #  M_v = (0.214 +/- 0.047)([Fe/H] + 1.5) + 0.45+/-0.05
     # Benedict et al. (2011 AJ, 142, 187)
-    # assuming [Fe/H] = -0.5 for Sgr
-    fe_h = -0.5
     Mabs = 0.214*(fe_h + 1.5) + 0.45
+    return Mabs
+
+def apparent_magnitude(M_V, d):
+    """ Compute the apparent magnitude of a source given an absolute magnitude
+        and a distance (Quantity).
+    """
+    if not isinstance(d, u.Quantity):
+        raise TypeError("Distance must be an Astropy Quantity object!")
+        
+    V = M_V + 5.*np.log10(d.to(u.pc).value)
     
-    # Johnson/Cousins (V-IC)  
-    # (V-IC) color=vmic
-    # 0.1-0.58
-    # Guldenschuh et al. (2005 PASP 117, 721)
-    vmic = 0.3
-    V = Mabs + 5.*np.log10(d.to(u.pc).value)
+    return V
+
+def parallax_error(V, V_minus_I):
+    """ http://www.rssd.esa.int/index.php?project=GAIA&page=Science_Performance#chapter1 """
     
     # GAIA G mag
-    g = V - 0.0257 - 0.0924*vmic- 0.1623*vmic**2 + 0.0090*vmic**3
+    g = V - 0.0257 - 0.0924*V_minus_I- 0.1623*V_minus_I**2 + 0.0090*V_minus_I**3
     zz = 10**(0.4*(g-15.)) 
     p = g < 12.
     
-    if sum(p) > 0:
-        zz[p] = 10**(0.4*(12. - 15.))
+    if isiterable(V):
+        if sum(p) > 0:
+            zz[p] = 10**(0.4*(12. - 15.))
+    else:
+        if p:
+            zz[p] = 10**(0.4*(12. - 15.))
     
     # "end of mission parallax standard"
     # σπ [μas] = sqrt(9.3 + 658.1 · z + 4.568 · z^2) · [0.986 + (1 - 0.986) · (V-IC)]
-    dp = np.sqrt(9.3 + 658.1*zz + 4.568*zz**2)*(0.986 + (1 - 0.986)*vmic)*1E-6*u.arcsecond
+    dp = np.sqrt(9.3 + 658.1*zz + 4.568*zz**2)*(0.986 + (1 - 0.986)*V_minus_I)*1E-6*u.arcsecond
     
     return dp
 
-def proper_motion_error(d):
-    dp = parallax_error(d)
+def proper_motion_error(V, V_minus_I):
+    dp = parallax_error(V, V_minus_I)
     
     # assume 5 year baseline, µas/year
     dmu = dp/(5.*u.year)
@@ -63,3 +70,77 @@ def proper_motion_error(d):
     dmu = 0.526*dmu
 
     return dmu.to(u.radian/u.yr)
+
+def add_observational_uncertainties(x,y,z,vx,vy,vz):
+    """ Given 3D galactocentric position and velocity, transform to heliocentric
+        coordinates, apply observational uncertainty estimates, then transform
+        back to galactocentric frame.
+    """
+    
+    if not isinstance(x,u.Quantity) or not isinstance(y,u.Quantity) or not isinstance(z,u.Quantity):
+        raise TypeError("Positions must be Astropy Quantity objects!")
+    
+    if not isinstance(vx,u.Quantity) or not isinstance(vy,u.Quantity) or not isinstance(vz,u.Quantity):
+        raise TypeError("Velocities must be Astropy Quantity objects!")
+    
+    # Johnson/Cousins (V-I_C)
+    # (V-I_C) color
+    # 0.1-0.58
+    # Guldenschuh et al. (2005 PASP 117, 721)
+    rr_lyrae_V_minus_I = 0.3
+    
+    # assuming [Fe/H] = -0.5 for Sgr
+    M_V = rr_lyrae_M_V(-0.5)
+    
+    # Transform to heliocentric coordinates
+    rsun = 8.*u.kpc
+    
+    x += rsun
+    
+    d = np.sqrt(x**2 + y**2 + z**2)*x.unit
+    V = apparent_magnitude(M_V, d)
+    
+    vr = (x*vx + y*vy + z*vz) / d 
+    
+    # proper motions in km/s/kpc
+    rad = np.sqrt(x**2 + y**2)*x.unit
+    vrad = (x*vx + y*vy) / rad
+    mul = (x*vy - y*vx) / rad / d
+    mub = (-z*vrad + rad*vz) / d**2
+       
+    # angular position
+    sinb = z/d
+    cosb = rad/d
+    cosl = x/rad
+    sinl = y/rad
+    
+    # DISTANCE ERROR -- assuming 2% distances from RR Lyrae mid-IR
+    d += np.random.normal(0., 0.02*d.value)*d.unit
+    
+    # VELOCITY ERROR -- 5 km/s (TODO: ???)
+    vr += np.random.normal(0., (5.*u.km/u.s))*u.km/u.s
+
+    dmu = proper_motion_error(V, rr_lyrae_V_minus_I)
+    
+    # translate to radians/year
+    #conv1 = np.pi/180./60./60./1.e6
+    # translate to km/s from  kpc/year 
+    #kmpkpc = 3.085678e16
+    #secperyr = 3.1536e7 
+    #conv2 = kmpkpc/secperyr
+    #dmu = dmu*conv1*conv2
+    
+    dmu = (dmu.to(u.rad/u.s).value / u.s).to(u.km / (u.kpc*u.s))
+    mul += np.random.normal(0., dmu.value)*dmu.unit
+    mub += np.random.normal(0., dmu.value)*dmu.unit
+    
+    # CONVERT BACK
+    x = d*cosb*cosl - rsun
+    y = d*cosb*sinl
+    z = d*sinb
+    
+    vx = vr*cosb*cosl - d*mul*sinl - d*mub*sinb*cosl
+    vy = vr*cosb*sinl + d*mul*cosl - d*mub*sinb*sinl
+    vz = vr*sinb + d*mub*cosb
+    
+    return (x,y,z,vx,vy,vz)
