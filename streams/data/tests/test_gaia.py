29dcc2f3be0e2847fe11db2ca9929daf228c7453
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-03-14 11:49:16 -0400

    add some unit tests

---
+++ streams/data/tests/test_gaia.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+"""
+    Test the ...
+"""
+
+from __future__ import absolute_import, unicode_literals, division, print_function
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Standard library
+import os, sys
+
+# Third-party
+import astropy.units as u
+import numpy as np
+import pytest
+
+from ..gaia import *
+
+def test_against_table():
+    """ Test astrometric precision against the table at:
+        http://www.rssd.esa.int/index.php?project=GAIA&page=Science_Performance
+    """
+    
+    # B1V star
+    np.testing.assert_almost_equal(parallax_error(15, -0.22).value, 26E-6, 5)
+    np.testing.assert_almost_equal(parallax_error(20, -0.22).value, 330E-6, 4)
+    
+    # G2V star
+    np.testing.assert_almost_equal(parallax_error(15, 0.75).value, 24E-6, 6)
+    np.testing.assert_almost_equal(parallax_error(20, 0.75).value, 290E-6, 4)
+    
+    # M6V star
+    np.testing.assert_almost_equal(parallax_error(15, 3.85).value, 9E-6, 4)
+    np.testing.assert_almost_equal(parallax_error(20, 3.85).value, 100E-6, 4)
+
+def test_add_uncertainties():
+    
+    N = 20
+    x = np.random.uniform(0., 10., N) * u.kpc
+    y = np.random.uniform(0., 10., N) * u.kpc
+    z = np.random.uniform(0., 10., N) * u.kpc
+    
+    vx = np.random.uniform(0., 25., N) * u.km/u.s
+    vy = np.random.uniform(0., 25., N) * u.km/u.s
+    vz = np.random.uniform(0., 25., N) * u.km/u.s
+    
+    print(add_observational_uncertainties(x,y,z,vx,vy,vz))
