e527260a9457f3770b4336f4a54d6eb135fa1466
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-03-31 22:51:40 -0400

    fix vary_one parameter

--- scripts/grid_search.py
+++ scripts/grid_search.py
@@ -20,15 +20,9 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 from streams.potential import *
-from streams.simulation import run_back_integration
+from streams.simulation import back_integrate
 from streams.data import SgrSnapshot, SgrCen
-
-true_halo_params = dict(v_halo=(121.858*u.km/u.s).to(u.kpc/u.Myr).value,
-                        q1=1.38,
-                        q2=1.0,
-                        qz=1.36,
-                        phi=1.692969,
-                        c=12.)
+from streams.potential.lm10 import halo_params as true_halo_params
 
 param_ranges = dict(qz=(1.,2.),
                     q1=(1.,2.),
@@ -36,35 +30,32 @@ param_ranges = dict(qz=(1.,2.),
                     v_halo=((100.*u.km/u.s).to(u.kpc/u.Myr).value,
                             (150.*u.km/u.s).to(u.kpc/u.Myr).value),
                     phi=(np.pi/4, 3*np.pi/4),
-                    c=(8,20))
+                    r_halo=(8,20))
 
-def vary_one_parameter():
+def vary_one_parameter(sgr_cen, sgr_snap, dt):
 
     stats_for_param = dict()
-    for param in ["qz", "q1", "q2", "v_halo", "phi", "c"]:
-        stats = dict(mean=[], std=[], median=[])
+    for param in ["qz", "q1", "q2", "v_halo", "phi", "r_halo"]:
+        print(param)
+        gen_variances = []
         vals = np.linspace(param_ranges[param][0], param_ranges[param][1], 20)
         for val in vals:
             halo_params = true_halo_params.copy()
             halo_params[param] = val
-            halo_potential = LogarithmicPotentialLJ(**halo_params)
+            potential = LawMajewski2010(**halo_params)
+            
+            var = back_integrate(potential, sgr_snap, sgr_cen, dt=dt)
+            gen_variances.append(var)
         
-            dist = run_back_integration(halo_potential, sgr_snap, sgr_cen)
+        stats_for_param[param] = gen_variances
         
-            stats["mean"].append(np.mean(dist))
-            stats["std"].append(np.std(dist))
-            stats["median"].append(np.median(dist))
-        
-        stats_for_param[param] = stats
-    
-    for param in ["qz", "q1", "q2", "v_halo", "phi", "c"]:
-        vals = np.linspace(param_ranges[param][0], param_ranges[param][1], 20)
-        
-        plt.figure(figsize=(12,8))
-        plt.plot(vals, np.array(stats_for_param[param]["median"]), color="k", marker="o", linestyle="none")
+        plt.figure(figsize=(12,8))    
+        plt.plot(vals, np.array(stats_for_param[param]), color="k", marker="o", linestyle="none")
         plt.axvline(true_halo_params[param])
         plt.xlabel(param)
         plt.ylabel("Median of minimum energy distance distribution")
+        plt.savefig("plots/grid_search/{0}.png".format(param))     
+        
         
 def vary_two_parameters(param_pair, sgr_snap, sgr_cen, dt, grid_size=10):
     param1,param2 = param_pair
@@ -81,24 +72,24 @@ def vary_two_parameters(param_pair, sgr_snap, sgr_cen, dt, grid_size=10):
             halo_params = true_halo_params.copy()
             halo_params[param1] = p1_val
             halo_params[param2] = p2_val
-            halo_potential = LogarithmicPotentialLJ(**halo_params)
+            potential = LawMajewski2010(**halo_params)
         
-            dist = run_back_integration(halo_potential, sgr_snap, sgr_cen, dt)
-            Z[ii,jj] = dist
+            gen_variance = back_integrate(potential, sgr_snap, sgr_cen, dt)
+            Z[ii,jj] = gen_variance
     
     return X,Y,Z
 
-def main():
-    sgr_snap = SgrSnapshot(num=100, no_bound=True)
+def main_two():
+    sgr_snap = SgrSnapshot(num=100)
     sgr_cen = SgrCen()
     
     # Get timestep information from SGR_CEN
-    t1 = min(sgr_cen.data["t"])
-    t2 = max(sgr_cen.data["t"])
-    dt = sgr_cen.data["dt"][0]*10
-        
+    t1 = min(sgr_cen.t)
+    t2 = max(sgr_cen.t)
+    dt = sgr_cen.dt[0]*10
+    
     # Interpolate SgrCen data onto new times
-    ts = np.arange(t2, t1, -dt)
+    ts = np.arange(t2, t1, -dt)*u.Myr
     sgr_cen.interpolate(ts)
 
     # q2 qz phi v_halo c
@@ -120,6 +111,23 @@ def main():
         plt.xlabel(params[0])
         plt.ylabel(params[1])
         plt.savefig("plots/grid_search/{0}_{1}.png".format(*params))
+    
+def main_one():
+    sgr_snap = SgrSnapshot(num=100)
+    sgr_cen = SgrCen()
+    
+    # Get timestep information from SGR_CEN
+    t1 = min(sgr_cen.t)
+    t2 = max(sgr_cen.t)
+    dt = sgr_cen.dt[0]*10
+    
+    # Interpolate SgrCen data onto new times
+    ts = np.arange(t2, t1, -dt)*u.Myr
+    sgr_cen.interpolate(ts)
+    
+    vary_one_parameter(sgr_cen, sgr_snap, dt)
 
 if __name__ == "__main__":
-    main()
+    #main_two()
+    main_one()
+    
