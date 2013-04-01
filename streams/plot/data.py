f7056cfe76604df87248329c921902e4051aef96
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-04-01 15:32:00 -0400

    ignore data path and add skeleton data plotting module

---
+++ streams/plot/data.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+""" Plotting utilities for data classes. """
+
+from __future__ import division, print_function
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Standard library
+import os, sys
+
+# Third-party
+import numpy as np
+
