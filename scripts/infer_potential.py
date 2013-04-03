3a42fd99ccc84652ec3ee21de83b6bbd03e35201
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-04-03 15:04:34 -0400

    move plotting to separate file

--- scripts/infer_potential.py
+++ scripts/infer_potential.py
@@ -30,40 +30,23 @@ import emcee
 from emcee.utils import MPIPool
 
 # Project
-from streams.potential.lm10 import param_ranges
+from streams.potential.lm10 import param_ranges, param_to_latex
 from streams.simulation import make_posterior
 from streams.simulation.setup import simulation_setup
-
-def plot_mcmc(param_names, acceptance_fraction, flatchain, chain):
-    """ Make MCMC plots: posterior and trace of chains. """
-    
-    ndim = chain.shape[2]
-    posterior_fig,posterior_axes = plt.subplots(ndim, 1, figsize=(14,5*(ndim+1)))
-    trace_fig,trace_axes = plt.subplots(ndim, 1, figsize=(14,5*(ndim+1)))
-
-    posterior_fig.suptitle("Median acceptance fraction: {0:.3f}".format(np.median(acceptance_fraction)))
-    trace_fig.suptitle("Median acceptance fraction: {0:.3f}".format(np.median(acceptance_fraction)))
-    for ii in range(ndim):
-        posterior_axes[ii].set_title(param_names[ii])
-        posterior_axes[ii].hist(flatchain[:,ii], bins=25, color="k", histtype="step", alpha=0.75)
-        posterior_axes[ii].axvline(true_halo_params[param_names[ii]], color="r", linestyle="--", linewidth=2)
-
-        trace_axes[ii].set_title(param_names[ii])
-        for k in range(chain.shape[0]):
-            trace_axes[ii].plot(np.arange(len(chain[k,:,ii])),
-                                chain[k,:,ii], color="k", drawstyle="steps", alpha=0.2)
-    
-    return (posterior_fig, trace_fig)
 
 def infer_potential(ln_posterior, nwalkers=100, nsamples=100, nburn_in=100, 
                     params=[], output_path="/tmp", with_errors=True, mpi=False, 
                     nthreads=multiprocessing.cpu_count(), logger=None, 
-                    description="", expr="", nparticles=100):
+                    description="", expr="", nparticles=100, sgr_snap=None,
+                    sgr_cen=None, **kwargs):
     
     """ Given a log posterior function, infer the parameters for the halo model. """
     
     if len(params) == 0:
         raise ValueError("No parameters specified!")
+    
+    if sgr_cen == None or sgr_snap == None:
+        raise ValueError("You must supply sgr_snap and sgr_cen keywords!")
     
     tf_yn = dict(True="yes", False="no")
     # Create list of strings to write to run_parameters file
@@ -106,69 +89,54 @@ def infer_potential(ln_posterior, nwalkers=100, nsamples=100, nburn_in=100,
     if not os.path.exists(path):
         os.mkdir(path)
     
-    fig = plot_sgr_snap(sgr_snap)
+    # Plot the initial positions of the particles in galactic XYZ coordinates
+    fig,axes = sgr_snap.plot_positions()
     fig.savefig(os.path.join(path, "particles.png"))
     
+    # If a burn-in period is requested, run the sampler for nburn_in steps then
+    #   reset the walkers and use the end positions as new initial conditions
     if nburn_in > 0:
         pos, prob, state = sampler.run_mcmc(p0, nburn_in)
         sampler.reset()
     else:
         pos = p0
-
+    
     sampler.run_mcmc(pos, nsamples)
-    logger.info("Median acceptance fraction: {0:.3f}".format(np.median(sampler.acceptance_fraction)))
-    run_parameters.append("median acceptance fraction: {0:.3f}".format(np.median(sampler.acceptance_fraction)))
+    run_parameters.append("median acceptance fraction: {0:.3f}".\
+                            format(np.median(sampler.acceptance_fraction)))
+    
+    # if we're running with MPI, we have to close the processor pool, otherwise
+    #   the script will never finish running until the end of timmmmeeeee (echo)
     if mpi: pool.close()
 
     data_file = os.path.join(path, "sampler_data.pickle")
     if os.path.exists(data_file):
         os.remove(data_file)
-
-    sampler_pickle = (sampler.acceptance_fraction,sampler.flatchain,sampler.chain)
-    fnpickle(sampler_pickle, data_file)
-    acceptance_fraction,flatchain,chain = sampler_pickle
-
-    idx = (acceptance_fraction > 0.1) & (acceptance_fraction < 0.6) # rule of thumb, bitches
-    run_parameters.append("{0} walkers ({1:.1f}%) converged".format(sum(idx), sum(idx)/nwalkers*100))
     
-    chain = chain[idx] 
-    flatchain = []
-    for walker in chain:
-        flatchain += list(walker)
-    flatchain = np.array(flatchain)
+    sampler.lnprobfn = None
+    try:
+        sampler.pickle(data_file)
+    except:
+        run_parameters.append("*** Unable to pickle sampler file! ***")
+
+    idx = (sampler.acceptance_fraction > 0.1) & \
+            (sampler.acceptance_fraction < 0.6) # rule of thumb, bitches
+    run_parameters.append("{0} walkers ({1:.1f}%) converged"\
+                            .format(sum(idx), sum(idx)/nwalkers*100))
 
     # If chains converged, make mcmc plots
     if len(flatchain) == 0:
         logger.warning("Not making plots -- no chains converged!")
     else:
-        posterior_fig, trace_fig = plot_mcmc(params, acceptance_fraction, flatchain, chain)
-        
-        posterior_fig.savefig(os.path.join(path, "mcmc_posterior.png"), format="png")
-        trace_fig.savefig(os.path.join(path, "mcmc_trace.png"), format="png")
+        fig = emcee_plot(sampler, params=params, converged_idx=100, 
+                         acceptance_fraction_bounds=(0.1, 0.6))
+        fig.savefig(os.path.join(path, "emcee_sampler.png"), format="png")
     
     # Save the run parameters
     with open(os.path.join(path, "run_parameters"), "w") as f:
         f.write("\n".join(run_parameters))
         
     return
-
-def plot_sgr_snap(sgr_snap):
-    """ Plot 3 projections of the initial particle positions. """
-    
-    fig, axes = plt.subplots(2,2,figsize=(10,10))
-    
-    axes[0,0].scatter(sgr_snap.x, sgr_snap.y, edgecolor="none", c="k", s=10)
-    axes[1,0].scatter(sgr_snap.x, sgr_snap.z, edgecolor="none", c="k", s=10)
-    axes[1,1].scatter(sgr_snap.y, sgr_snap.z, edgecolor="none", c="k", s=10)
-    
-    axes[0,0].set_ylabel("{0} [{1}]".format("Y", sgr_snap.r_unit))
-    axes[1,0].set_xlabel("{0} [{1}]".format("X", sgr_snap.r_unit))
-    axes[1,0].set_ylabel("{0} [{1}]".format("Z", sgr_snap.r_unit))
-    axes[1,1].set_xlabel("{0} [{1}]".format("Y", sgr_snap.r_unit))
-    
-    axes[0,1].set_visible(False)
-    fig.subplots_adjust(hspace=0.1, wspace=0.1, left=0.08, bottom=0.08, top=0.9, right=0.9 )
-    return fig
 
 if __name__ == "__main__":
     config_kwargs = simulation_setup()
