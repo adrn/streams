9c0f7cc27d45645032b9ed680215d5b6c59e19ac
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-04-18 15:07:46 -0400

    minor optimizations to potential code

--- streams/potential/_common.pyx
+++ streams/potential/_common.pyx
@@ -1,4 +1,11 @@
-# cython -a _common.pyx; gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing -I /usr/include/python2.7 -L /usr/lib/python2.7 -l python -I/System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python/numpy/core/include -o _common.so _common.c
+"""
+Deimos:
+cython -a _common.pyx
+gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing -I/usr/local/lib/python2.7/dist-packages/numpy/core/include -o _common.so _common.c
+
+
+gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing -I /usr/include/python2.7 -L /usr/lib/python2.7 -l python -I/System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python/numpy/core/include -o _common.so _common.c
+"""
 from __future__ import division
 
 import numpy as np
@@ -17,36 +24,25 @@ cdef extern from "math.h":
 # This really only helps for >10^6 particles!
 
 @cython.boundscheck(False) # turn of bounds-checking for entire function
-def _miyamoto_nagai_dx(double G, double M, double a, double b, np.ndarray[double, ndim=1] _x, np.ndarray[double, ndim=1] _y, np.ndarray[double, ndim=1] _z, long length):
-
-    cdef np.ndarray[double, ndim=1] data
-    data = np.zeros(length, dtype=np.float64)
-
+def _cartesian_point_mass_df(double G, double m, long length, 
+                             np.ndarray[double, ndim=1] origin, 
+                             np.ndarray[double, ndim=1] x, 
+                             np.ndarray[double, ndim=1] y, 
+                             np.ndarray[double, ndim=1] z):
+                             
+    cdef np.ndarray[double, ndim=2] data
+    data = np.zeros((3,length), dtype=np.float64)
+    cdef double xx, yy, zz
+    
     for ii in range(length):
-        data[ii] = G * M * _x[ii] / (_x[ii]*_x[ii] + _y[ii]*_y[ii] + (a + sqrt(_z[ii]*_z[ii] + b*b))**2)**1.5
-
-    return data
-
-@cython.boundscheck(False) # turn of bounds-checking for entire function
-def _miyamoto_nagai_dy(double G, double M, double a, double b, np.ndarray[double, ndim=1] _x, np.ndarray[double, ndim=1] _y, np.ndarray[double, ndim=1] _z, long length):
-
-    cdef np.ndarray[double, ndim=1] data
-    data = np.zeros(length, dtype=np.float64)
-
-    for ii in range(length):
-        data[ii] = G * M * _y[ii] / (_x[ii]*_x[ii] + _y[ii]*_y[ii] + (a + sqrt(_z[ii]*_z[ii] + b*b))**2)**1.5
-
-    return data
-
-@cython.boundscheck(False) # turn of bounds-checking for entire function
-def _miyamoto_nagai_dz(double G, double M, double a, double b, np.ndarray[double, ndim=1] _x, np.ndarray[double, ndim=1] _y, np.ndarray[double, ndim=1] _z, long length):
-
-    cdef double zsqrt
-    cdef np.ndarray[double, ndim=1] data
-    data = np.zeros(length, dtype=np.float64)
-
-    for ii in range(length):
-        zsqrt = sqrt(_z[ii]*_z[ii] + b*b)
-        data[ii] = G * M * _z[ii] * (1. + a / zsqrt) / (_x[ii]*_x[ii] + _y[ii]*_y[ii] + (a + zsqrt)**2)**1.5
+        xx = (x[ii]-origin[0])
+        yy = (y[ii]-origin[1])
+        zz = (z[ii]-origin[2]) 
+        
+        fac = G * m * (xx*xx + yy*yy + zz*zz)**-1.5
+        
+        data[0][ii] = fac*xx
+        data[1][ii] = fac*yy
+        data[2][ii] = fac*zz
 
     return data
