edcc7d565089c71f5608d7efbb6fbb91ff65e144
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-05-06 10:44:53 -0400

    setup script for building cython code

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+# Get numpy path
+import os, numpy
+numpy_base_path = os.path.split(numpy.__file__)[0]
+numpy_incl_path = os.path.join(numpy_base_path, "core", "include")
+
+integrate = Extension("streams.simulation._integrate_lm10", 
+                      ["streams/simulation/_integrate_lm10.pyx"],
+                      include_dirs=[numpy_incl_path])
+
+setup(
+    name="Streams",
+    version="0.0",
+    author="Adrian Price-Whelan",
+    author_email="adrn@astro.columbia.edu",
+    license="BSD",
+    cmdclass = {'build_ext': build_ext},
+    ext_modules=[integrate]
+)
