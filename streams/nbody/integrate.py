4fe4b76b7b98c75175d47b87d41672c9493a666f
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-05-20 01:43:57 -0400

    woot, working

--- streams/nbody/integrate.py
+++ streams/nbody/integrate.py
@@ -12,9 +12,52 @@ import os, sys
 # Third-party
 import numpy as np
 import astropy.units as u
+from astropy.constants import G
 
 __all__ = ["nbody_integrate"]
+
+
+def _nbody_acceleration(G, R, M):
+    a = np.zeros_like(R)
+    for ii in range(R.shape[0]):
+        other_r = np.delete(R, ii, axis=0)
+        other_m = np.delete(M, ii, axis=0)[...,np.newaxis]
+        
+        rr = R[ii] - other_r
+        num = (-G * other_m * rr).sum(axis=0)
+        denom = (np.sum(rr**2,axis=0)**1.5).sum(axis=0)
+        a[ii] = num / denom
+    
+    return a
 
 def nbody_integrate(particle_collection, time_steps, merge_length):
     """ TODO """
     
+    dt = (time_steps[1]-time_steps[0])\
+            .decompose(bases=particle_collection._units.values()).value
+    Nsteps = len(time_steps)
+    
+    r_i = particle_collection._r
+    v_i = particle_collection._v
+    m = particle_collection._m
+    _G = G.decompose(bases=particle_collection._units.values()).value
+    
+    # Shape of final object should be (Ntimesteps, Nparticles, Ndim)
+    rs = np.zeros((Nsteps,) + r_i.shape, dtype=np.float64)
+    vs = np.zeros((Nsteps,) + v_i.shape, dtype=np.float64)
+    
+    a_ip1 = _nbody_acceleration(_G, r_i, m)
+    for ii in range(Nsteps):
+        a_i = a_ip1
+        
+        r_ip1 = r_i + v_i*dt + 0.5*a_i*dt*dt
+        a_ip1 = _nbody_acceleration(_G, r_ip1, m)
+        v_ip1 = v_i + 0.5*(a_i + a_ip1)*dt
+        
+        rs[ii,:,:] = r_i
+        vs[ii,:,:] = v_i
+
+        r_i = r_ip1
+        v_i = v_ip1
+    
+    return rs, vs
--- streams/nbody/tests/test_integrate.py
+++ streams/nbody/tests/test_integrate.py
@@ -10,23 +10,90 @@ import os, sys
 # Third-party
 import numpy as np
 import pytest
+import astropy.units as u
+from astropy.constants import G
+import matplotlib.pyplot as plt
 
 from ..core import *
 from ..integrate import *
+from ..integrate import _nbody_acceleration
+
+def test_acceleration():
+    r = np.array([[1.,0.],
+                  [0, 1.],
+                  [-1., 0.],
+                  [0., -1.]])
+    m = np.array([1.,1.,1.,1.])
+    _G = G.decompose(bases=[u.kpc,u.Myr,u.M_sun]).value
+    
+    a = _nbody_acceleration(_G, r, m)
+    assert a.shape == r.shape
+    
+    print(a)
 
 def test_api():
     
     # Create particles
-    N = 100
     particles = []
-    for ii in range(N):
-        p = Particle(r=np.array([0.08*ii+1.,0.,0.])*u.kpc,
-                     v=np.array([0., 220., 0.])*u.km/u.s,
+    earth = Particle(r=np.array([1., 0., 0.])*u.au,
+                     v=np.array([0., 2.*np.pi, 0.])*u.au/u.yr,
+                     m=1.E-6*u.M_sun)
+    particles.append(earth)
+    
+    sun = Particle(r=np.array([0.,0.,0.])*u.au,
+                   v=np.array([0.,0.,0.])*u.au/u.yr,
                      m=1.*u.M_sun)
-        particles.append(p)
-    pc = ParticleCollection(particles=particles)
+    particles.append(sun)
+    
+    pc = ParticleCollection(particles=particles, units=[u.au, u.yr, u.M_sun])
     
     # Create time grid to integrate on
-    t = np.arange(0., 6000, 1.) * u.Myr
+    t = np.arange(0., 10., 0.02) * u.yr
+    r,v = nbody_integrate(pc, time_steps=t, merge_length=1.E-3*u.au)
     
-    orbits = nbody_integrate(pc, time_steps=t, merge_length=1.*u.pc)
+    plt.figure(figsize=(8,8))
+    plt.plot(r[:,0,0], r[:,0,1], 'b-')
+    plt.plot(r[:,1,0], r[:,1,1], 'b-')
+    plt.show()
+
+def test_collection():
+    
+    # Create particles
+    particles = []
+    p1 = Particle(r=np.array([1., 0., 0.])*u.au,
+                  v=np.array([0., 2.*np.pi, 0.])*u.au/u.yr,
+                  m=1.*u.M_sun)
+    particles.append(p1)
+    
+    p2 = Particle(r=np.array([0., 1., 0.])*u.au,
+                  v=np.array([-2.*np.pi, 0., 0.])*u.au/u.yr,
+                  m=1.*u.M_sun)
+    particles.append(p2)
+    
+    p3 = Particle(r=np.array([-1., 0., 0.])*u.au,
+                  v=np.array([0., -2.*np.pi, 0.])*u.au/u.yr,
+                  m=1.*u.M_sun)
+    particles.append(p3)
+    
+    p4 = Particle(r=np.array([0., -1., 0.])*u.au,
+                  v=np.array([2.*np.pi, 0., 0.])*u.au/u.yr,
+                  m=1.*u.M_sun)
+    particles.append(p4)
+    
+    p5 = Particle(r=np.array([0., 0., 0.])*u.au,
+                  v=np.array([0., 0., 0.])*u.au/u.yr,
+                  m=10.*u.M_sun)
+    particles.append(p5)
+    
+    pc = ParticleCollection(particles=particles, units=[u.au, u.yr, u.M_sun])
+    
+    # Create time grid to integrate on
+    t = np.arange(0., 10., 0.01) * u.yr
+    r,v = nbody_integrate(pc, time_steps=t, merge_length=1.E-3*u.au)
+    
+    plt.figure(figsize=(8,8))
+    plt.plot(r[:,0,0], r[:,0,1], 'k-')
+    plt.plot(r[:,1,0], r[:,1,1], 'b-')
+    plt.plot(r[:,2,0], r[:,2,1], 'r-')
+    plt.plot(r[:,3,0], r[:,3,1], 'g-')
+    plt.show()
