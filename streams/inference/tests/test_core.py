b6a8d8b665082522ebc952d8fc685f1308460c79
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-05-20 16:49:00 -0400

    totally broken test...

--- streams/inference/tests/test_core.py
+++ streams/inference/tests/test_core.py
@@ -14,13 +14,11 @@ import numpy as np
 import pytest
 import astropy.units as u
 
-try:
-    from ..core import ln_likelihood
-    from ...simulation import TestParticle, TestParticleOrbit
-except ValueError:
-    from streams.inference.core import ln_likelihood
-    from streams.simulation import TestParticle, TestParticleOrbit
+from ..core import ln_likelihood
+from ...nbody import ParticleCollection
     
+# TODO: totally broken...    
+
 def cprofile_time():
     for ii in range(3):
         test_time_likelihood()
@@ -29,8 +27,10 @@ def test_time_likelihood():
     
     p = [1.2, 1.2]
     param_names = ["q1", "qz"]
-    particles = TestParticle(r=np.random.uniform(size=(100,3))*u.kpc,
-                             v=np.random.uniform(size=(100,3))*u.kpc/u.Myr)
+    particles = ParticleCollection(r=np.random.uniform(size=(100,3))*u.kpc,
+                                   v=np.random.uniform(size=(100,3))*u.kpc/u.Myr,
+                                   m=np.zeros(100)*u.M_sun,
+                                   units=[u.kpc,u.Myr,u.M_sun])
     
     t = np.linspace(0., 10., 500)*u.Myr
     sat_orbit = TestParticleOrbit(t=t,
