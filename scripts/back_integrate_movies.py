2f5e7f69763acfec234561cc786cc113695e2c0e
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-05-27 10:08:29 -0400

    still updating movie maker

--- scripts/back_integrate_movies.py
+++ scripts/back_integrate_movies.py
@@ -12,6 +12,7 @@ import os, sys
 # Third-party
 import astropy.units as u
 import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
 import numpy as np
 
 from streams.data import read_lm10
@@ -26,12 +27,6 @@ ffmpeg_cmd = "ffmpeg -i {0} -r 12 -b 5000 -vcodec libx264 -vpre medium -b 3000k 
 plot_path = "plots/movies/back_integrate"
 if not os.path.exists(plot_path):
     os.makedirs(plot_path)
-
-# Read in particles from Law & Majewski 2010 simulation of Sgr
-#expr="(Pcol>0) & (abs(Lmflag)==1)"
-t,satellite, particles = read_lm10(N=5000, 
-                                  expr="(abs(Lmflag)==1)",
-                                  dt=1.*u.Myr)
 
 def back_integrate(potential):
     xx,r,v = leapfrog(potential._acceleration_at, 
@@ -68,18 +63,9 @@ def plot_orbits(potential, s, p):
     
     return fig,axes
 
-# Define correct potential, and 10% wrong potential
-correct = LawMajewski2010()
-wrong = LawMajewski2010(q1=true_params["q1"]*1.05,
-                        qz=true_params["qz"]*1.05,
-                        phi=true_params["phi"]*1.05,
-                        v_halo=true_params["v_halo"]*1.05)
-
-c_s_orbit, c_p_orbit = back_integrate(correct) # correct
-w_s_orbit, w_p_orbit = back_integrate(wrong) # wrong
-
 def plot_animation(potential, s, p, filename=""):
-    grid = np.linspace(np.min(p._r)-2., np.max(p._r)+2., 200)*u.kpc
+    #grid = np.linspace(np.min(p._r)-2., np.max(p._r)+2., 200)*u.kpc
+    grid = np.linspace(-81, 81, 200)*u.kpc
     fig = None
     
     idx = np.ones(p._r.shape[1]).astype(bool)
@@ -87,42 +73,91 @@ def plot_animation(potential, s, p, filename=""):
     all_D_ps = np.sqrt(np.sum(R**2, axis=-1) + np.sum(V**2, axis=-1))
     r_tide = tidal_radius(potential, s)[:,:,np.newaxis]
     
-    for ii in range(len(t)):
-        if fig == None:
-            fig,axes = potential.plot(ndim=3, grid=grid)
-        else:
-            [a.cla() for a in np.ravel(axes)]
-            fig,axes = potential.plot(ndim=3, grid=grid, axes=axes)
-        
+    fig,axes = potential.plot(ndim=3, grid=grid)
+    jj = 0
+    for ii in range(0,len(t),10):
         D_ps = all_D_ps[ii]
         idx = idx & (D_ps > 2.8)
-        #eja = np.argmin(D_ps)
-        #print(R[ii,eja], V[ii,eja], sum(idx))
-        print(sum(idx))
         
-        axes[0,0].set_xlim(-75,75)
-        axes[0,0].set_ylim(-75,75)
-        axes[0,0].scatter(s._r[ii,0,0], s._r[ii,0,1], marker='o', color='r', alpha=0.75)
-        axes[0,0].scatter(p._r[ii,:,0][idx], p._r[ii,:,1][idx], marker='.', color='w', alpha=0.2, s=8)
+        offsets = p._r[ii]
+        offsets[np.logical_not(idx)] = np.ones_like(offsets[np.logical_not(idx)])*10000.
+        sat_r = s._r[ii,0]
         
-        axes[1,0].scatter(s._r[ii,0,0], s._r[ii,0,2], marker='o', color='r', alpha=0.75)
-        axes[1,0].scatter(p._r[ii,:,0][idx], p._r[ii,:,2][idx], marker='.', color='w', alpha=0.2, s=8)
+        try:
+            scatter_map[(0,0)].set_offsets(np.vstack((offsets[:,0],offsets[:,1])).T)
+            scatter_map[(1,0)].set_offsets(np.vstack((offsets[:,0],offsets[:,2])).T)
+            scatter_map[(1,1)].set_offsets(np.vstack((offsets[:,1],offsets[:,2])).T)
+            
+            scatter_map_sat[(0,0)].center = (sat_r[0], sat_r[1])
+            scatter_map_sat[(0,0)].set_radius(r_tide[ii])
+            scatter_map_sat[(1,0)].center = (sat_r[0], sat_r[2])
+            scatter_map_sat[(1,0)].set_radius(r_tide[ii])
+            scatter_map_sat[(1,1)].center = (sat_r[1], sat_r[2])
+            scatter_map_sat[(1,1)].set_radius(r_tide[ii])
+        except NameError:
+            scatter_map = dict()
+            scatter_map_sat = dict()
+            
+            c = Circle((sat_r[0], sat_r[1]), radius=r_tide[ii], color='r', alpha=0.5)
+            axes[0,0].add_patch(c)
+            scatter_map_sat[(0,0)] = c
+            
+            c = Circle((sat_r[0], sat_r[2]), radius=r_tide[ii], color='r', alpha=0.5)
+            axes[1,0].add_patch(c)
+            scatter_map_sat[(1,0)] = c
+            
+            c = Circle((sat_r[1], sat_r[2]), radius=r_tide[ii], color='r', alpha=0.5)
+            axes[1,1].add_patch(c)
+            scatter_map_sat[(1,1)] = c
+            
+            scatter_map[(0,0)] = axes[0,0].scatter(offsets[:,0], offsets[:,1], 
+                                                   marker='.', color='w', 
+                                                   alpha=0.2, s=5)
+            scatter_map[(1,0)] = axes[1,0].scatter(offsets[:,0], offsets[:,2], 
+                                                   marker='.', color='w', 
+                                                   alpha=0.2, s=5)
+            scatter_map[(1,1)] = axes[1,1].scatter(offsets[:,1], offsets[:,2], 
+                                                   marker='.', color='w', 
+                                                   alpha=0.2, s=5)
         
-        axes[1,1].scatter(s._r[ii,0,1], s._r[ii,0,2], marker='o', color='r', alpha=0.75)
-        axes[1,1].scatter(p._r[ii,:,1][idx], p._r[ii,:,2][idx], marker='.', color='w', alpha=0.2, s=8)
+        axes[0,0].set_xlim(-81,81)
+        axes[0,0].set_ylim(-81,81)
+        fig.savefig(os.path.join(plot_path,"{0}{1:04d}.png".format(filename,jj)))
+        jj += 1
+    
+    print("{0} unbound at end of run.".format(sum(idx)))
+
+if __name__ == "__main__":
+    
+    N = 10000
+    dt = 1.*u.Myr
+    
+    # Read in particles from Law & Majewski 2010 simulation of Sgr
+    #expr="(Pcol>0) & (abs(Lmflag)==1)"
+    t,satellite, particles = read_lm10(N=N, 
+                                      expr="(Pcol > -1) & (Pcol < 6) & (abs(Lmflag) == 1)",
+                                      dt=dt)
+    
+    # Define correct potential, and 10% wrong potential
+    correct = LawMajewski2010()
+    wrong = LawMajewski2010(q1=true_params["q1"]*1.05,
+                            qz=true_params["qz"]*1.05,
+                            phi=true_params["phi"]*1.05,
+                            v_halo=true_params["v_halo"]*1.05)
+    
+    c_s_orbit, c_p_orbit = back_integrate(correct) # correct
+    w_s_orbit, w_p_orbit = back_integrate(wrong) # wrong
         
-        fig.savefig(os.path.join(plot_path,"{0}{1:04d}.png".format(filename,ii)))
-
-fig,axes = plot_orbits(correct, c_s_orbit, c_p_orbit)
-fig.savefig(os.path.join(plot_path,"correct_orbits.png"))
-plot_animation(correct, c_s_orbit, c_p_orbit, filename="correct_")
-
-fig,axes = plot_orbits(wrong, w_s_orbit, w_p_orbit)
-fig.savefig(os.path.join(plot_path,"wrong_orbits.png"))
-plot_animation(wrong, w_s_orbit, w_p_orbit, filename="wrong_")
-
-os.system(ffmpeg_cmd.format(os.path.join(plot_path, "correct_%4d.png"), 
-                            os.path.join(plot_path, "correct.mp4")))
-
-os.system(ffmpeg_cmd.format(os.path.join(plot_path, "wrong_%4d.png"), 
-                            os.path.join(plot_path, "wrong.mp4")))
+    fig,axes = plot_orbits(correct, c_s_orbit, c_p_orbit)
+    fig.savefig(os.path.join(plot_path,"correct_orbits.png"))
+    plot_animation(correct, c_s_orbit, c_p_orbit, filename="correct_")
+    
+    fig,axes = plot_orbits(wrong, w_s_orbit, w_p_orbit)
+    fig.savefig(os.path.join(plot_path,"wrong_orbits.png"))
+    plot_animation(wrong, w_s_orbit, w_p_orbit, filename="wrong_")
+    
+    os.system(ffmpeg_cmd.format(os.path.join(plot_path, "correct_%4d.png"), 
+                                os.path.join(plot_path, "correct.mp4")))
+    
+    os.system(ffmpeg_cmd.format(os.path.join(plot_path, "wrong_%4d.png"), 
+                                os.path.join(plot_path, "wrong.mp4")))
