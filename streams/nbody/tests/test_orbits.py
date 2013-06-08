4fb9cd8c3ac6156b32a27c8102aa807a939686f5
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-06-08 16:41:12 -0400

    only init tests

--- streams/nbody/tests/test_orbits.py
+++ streams/nbody/tests/test_orbits.py
@@ -19,59 +19,21 @@ from ..orbits import *
 
 usys = UnitSystem(u.kpc, u.Myr, u.M_sun)
 
-def test_particlecollection_init():
-
-    # Init with individual arrays of ndim=1
-    r = np.random.random(3)*u.kpc
-    v = np.random.random(3)*u.km/u.s
-    m = np.random.random()*u.M_sun
+def test_orbitcollection_init():
+    t = np.linspace(0., 10., 100)*u.yr
+    r = np.random.random((100,10,3))*u.kpc
+    v = np.random.random((100,10,3))*u.kpc/u.yr
+    m = np.random.random(10)*u.M_sun
     
+    oc = OrbitCollection(t=t, r=r, v=v, m=m)
+    
+    # Should fail -- different length units, no usys:
+    t = np.linspace(0., 10., 100)*u.yr
+    r = np.random.random((100,10,3))*u.kpc
+    v = np.random.random((100,10,3))*u.km/u.s
+    m = np.random.random(10)*u.M_sun
     with pytest.raises(ValueError):
-        pc = ParticleCollection(r=r, v=v, m=m, unit_system=usys)
+        oc = OrbitCollection(t=t, r=r, v=v, m=m)
     
-    r = np.random.random(size=(10,3))*u.kpc
-    v = np.random.random(size=(10,3))*u.km/u.s
-    m = np.random.random(10)*u.M_sun
-    
-    pc = ParticleCollection(r=r, v=v, m=m, unit_system=usys)
-    assert np.all(pc.r.value == r.value)
-    assert np.all(pc.v.value == v.value)
-    assert np.all(pc.m.value == m.value)
-    
-    assert np.all(pc._r == r.value)
-    assert np.all(pc._v == v.value)
-    assert np.all(pc._m == m.value)
-
-def test_acceleration():
-    r = np.array([[1.,0.],
-                  [0, 1.],
-                  [-1., 0.],
-                  [0., -1.]])*u.kpc
-    v = np.zeros_like(r.value)*u.km/u.s
-    m = np.random.random()*u.M_sun
-    
-    pc = ParticleCollection(r=r, v=v, m=m, unit_system=usys)
-
-    pc.acceleration_at(np.array([0.,0.])*u.kpc, m=1.*u.M_sun)
-    
-    a = pc.acceleration_at(np.array([[0.5,0.5], [0.0,0.0], [-0.5, -0.5]])*u.kpc,
-                           m=[1.,1.,1.]*u.M_sun)
-
-def test_merge():
-    # test merging two particle collections
-    
-    r = np.random.random(size=(10,3))*u.kpc
-    v = np.random.random(size=(10,3))*u.km/u.s
-    m = np.random.random(10)*u.M_sun
-    
-    pc1 = ParticleCollection(r=r, v=v, m=m, unit_system=usys)
-    
-    r = np.random.random(size=(10,3))*u.kpc
-    v = np.random.random(size=(10,3))*u.km/u.s
-    m = np.random.random(10)*u.M_sun
-    
-    pc2 = ParticleCollection(r=r, v=v, m=m, unit_system=usys)
-    
-    pc_merged = pc1.merge(pc2)
-    
-    assert pc_merged._r.shape == (20,3)
+    # should pass bc we give a usys
+    oc = OrbitCollection(t=t, r=r, v=v, m=m, unit_system=usys)
