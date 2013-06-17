7d474b9deea702cb9e57ac23da29eccaa60654fa
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-06-17 15:25:25 -0400

    change max likelihood paramters to median

--- streams/inference/inferpotential.py
+++ streams/inference/inferpotential.py
@@ -91,15 +91,23 @@ def max_likelihood_parameters(sampler):
     nwalkers, nsteps, nparams = sampler.chain.shape
     
     # only use samplers that have reasonable acceptance fractions
-    good_walkers = (sampler.acceptance_fraction > 0.15) & \
-                   (sampler.acceptance_fraction < 0.6)
+    good_walkers = (sampler.acceptance_fraction > 0.05) & \
+                   (sampler.acceptance_fraction < 0.8)
     
     logger.info("{0} walkers ({1:.1f}%) converged"
                 .format(sum(good_walkers), sum(good_walkers)/nwalkers*100))
     
     good_chain = sampler.chain[good_walkers] # (sum(good_walkers), nsteps, nparams)
     good_probs = sampler.lnprobability[good_walkers] # (sum(good_walkers), nsteps)
+    flatchain = np.vstack(good_chain)
     
+    # Find median values in each parameter
+    best_params = []
+    for ii in range(nparams):
+        xsorted = sorted(flatchain[:,ii])
+        best_params.append(xsorted[int(0.5 * len(xsorted))])
+    
+    """
     best_step_idx = np.argmax(np.ravel(good_probs))
     
     flatchain = []
@@ -108,5 +116,6 @@ def max_likelihood_parameters(sampler):
     flatchain = np.array(flatchain)
 
     best_params = flatchain[best_step_idx]
+    """
     
-    return best_params
+    return np.array(best_params)
