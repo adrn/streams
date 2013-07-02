503267b803d5c6767ab51d06ebf01ecf4c05d355
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-07-02 14:16:18 -0400

    fixing imports with new observation subpackage

--- scripts/paper1_figures.py
+++ scripts/paper1_figures.py
@@ -14,9 +14,10 @@ import astropy.units as u
 import numpy as np
 import matplotlib.pyplot as plt
 
-from streams.data.gaia import parallax_error, proper_motion_error, \
-                              rr_lyrae_V_minus_I, rr_lyrae_M_V, \
-                              apparent_magnitude
+from streams.observation.gaia import parallax_error, proper_motion_error, \
+                              rr_lyrae_V_minus_I, apparent_magnitude
+from streams.observation.rrlyrae import rrl_M_V
+
 from streams.potential import LawMajewski2010
 from streams.integrate.satellite_particles import SatelliteParticleIntegrator
 from streams.data import lm10_particles, lm10_satellite, lm10_time
