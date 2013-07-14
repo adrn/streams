5a32410b74997216ddf99ea68a8b4fb98fb04d85
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-07-14 15:09:06 -0400

    added test for to() in orbitcollection

--- streams/nbody/tests/test_orbits.py
+++ streams/nbody/tests/test_orbits.py
@@ -37,3 +37,19 @@ def test_orbitcollection_init():
     
     # should pass bc we give a usys
     oc = OrbitCollection(t=t, r=r, v=v, m=m, unit_system=usys)
+
+def test_to():
+    t = np.arange(0., 100, 0.1)*u.Myr
+    r = np.random.random(size=(len(t),10,3))*u.kpc
+    v = np.random.random(size=(len(t),10,3))*u.kpc/u.Myr
+    m = np.random.random(10)*u.M_sun
+    
+    pc = OrbitCollection(t=t, r=r, v=v, m=m, unit_system=usys)
+    
+    usys2 = UnitSystem(u.km, u.s, u.kg)    
+    pc2 = pc.to(usys2)
+    
+    assert np.allclose(pc2._t, t.to(u.s).value)
+    assert np.allclose(pc2._r, r.to(u.km).value)
+    assert np.allclose(pc2._v, v.to(u.km/u.s).value)
+    assert np.allclose(pc2._m, m.to(u.kg).value)
