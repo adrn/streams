b76e09a354201f52b1b219d955c8711d441f0cb7
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-07-14 15:49:32 -0400

    made orphan reading nicer

--- streams/io/orphan.py
+++ streams/io/orphan.py
@@ -23,7 +23,7 @@ from .core import read_table
 from ..nbody import ParticleCollection, OrbitCollection
 from ..misc import UnitSystem
 
-__all__ = ["orphan_particles", "orphan_satellite", "orphan_time"]
+__all__ = ["particles", "orphan_satellite", "orphan_time"]
 
 _orphan_path = os.path.join(project_root, "data", "simulation", "Orphan")
 
@@ -33,7 +33,7 @@ mass_unit = u.Unit("1E7 M_sun")
 time_unit = u.Unit("{:08f} Myr".format(X))
 orphan_usys = UnitSystem(length_unit, mass_unit, time_unit)
 
-def orphan_particles(N=None, expr=None):
+def particles(N=None, expr=None):
     """ Read in particles from Kathryn's run of a satellite similar to 
         the progenitor of the Orphan stream
     
@@ -56,55 +56,25 @@ def orphan_particles(N=None, expr=None):
     
     return pc.to(UnitSystem.galactic())
 
-def orphan_satellite():
+def satellite():
     """ Read in the position and velocity of the Orphan satellite center 
         at the end of the simulation (e.g., present day position to be
         back-integrated).
     
     """
-    sat_filename = os.path.join(project_root, 
-                                "data",
-                                "simulation", 
-                                "Orphan",
-                                "ORP_CEN")
-    sat_colnames = ["t","dt","x","y","z","vx","vy","vz"]
+    data = read_table("ORP_CEN", path=_orphan_path)
+    orbit = table_to_orbits(data, orphan_usys,
+                            position_columns=["x","y","z"],
+                            velocity_columns=["vx","vy","vz"])
     
-    # read in ascii file
-    satellite_data = ascii.read(sat_filename, names=sat_colnames)
+    return orbit[-1]
     
-    # initial conditions, or, position of the satellite today
-    i = -1
-    r = [[satellite_data[i]['x'], 
-          satellite_data[i]['y'],
-          satellite_data[i]['z']]]
-    r = r*length_unit
-    
-    v = [[satellite_data[i]['vx'], 
-          satellite_data[i]['vy'],
-          satellite_data[i]['vz']]]
-    v = v*length_unit/time_unit
-    
-    satellite = ParticleCollection(r=r.to(u.kpc), 
-                                   v=v.to(u.km/u.s), 
-                                   m=1E7*u.M_sun,
-                                   units=[u.kpc,u.Myr,u.M_sun])
-    
-    return satellite
-    
-def orphan_time():
+def time():
     """ Read in the time information for Orphan stream simulation 
     """
-    sat_filename = os.path.join(project_root, 
-                                "data",
-                                "simulation", 
-                                "Orphan",
-                                "ORP_CEN")
-    sat_colnames = ["t","dt","x","y","z","vx","vy","vz"]
+    data = read_table("ORP_CEN", path=_orphan_path)
     
-    # read in ascii file
-    satellite_data = ascii.read(sat_filename, names=sat_colnames)
-    
-    t1 = (max(satellite_data["t"])*time_unit).to(u.Myr).value
-    t2 = (min(satellite_data["t"])*time_unit).to(u.Myr).value
+    t1 = (max(data["t"])*time_unit).to(u.Myr).value
+    t2 = (min(data["t"])*time_unit).to(u.Myr).value
     
     return t1, t2
