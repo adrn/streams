ac70be76d62800feb6a6d7b667fddbeacd4131a3
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-07-14 18:58:44 -0400

    renaming...

--- streams/inference/core.py
+++ streams/inference/core.py
@@ -92,7 +92,6 @@ def generalized_variance(potential, particle_orbits, satellite_orbit):
     
     # cov_matrix -> (6 x 6) covariance matrix for particles
     #w,v = np.linalg.eig(cov_matrix)
-    
     #return np.sum(w)
     #return np.prod(w)
     
