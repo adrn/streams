e80305bd0f8d0ad62d6ea86e08ac5c58a204f18f
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-07-14 13:16:37 -0400

    reorganize io subpackage

---
+++ streams/io/simulation.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+""" Reader functions for ASCII files of simulation data. """
+
+from __future__ import division, print_function
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Standard library
+import os, sys
+
+# Third-party
+import numpy as np
+import astropy.io.ascii as ascii
+
+from ...util import project_root
+
+__all__ = [""]
+
