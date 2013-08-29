f92f4f32eb0ba789f0ba324b3f2df37dd854a3ae
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-08-29 22:09:15 +0000

    move emcee plot, make more general

--- scripts/plot_emcee_sampler.py
+++ scripts/plot_emcee_sampler.py
@@ -14,8 +14,9 @@ import cPickle as pickle
 import numpy as np
 import matplotlib.pyplot as plt
 import astropy.units as u
+from astropy.io.misc import fnunpickle
 
-from streams.plot import plot_sampler_pickle
+from streams.plot.emcee import emcee_plot
     
 if __name__ == "__main__":
     from argparse import ArgumentParser
@@ -23,8 +24,10 @@ if __name__ == "__main__":
     parser = ArgumentParser(description="")
     parser.add_argument("-f", "--file", dest="file", required=True,
                     help="Path to the sampler pickle file.")
-    parser.add_argument("--params", dest="params", default=[], nargs='+', 
+    parser.add_argument("--params", dest="params", default=None, nargs='+', 
                     required=True, action='store', help="The halo parameters.")
+    parser.add_argument("-s", "--source", dest="particle_source", required=True,
+                        help="The source of the particles, e.g., lm10 or pal5")
     parser.add_argument("-o", "--output-file", dest="output_file", default=None,
                     help="Path to save the plot.")
     parser.add_argument("-a", "--acceptance-fraction", dest="acc_frac", nargs=2,
@@ -35,9 +38,23 @@ if __name__ == "__main__":
     
     args = parser.parse_args()
     
-    fig = plot_sampler_pickle(args.file, args.params, 
-                              acceptance_fraction_bounds=args.acc_frac, 
-                              show_true=args.show_true)
+    if args.particle_source.lower() == 'lm10':
+        from streams.potential.lm10 import _true_params
+    elif args.particle_source.lower() == 'pal5':
+        from streams.potential.pal5 import _true_params
+    else:
+        raise ValueError("Invalid particle source {0}"
+                         .format(config["particle_source"]))
+    
+    sampler = fnunpickle(args.file)
+    xs = sampler.chain
+    
+    if args.show_true:
+        truths = [_true_params[p] for p in args.params]
+    else:
+        truths = None
+    
+    fig = emcee_plot(xs, labels=args.params, truths=truths)
     
     if args.output_file:
         fig.savefig(args.output_file)
