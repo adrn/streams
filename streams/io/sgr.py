1319a9e77a2889afb4c35d6590610616d0983c3e
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-09-11 15:38:08 -0400

    Added script for reading KVJ's Sgr sims

---
+++ streams/io/sgr.py
@@ -0,0 +1,88 @@
+# coding: utf-8
+
+""" Classes for accessing simulation data for the Orphan stream. """
+
+from __future__ import division, print_function
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Standard library
+import os, sys
+
+# Third-party
+import numpy as np
+import numexpr
+import astropy.io.ascii as ascii
+from astropy.table import Column
+import astropy.units as u
+from astropy.constants import G
+
+# Project
+from .core import read_table, table_to_particles
+from ..util import project_root
+from ..dynamics import ParticleCollection, OrbitCollection
+from ..misc import UnitSystem
+
+__all__ = ["particles", "satellite", "time", "sgr_usys"]
+
+_path = os.path.join(project_root, "data", "simulation", "Sgr")
+
+X = (G.decompose(bases=[u.kpc,u.M_sun,u.Myr]).value / 0.15**3 * 1E7)**-0.5
+length_unit = u.Unit("0.15 kpc")
+mass_unit = u.Unit("1E7 M_sun")
+time_unit = u.Unit("{:08f} Myr".format(X))
+orphan_usys = UnitSystem(length_unit, mass_unit, time_unit)
+
+def particles(m, N=None, expr=None):
+    """ Read in particles from Kathryn's run of Sgr-like orbits for a 
+        variety of masses.
+    
+        Parameters
+        ----------
+        N : int
+            Number of particles to read. None means 'all'.
+        expr : str
+            String selection condition to be fed to numexpr for selecting 
+            particles.
+        
+    """
+    
+    _full_path = os.path.join(_path, m)
+    
+    # Read in particle data -- a snapshot of particle positions, velocities at
+    #   the end of the simulation
+    data = read_table("SNAP", path=_full_path, N=N, expr=expr)
+    pc = table_to_particles(data, orphan_usys,
+                            position_columns=["x","y","z"],
+                            velocity_columns=["vx","vy","vz"])
+    
+    return pc.to(UnitSystem.galactic())
+
+def satellite_orbit():
+    """ Read in the position and velocity of the Orphan satellite center 
+        over the whole simulation.
+    """
+    data = read_table("ORP_CEN", path=_orphan_path)
+    orbit = table_to_orbits(data, orphan_usys,
+                            position_columns=["x","y","z"],
+                            velocity_columns=["vx","vy","vz"])
+    
+    return orbit
+
+def satellite():
+    """ Read in the position and velocity of the Orphan satellite center 
+        at the end of the simulation (e.g., present day position to be
+        back-integrated).
+    
+    """
+    return satellite_orbit()[-1]
+    
+def time():
+    """ Read in the time information for Orphan stream simulation 
+    """
+    data = read_table("ORP_CEN", path=_orphan_path)
+    
+    t1 = (max(data["t"])*time_unit).to(u.Myr).value
+    t2 = (min(data["t"])*time_unit).to(u.Myr).value
+    
+    return t1, t2
