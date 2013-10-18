4af92deb2a34a3135be4177b19f9547c54c43920
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-10-18 13:03:11 -0400

    added new likelihood to inference

--- streams/inference/back_integrate.py
+++ streams/inference/back_integrate.py
@@ -19,25 +19,55 @@ from ..integrate.satellite_particles import satellite_particles_integrate
 
 __all__ = ["back_integrate_likelihood", "variance_likelihood"]
 
-def objective():
-    pass
-
-def back_integrate_likelihood(p, param_names, satellite, particles, 
+def back_integrate_likelihood(p, potential_params, satellite, 
+                              data_particles, data_errors, 
                               Potential, t1, t2):
-    """ Evaluate the TODO """
+    """ This is a simplified version of the likelihood laid out by D. Hogg in 
+        Bread and Butter (https://github.com/davidwhogg/BreadAndButter/). The
+        stars are assumed to come from a Gaussian progenitor, described by 
+        just two scales -- the tidal radius and velocity dispersion.
+    """
 
-    model_params = dict(zip(param_names, p))
-    potential = Potential(**model_params)
+    # First need to pull apart the parameters p -- first few are the 
+    #   potential parameters, then the true position of the stars, then
+    #   the time the stars came unbound from their progenitor.
+    Nparticles = len(data_particles)
+    Nparams = len(potential_param_names)
 
-    Nparticles = len(particles)
-    acc = np.zeros((Nparticles+1,3)) # placeholder
-    s_orbit,p_orbits = satellite_particles_integrate(satellite, particles,
-                                                     potential,
-                                                     potential_args=(Nparticles+1, acc), \
-                                                     time_spec=dict(t1=t1, t2=t2, dt=-1.))
+    # Use the specified Potential class and parameters 
+    potential_params = dict(zip(potential_param_names, p[:Nparams]))
+    potential = Potential(**potential_params)
     
-    # TODO: old one used generalized_variance, new one uses??
-    return #objective2(lm10, s_orbit, p_orbits, v_disp=0.0133)
+    # These are the true positions/velocities of the particles, which we 
+    #   add as parameters in the model
+    x = np.array(p[Nparams:Nparams+(Nparticles*6)]).reshape(Nparticles,6)
+    log_p_D_given_x = -0.5 * (np.prod(data_errors**2) + np.sum((x-data_particles._x)**2 / data_errors**2, axis=-1))
+
+    # These are the unbinding times for each particle
+    t_idx = p[Nparams+(N*6):]
+    
+    # A Particle object for the true positions of the particles -- not great...
+    particles = Particle(x[:,:3]*u.kpc, x[:,3:]*u.kpc/u.Myr, 0.*u.M_sun)
+    
+    acc = np.zeros((Nparticles+1,3))
+    s,p = satellite_particles_integrate(satellite, particles, potential, 
+                                        potential_args=(Nparticles+1, acc), 
+                                        time_spec=dict(t1=t1, t2=t2, dt=-1.))
+    
+    Ntimesteps  = p._x.shape[0]
+    
+    sat_var = np.zeros((Ntimesteps,6))
+    sat_var[:,:3] = potential._tidal_radius(satellite._m, s._r) * 1.26
+    sat_var[:,3:] += v_disp # kpc/Myr
+    cov = sat_var**2
+    
+    l = np.zeros((N,))
+    for ii in range(N):
+        pref = (0.004031441804149937 * (np.prod(cov[t_idx[ii]])**-0.5))
+        yy = -0.5*np.sum((p._x[t_idx[ii],ii] - s._x[t_idx[ii],0])**2 / cov[t_idx[ii]])
+        l[ii] = np.log(pref) + yy + log_p_D_given_x[ii]
+    
+    return np.sum(l)
 
 def variance_likelihood(p, param_names, satellite, particles, 
                         Potential, t1, t2):
