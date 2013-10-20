9db50b27d78e480d407e82a760e03536210c61f9
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-10-20 19:15:11 -0400

    changed from _x to _X

--- streams/dynamics/particles.py
+++ streams/dynamics/particles.py
@@ -19,34 +19,34 @@ import triangle
 from .core import _validate_quantity, DynamicalBase
 
 __all__ = ["Particle"]
-    
+
 class Particle(DynamicalBase):
-    
+
     def __init__(self, r, v, m=None, units=None):
         """ A represents a dynamical particle or collection of particles.
-            Particles can have mass or be massless. 
-            
+            Particles can have mass or be massless.
+
             Parameters
-            ---------- 
+            ----------
             r : astropy.units.Quantity
-                Position of the particle(s). Should have shape 
+                Position of the particle(s). Should have shape
                 (nparticles, ndim).
             v : astropy.units.Quantity
-                Velocity of the particle(s). Should have shape 
+                Velocity of the particle(s). Should have shape
                 (nparticles, ndim).
             m : astropy.units.Quantity (optional)
                 Mass of the particle(s). Should have shape (nparticles, ).
             units : list (optional)
-                A list of units defining the desired unit system for 
-                the particles. If not provided, will use the units of 
-                the input Quantities to define a system of units. Mainly 
+                A list of units defining the desired unit system for
+                the particles. If not provided, will use the units of
+                the input Quantities to define a system of units. Mainly
                 used for internal representations.
         """
-        
+
         # Make sure position has position-like units, same for velocity
         _validate_quantity(r, unit_like=u.km)
         _validate_quantity(v, unit_like=u.km/u.s)
-        
+
         if r.value.ndim == 1:
             r = r.reshape((1, len(r)))
             v = v.reshape((1, len(v)))
@@ -56,76 +56,82 @@ class Particle(DynamicalBase):
         except ValueError:
             raise ValueError("Position and velocity should have shape "
                              "(nparticles, ndim)")
-        
+
         if units is None and m is None:
-            raise ValueError("If not specifying a list of units, you must " 
+            raise ValueError("If not specifying a list of units, you must "
                              "specify a mass Quantity for the particles to"
                              "complete the unit system specification.")
         elif units is not None and m is None:
             m = ([0.] * self.nparticles * u.kg).decompose(units)
 
         _validate_quantity(m, unit_like=u.kg)
-        
+
         if units is None:
             _units = [r.unit, m.unit] + v.unit.bases + [u.radian]
             self.units = set(_units)
         else:
             self.units = units
-            
+
         unq_types = np.unique([x.physical_type for x in self.units])
         if len(self.units) != len(unq_types):
             raise ValueError("Multiple units specify the same physical type!")
 
         if r.value.shape != v.value.shape:
             raise ValueError("Position and velocity must have same shape.")
-        
+
         # decompose each input into the specified unit system
         _r = r.decompose(self.units).value
         _v = v.decompose(self.units).value
-        
-        # create container for all 6 phasespace 
-        self._x = np.zeros((self.nparticles, self.ndim*2))
-        self._x[:,:self.ndim] = _r
-        self._x[:,self.ndim:] = _v
+
+        # create container for all 6 phasespace
+        self._X = np.zeros((self.nparticles, self.ndim*2))
+        self._X[:,:self.ndim] = _r
+        self._X[:,self.ndim:] = _v
         self._m = m.decompose(self.units).value
-        
+
         # Create internal G in the correct unit system for speedy acceleration
         #   computation
         self._G = G.decompose(self.units).value
-    
+
+    def observe(self, ErrorModel):
+        """ Given an error model, transform to heliocentric coordinates,
+            apply errors models, transform back and return a new Particle
+            object.
+        """
+
     def to(self, units):
         """ Return a new Particle in the specified unit system. """
-        
+
         return Particle(r=self.r, v=self.v, m=self.m, units=units)
 
     def acceleration_at(self, r):
-        """ Compute the acceleration at a given position due to the 
+        """ Compute the acceleration at a given position due to the
             collection of particles. Inputs must be Quantity objects.
-            
+
             Parameters
             ----------
             r : astropy.units.Quantity
                 Position.
-                
+
         """
         _validate_quantity(r, unit_like=u.km)
-        
+
         r_unit = filter(lambda x: x.is_equivalent(u.km), self.units)[0]
         t_unit = filter(lambda x: x.is_equivalent(u.s), self.units)[0]
 
         a = self._acceleration_at(r.decompose(self.units).value)
         return a * r_unit / t_unit**2
-    
+
     def _acceleration_at(self, _r):
-        """ Compute the acceleration at a given position due to the 
-            collection of particles. Inputs are arrays, and assumes they 
+        """ Compute the acceleration at a given position due to the
+            collection of particles. Inputs are arrays, and assumes they
             are in the correct system of units.
         """
         if _r.ndim == 1:
             rr = _r - self._r
-            a = self._G * self._m * rr / np.linalg.norm(rr)**3  
+            a = self._G * self._m * rr / np.linalg.norm(rr)**3
             return np.sum(a, axis=0)
-            
+
         elif _r.ndim == 2:
             # could do: (np.repeat(_r[np.newaxis], self._r.shape[0], axis=0) - \
             #                self._r[...,np.newaxis]).sum(axis=0)
@@ -135,87 +141,87 @@ class Particle(DynamicalBase):
                 rr = _r[ii] - self._r
                 a[ii] = (self._G * self._m * rr / np.sum(rr**2,axis=0)**1.5)\
                         .sum(axis=0)
-        
+
             return a
-            
+
         else:
             raise ValueError()
-    
+
     def plot_r(self, coord_names=['x','y','z'], **kwargs):
-        """ Make a scatter-plot of 3 projections of the positions of the 
+        """ Make a scatter-plot of 3 projections of the positions of the
             particle coordinates.
-            
+
             Parameters
             ----------
             coord_names : list
                 Name of each axis, e.g. ['x','y','z']
             kwargs (optional)
                 Keyword arguments that get passed to triangle.corner()
-        """   
+        """
         from ..plot.data import scatter_plot_matrix
         if not len(coord_names) == self.ndim:
             raise ValueError("Must pass a coordinate name for each dimension.")
-        
+
         labels = [r"{0} [{1}]".format(nm, self.r.unit)
                     for nm in coord_names]
-        
+
         kwargs["ms"] = kwargs.get("ms", 2.)
         kwargs["alpha"] = kwargs.get("alpha", 0.5)
 
-        fig = triangle.corner(self._r, labels=labels, 
-                              plot_contours=False, plot_datapoints=True, 
+        fig = triangle.corner(self._r, labels=labels,
+                              plot_contours=False, plot_datapoints=True,
                               **kwargs)
         return fig
-    
+
     def plot_v(self, coord_names=['vx','vy','vz'], **kwargs):
-        """ Make a scatter-plot of 3 projections of the velocities of the 
+        """ Make a scatter-plot of 3 projections of the velocities of the
             particle coordinates.
-            
+
             Parameters
             ----------
             coord_names : list
                 Name of each axis, e.g. ['Vx','Vy','Vz']
             kwargs (optional)
                 Keyword arguments that get passed to triangle.corner()
-        """   
+        """
         from ..plot.data import scatter_plot_matrix
         assert len(coord_names) == self.ndim, "Must pass a coordinate name for each dimension."
-        
+
         labels = [r"{0} [{1}]".format(nm, self.v.unit)
                     for nm in coord_names]
-        
+
         kwargs["ms"] = kwargs.get("ms", 2.)
         kwargs["alpha"] = kwargs.get("alpha", 0.5)
 
-        fig = triangle.corner(self._v, labels=labels, 
-                              plot_contours=False, plot_datapoints=True, 
+        fig = triangle.corner(self._v, labels=labels,
+                              plot_contours=False, plot_datapoints=True,
                               **kwargs)
         return fig
-    
+
     def merge(self, other):
         """ Merge two particle collections. Takes unit system from the first
             Particle object.
         """
-        
+
         if not isinstance(other, Particle):
             raise TypeError("Can only merge two Particle objects!")
-        
+
         other_r = other.r.decompose(self.units).value
         other_v = other.v.decompose(self.units).value
         other_m = other.m.decompose(self.units).value
-        
+
         r = np.vstack((self._r,other_r)) * self.r.unit
         v = np.vstack((self._v,other_v)) * self.v.unit
         m = np.append(self._m,other_m) * self.m.unit
-        
+
         return Particle(r=r, v=v, m=m, units=self.units)
-    
+
     def __getitem__(self, key):
         r = self.r[key]
         v = self.v[key]
         m = self.m[key]
-        
+
         return Particle(r=r, v=v, m=m, units=self.units)
-    
+
     def __len__(self):
         return self.nparticles
--- streams/inference/back_integrate.py
+++ streams/inference/back_integrate.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
-""" Contains likelihood function specific to back-integration and 
-    the Rewinder 
+""" Contains likelihood function specific to back-integration and
+    the Rewinder
 """
 
 from __future__ import division, print_function
@@ -29,25 +29,25 @@ def _gc_to_hel(gc):
     """
     Rsun = 8. # kpc
     Vcirc = 0.224996676312 # kpc / Myr
-    
+
     x,y,z,vx,vy,vz = gc.T
 
     # transform to heliocentric cartesian
     x += Rsun
     vy -= Vcirc
-    
+
     # transform from cartesian to spherical
     d = np.sqrt(x**2 + y**2 + z**2)
     l = np.arctan2(y, x)
     b = np.pi/2. - np.arccos(z/d)
-    
+
     # transform cartesian velocity to spherical
     d_xy = np.sqrt(x**2 + y**2)
-    vr = (x*vx + y*vy + z*vz) / d # velocity 
+    vr = (x*vx + y*vy + z*vz) / d # velocity
 
     mu_l = -(vx*y - x*vy) / d_xy**2 # rad/Myr
     mu_b = -(z*(x*vx + y*vy) - d_xy**2*vz) / (d**2 * d_xy) # rad/Myr
-    
+
     hel = np.zeros_like(gc).T
     for ii,col in enumerate([l,b,d,mu_l,mu_b,vr]):
         hel[ii] = col
@@ -69,71 +69,71 @@ def _hel_to_gc(hel):
     x = d*np.cos(b)*np.cos(l)
     y = d*np.cos(b)*np.sin(l)
     z = d*np.sin(b)
-    
+
     # transform spherical velocity to cartesian
     omega_l = -mul
     omega_b = -mub
-    
+
     vx = x/d*vr + y*omega_l + z*np.cos(l)*omega_b
     vy = y/d*vr - x*omega_l + z*np.sin(l)*omega_b
     vz = z/d*vr - d*np.cos(b)*omega_b
-    
+
     x -= Rsun
     vy += Vcirc
-    
+
     gc = np.zeros_like(hel).T
     for ii,col in enumerate([x,y,z,vx,vy,vz]):
         gc[ii] = col
 
     return gc.T
 
-def back_integrate_likelihood(p, potential_params, satellite, 
-                              data, data_errors, 
+def back_integrate_likelihood(p, potential_params, satellite,
+                              data, data_errors,
                               Potential, t1, t2):
-    """ This is a simplified version of the likelihood laid out by D. Hogg in 
+    """ This is a simplified version of the likelihood laid out by D. Hogg in
         Bread and Butter (https://github.com/davidwhogg/BreadAndButter/). The
-        stars are assumed to come from a Gaussian progenitor, described by 
+        stars are assumed to come from a Gaussian progenitor, described by
         just two scales -- the tidal radius and velocity dispersion.
     """
 
-    # First need to pull apart the parameters p -- first few are the 
+    # First need to pull apart the parameters p -- first few are the
     #   potential parameters, then the true position of the stars, then
     #   the time the stars came unbound from their progenitor.
     Nparticles,Ndim = data.shape
     Nparams = len(potential_params)
-    dt = -1. 
+    dt = -1.
 
-    # Use the specified Potential class and parameters 
+    # Use the specified Potential class and parameters
     potential_params = dict(zip(potential_params, p[:Nparams]))
     potential = Potential(**potential_params)
-    
-    # These are the true positions/velocities of the particles, which we 
+
+    # These are the true positions/velocities of the particles, which we
     #   add as parameters in the model
     x = np.array(p[Nparams:Nparams+(Nparticles*6)]).reshape(Nparticles,6)
     hel = _gc_to_hel(x)
 
     # These are the unbinding times for each particle
     t_idx = [int(pp) for pp in p[Nparams+(Nparticles*6):]]
-    
+
     # A Particle object for the true positions of the particles -- not great...
     particles = Particle(x[:,:3]*u.kpc, x[:,3:]*u.kpc/u.Myr, 0.*u.M_sun)
-    
+
     acc = np.zeros((Nparticles+1,3))
-    s,p = satellite_particles_integrate(satellite, particles, potential, 
-                                        potential_args=(Nparticles+1, acc), 
+    s,p = satellite_particles_integrate(satellite, particles, potential,
+                                        potential_args=(Nparticles+1, acc),
                                         time_spec=dict(t1=t1, t2=t2, dt=dt))
-    
-    Ntimesteps  = p._x.shape[0]
-    
+
+    Ntimesteps  = p._X.shape[0]
+
     sat_var = np.zeros((Ntimesteps,6))
     sat_var[:,:3] = potential._tidal_radius(satellite._m, s._r) * 1.26
     sat_var[:,3:] += 0.0083972030362941957 #v_disp # kpc/Myr for 2.5E7
     cov = sat_var**2
 
     Sigma = np.array([cov[jj] for ii,jj in enumerate(t_idx)])
-    p_x = np.array([p._x[jj,ii] for ii,jj in enumerate(t_idx)])
-    s_x = np.array([s._x[jj,0] for ii,jj in enumerate(t_idx)])
-    log_p_x_given_phi = -0.5*np.sum(-2.*np.log(Sigma) + 
+    p_x = np.array([p._X[jj,ii] for ii,jj in enumerate(t_idx)])
+    s_x = np.array([s._X[jj,0] for ii,jj in enumerate(t_idx)])
+    log_p_x_given_phi = -0.5*np.sum(-2.*np.log(Sigma) +
                         (p_x-s_x)**2/Sigma, axis=1) * abs(dt)
 
     log_p_D_given_x = -0.5*np.sum(-2.*np.log(data_errors) + \
@@ -141,7 +141,7 @@ def back_integrate_likelihood(p, potential_params, satellite,
 
     return np.sum(log_p_D_given_x + log_p_x_given_phi)
 
-def variance_likelihood(p, param_names, satellite, particles, 
+def variance_likelihood(p, param_names, satellite, particles,
                         Potential, t1, t2):
     """ Evaluate the TODO """
 
@@ -154,5 +154,5 @@ def variance_likelihood(p, param_names, satellite, particles,
                                                      potential,
                                                      potential_args=(Nparticles+1, acc), \
                                                      time_spec=dict(t1=t1, t2=t2, dt=-1.))
-    
+
     return -generalized_variance(lm10, s_orbit, p_orbits)
