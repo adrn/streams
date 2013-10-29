538db6e40f4397a8ae24a0f251e7f3fa085d9d80
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-10-29 14:35:18 -0400

    hand id lines working

--- streams/reduction/observing.py
+++ streams/reduction/observing.py
@@ -125,7 +125,7 @@ class CCDRegion(list):
         super(CCDRegion, self).__init__(*slices)
 
 
-_line_colors = ["red", "green", "blue", "yellow", "magenta", "cyan"]
+_line_colors = ["red", "green", "blue", "magenta", "cyan", "yellow"]
 class ObservingRun(object):
 
     def __init__(self, path, ccd, data_path=None, redux_path=None):
@@ -186,9 +186,10 @@ class ObservingRun(object):
             overwrite : bool (optional)
                 Overwrite the cache file or not.
         """
+        plt.switch_backend("agg")
 
         narcs = int(narcs)
-        cache_file = os.path.join(self.data_path, "master_arc.json")
+        cache_file = os.path.join(self.redux_path, "arc", "master_arc.json")
 
         if os.path.exists(cache_file) and overwrite:
             os.remove(cache_file)
@@ -222,14 +223,14 @@ class ObservingRun(object):
 
             with open(cache_file, "w") as f:
                 s = dict()
-                s["pixels"] = pix.tolist()
+                s["pixel"] = pix.tolist()
                 s["arc"] = median_comp_1d.tolist()
                 f.write(json.dumps(s))
 
         with open(cache_file) as f:
             s = json.loads(f.read())
 
-        return s["pixels"], s["arc"]
+        return s["pixel"], s["arc"]
 
     def hand_id_lines(self, night, Nlines=4, overwrite=False):
         """ Have the user identify some number of lines by hand.
@@ -242,6 +243,8 @@ class ObservingRun(object):
             overwrite : bool (optional)
                 Overwrite the cache file or not.
         """
+        plt.switch_backend("agg")
+
         # TODO: could use a matplotlib color scaler...
         if Nlines > len(_line_colors):
             raise ValueError("Use a max of {0} lines."\
@@ -250,71 +253,86 @@ class ObservingRun(object):
         # TODO: maybe instead, make_master_arc makes self.pix, self.arc ?
         pix, arc = self.make_master_arc(night)
 
-        # used to split the spectrum into Nlines sections, finds the brightest
-        #   line in each section and asks the user to identify it
-        sub_div = len(arc) // Nlines
+        cache_file = os.path.join(self.redux_path, "arc", "hand_id.json")
 
-        # TODO: When matplotlib has a TextBox widget, or a dropdown, let the
-        #   user (me) identify the line on the plot
-        fig = plt.figure(figsize=(16,12))
-        gs = GridSpec(2, Nlines)
+        if os.path.exists(cache_file) and overwrite:
+            os.remove(cache_file)
 
-        # top plot is just the full
-        top_ax = plt.subplot(gs[0,:])
-        plot_spectrum(pix, arc, ax=top_ax)
-        top_ax.set_ylabel("Raw counts")
+        if not os.path.exists(cache_file):
+            # used to split the spectrum into Nlines sections, finds the brightest
+            #   line in each section and asks the user to identify it
+            sub_div = len(arc) // Nlines
 
-        line_centers = []
-        for ii in range(Nlines):
-            color = _line_colors[ii]
+            # TODO: When matplotlib has a TextBox widget, or a dropdown, let the
+            #   user (me) identify the line on the plot
+            fig = plt.figure(figsize=(16,12))
+            gs = GridSpec(2, Nlines)
 
-            # max pixel index to be center of gaussian fit
-            c_idx = np.argmax(arc[ii*sub_div:(ii+1)*sub_div])
-            c_idx += sub_div*ii
+            # top plot is just the full
+            top_ax = plt.subplot(gs[0,:])
+            plot_spectrum(pix, arc, ax=top_ax)
+            top_ax.set_ylabel("Raw counts")
 
-            try:
-                line_data = arc[c_idx-5:c_idx+5]
-                line_pix = pix[c_idx-5:c_idx+5]
-            except IndexError:
-                logger.debug("max value near edge of ccd...weird.")
-                continue
+            line_centers = []
+            for ii in range(Nlines):
+                color = _line_colors[ii]
 
-            g = gaussian_fit(line_pix, line_data)
-            print(g.mean)
-            return
-            line_centers.append(g.mean)
+                # max pixel index to be center of gaussian fit
+                c_idx = np.argmax(arc[ii*sub_div:(ii+1)*sub_div])
+                c_idx += sub_div*ii
 
-            top_ax.plot(line_pix, model_line, \
-                         drawstyle="steps", color=color, lw=2.)
+                try:
+                    line_data = arc[c_idx-5:c_idx+5]
+                    line_pix = pix[c_idx-5:c_idx+5]
+                except IndexError:
+                    logger.debug("max value near edge of ccd...weird.")
+                    continue
 
-            bottom_ax = plt.subplot(gs[1,ii])
-            bottom_ax.plot(pix, arc, drawstyle="steps")
-            bottom_ax.plot(line_pix, model_line, \
-                           drawstyle="steps", color=color, lw=2.)
-            bottom_ax.set_xlim(c_idx-10, c_idx+10)
-            bottom_ax.set_xlabel("Pixel")
-            if ii == 0:
-                bottom_ax.set_ylabel("Raw counts")
-            else:
-                bottom_ax.yaxis.set_visible(False)
+                g = gaussian_fit(line_pix, line_data)
+                line_centers.append(g.mean)
 
-        line_id_file = os.path.join(self.redux_path, "plots", "line_id.png")
-        fig.savefig(line_id_file)
+                top_ax.plot(line_pix, g(line_pix), \
+                             drawstyle="steps", color=color, lw=2.)
 
-        print("")
-        print("Now open: {0}".format(line_id_file))
-        print("Identify the colored lines. Default unit is angstrom, but ")
-        print("you can input values with units, e.g., 162.124 nanometer.")
-        print("")
+                bottom_ax = plt.subplot(gs[1,ii])
+                bottom_ax.plot(pix, arc, drawstyle="steps")
+                bottom_ax.plot(line_pix, g(line_pix), \
+                               drawstyle="steps", color=color, lw=2.)
+                bottom_ax.set_xlim(c_idx-10, c_idx+10)
+                bottom_ax.set_xlabel("Pixel")
+                if ii == 0:
+                    bottom_ax.set_ylabel("Raw counts")
+                else:
+                    bottom_ax.yaxis.set_visible(False)
 
-        line_wavelengths = []
-        for ii,color in enumerate(_line_colors[:Nlines]):
-            wvln = raw_input("\t Line {0} ({1} line) wavelength: ".format(ii,
-                                                                          color))
-            wvln = parse_wavelength(wvln)
-            line_wavelengths.append(wvln.to(u.angstrom).value)
+            line_id_file = os.path.join(self.redux_path, "plots",
+                                        "line_id.pdf")
+            fig.savefig(line_id_file)
+            plt.close()
 
-        return line_centers, line_wavelengths
+            print("")
+            print("Now open: {0}".format(line_id_file))
+            print("Identify the colored lines. Default unit is angstrom, but ")
+            print("you can input values with units, e.g., 162.124 nanometer.")
+            print("")
+
+            line_wavelengths = []
+            for ii,color in enumerate(_line_colors[:Nlines]):
+                wvln = raw_input("\t Line {0} ({1} line) wavelength: "\
+                                 .format(ii, color))
+                wvln = parse_wavelength(wvln)
+                line_wavelengths.append(wvln.to(u.angstrom).value)
+
+            with open(cache_file, "w") as f:
+                s = dict()
+                s["pixel"] = pix.tolist()
+                s["wavelength"] = median_comp_1d.tolist()
+                f.write(json.dumps(s))
+
+        with open(cache_file) as f:
+            s = json.loads(f.read())
+
+        return s["pixel"], s["wavelength"]
 
 
 class ObservingNight(object):
