aed47a7193ef2ba99e0194ed0b7536fd5c656b7b
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-11-17 21:41:18 -0500

    started writing tests for error model

---
+++ streams/observation/tests/test_error_model.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+"""
+    Test error models
+"""
+
+from __future__ import absolute_import, unicode_literals, division, print_function
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Standard library
+import os, sys
+
+# Third-party
+import astropy.units as u
+import numpy as np
+import pytest
+
+from ..error_model import *
+
+def test_error_model():
+    pass
+
+def test_spitzer_gaia():
