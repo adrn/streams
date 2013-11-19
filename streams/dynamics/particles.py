hmm logic fo units is wrong

streams/dynamics/particles.py
@@ -49,7 +49,7 @@ class Particle(object):
                 Any additional metadata.
         """
 
-        if isinstance(q, np.ndarray):
+        if isinstance(q, np.ndarray): # ndarray or Quantity! TODO: figure out this logic biatch
             if q.ndim == 1:
                 q = np.atleast_2d(q).T
 
@@ -82,6 +82,7 @@ class Particle(object):
         if len(names) != ndim:
             raise ValueError("Must specify coordinate name for each "
                              "dimension.")
+        self.names = names
 
         self._X = np.zeros((ndim,) + q[0].shape)
         for ii in range(ndim):
@@ -91,9 +92,6 @@ class Particle(object):
                 qq = q[ii]
 
             self._X[ii] = qq.to(self._internal_units[ii]).value
-
-        # TODO: descriptors to access
-        #   (self._X[ii]*self._internal_units[ii]).to(self._repr_units[ii)
 
     def __getitem__(self, slc):
 

streams/dynamics/tests/test_particles.py
@@ -32,7 +32,8 @@ def test_init():
     p = Particle(q=(x, vx), names=("x","vx"),
                  units=[u.kpc, u.km/u.s])
     p = Particle(q=(x*u.kpc, vx*u.km/u.s), names=("x","vx"))
-    assert p["x"].value == x
+    assert np.all(p["x"].value == x)
+    assert np.all(p["vx"].value == vx)
 
     x_vx = np.random.random(size=(2,100))
     p = Particle(q=x_vx, names=("x","vx"),