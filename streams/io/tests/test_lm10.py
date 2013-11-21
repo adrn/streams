38a432ce0e7e0e48229764c9c7b33e127d658fe0
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-11-21 16:01:13 -0500

    correct units

--- streams/io/tests/test_lm10.py
+++ streams/io/tests/test_lm10.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pytest
 
+from ... import usys
 from ..lm10 import LM10Simulation
 
 plot_path = "plots/tests/io/lm10"
@@ -24,7 +25,9 @@ if not os.path.exists(plot_path):
 
 lm10 = LM10Simulation()
 particles = lm10.particles(expr="Pcol==-1")
+particles = particles.decompose(usys)
 satellite = lm10.satellite()
+satellite = satellite.decompose(usys)
 
 # from email
 law_r = [19.0149, 2.64883, -6.8686]
@@ -35,24 +38,39 @@ def test_position():
     s_kwargs = dict(marker='o', linestyle='none', color='r', alpha=0.75)
     l_kwargs = dict(marker='o', linestyle='none', color='g', alpha=0.75)
 
-    fig,axes = plt.subplots(2, 2, sharex=True, sharey=True, figsize=(10,10))
+    #fig,axes = plt.subplots(2, 2, sharex=True, sharey=True, figsize=(10,10))
+    fig,axes = plt.subplots(2, 2, figsize=(10,10))
     axes[0,1].set_visible(False)
 
-    axes[0,0].plot(particles["x"].value, particles["y"].value, **p_kwargs)
+    axes[0,0].plot(particles["x"].value, particles["y"].value,
+                   label="all particles", **p_kwargs)
     axes[1,0].plot(particles["x"].value, particles["z"].value, **p_kwargs)
     axes[1,1].plot(particles["y"].value, particles["z"].value, **p_kwargs)
 
-    axes[0,0].plot(satellite["x"].value, satellite["y"].value, **s_kwargs)
-    axes[1,0].plot(satellite["x"].value, satellite["z"].value, **s_kwargs)
-    axes[1,1].plot(satellite["y"].value, satellite["z"].value, **s_kwargs)
+    axes[0,0].plot(satellite["x"].decompose(usys).value,
+                   satellite["y"].decompose(usys).value,
+                   label="Satellite", **s_kwargs)
+    axes[1,0].plot(satellite["x"].decompose(usys).value,
+                   satellite["z"].decompose(usys).value,
+                   **s_kwargs)
+    axes[1,1].plot(satellite["y"].decompose(usys).value,
+                   satellite["z"].decompose(usys).value,
+                   **s_kwargs)
 
-    axes[0,0].plot(law_r[0], law_r[1], **l_kwargs)
+    axes[0,0].plot(law_r[0], law_r[1], label="Law", **l_kwargs)
     axes[1,0].plot(law_r[0], law_r[2], **l_kwargs)
     axes[1,1].plot(law_r[1], law_r[2], **l_kwargs)
 
-    axes[0,0].set_xlim(-25,25)
-    axes[0,0].set_ylim(-25,25)
+    axes[0,0].set_xlim(law_r[0]-1, law_r[0]+1)
+    axes[0,0].set_ylim(law_r[1]-1, law_r[1]+1)
 
+    axes[1,0].set_xlim(law_r[0]-1, law_r[0]+1)
+    axes[1,0].set_ylim(law_r[2]-1, law_r[2]+1)
+
+    axes[1,1].set_xlim(law_r[1]-1, law_r[1]+1)
+    axes[1,1].set_ylim(law_r[2]-1, law_r[2]+1)
+
+    fig.subplots_adjust(hspace=0.02,wspace=0.02)
     fig.savefig(os.path.join(plot_path, "sat_ptcl_positions.png"))
 
 def test_velocity():
