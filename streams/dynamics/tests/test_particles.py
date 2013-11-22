9df48e44e91e9d05c494a0be1d107cf3db2c4861
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-11-22 10:36:09 -0500

    remove old observe test

--- streams/dynamics/tests/test_particles.py
+++ streams/dynamics/tests/test_particles.py
@@ -137,27 +137,3 @@ def test_to_frame():
     p = p.to_frame('galactocentric')
     fig = p.plot(ms=3.)
     fig.savefig(os.path.join(plot_path, "lm10_particle_6d_galacto.png"))
-
-'''
-def test_observe():
-
-    # from LM10 -- copied here so I don't have to read in each time...
-    r = np.array([[-24.7335, -9.35652, 17.7755], [4.04449, -8.58018, 43.6431], [40.7303, -9.38172, -52.45], [31.9827, -0.46618, 3.68191], [-55.4545, 3.40444, -14.4413], [-24.9119, -4.18854, 16.2446], [7.63141, 5.4841, 42.3225], [47.9773, -5.17874, -44.0926], [24.7687, 1.91995, -38.9927], [35.3565, 15.0336, -55.0006]]) * u.kpc
-    v = np.array([[-0.26623800000000003, -0.013696999999999999, -0.00862857], [-0.147417, -0.00453593, -0.0570219], [0.10542100000000001, 0.0255395, 0.0676476], [-0.199363, -0.00786105, 0.16698300000000002], [0.10564799999999999, 0.00946331, -0.0706881], [-0.0358232, 0.0333882, -0.20689400000000002], [-0.238398, -0.02333, -0.0920642], [0.0372335, 0.014637, 0.116505], [0.13775, 0.0183352, 0.0059355599999999994], [0.0649749, -0.018231400000000002, 0.12492600000000001]]) * u.kpc/u.Myr
-
-    p = Particle(r=r, v=v, m=1*u.M_sun)
-
-    for factor in [0.01, 0.1, 1., 10.]:
-        error_model = SpitzerGaiaErrorModel(units=usys, factor=factor)
-        new_p = p.observe(error_model)
-
-        fig = p.plot_r()
-        new_p.plot_r(fig=fig, color="r")
-        fig.savefig(os.path.join(plot_path,
-                    "particle_observed_r_fac{0}.png".format(factor)))
-
-        fig = p.plot_v()
-        new_p.plot_v(fig=fig, color="r")
-        fig.savefig(os.path.join(plot_path,
-                    "particle_observed_v_fac{0}.png".format(factor)))
-'''
