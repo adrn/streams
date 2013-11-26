4e162ca5698b7b855a9c940856305dcb04217729
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-11-25 23:54:20 -0500

    get pool AFTER reading all

--- scripts/new_infer_potential.py
+++ scripts/new_infer_potential.py
@@ -111,25 +111,6 @@ def main(config_file, job_name=None):
         config = yaml.load(f.read())
 
     # TODO: write a separate script just for plotting...
-    # get a pool object given the configuration parameters
-    #pool = get_pool(config)
-    if config.get("mpi", False):
-        # Initialize the MPI pool
-        pool = MPIPool()
-
-        # Make sure the thread we're running on is the master
-        if not pool.is_master():
-            pool.wait()
-            sys.exit(0)
-        logger.debug("Running with MPI.")
-
-    elif config.get("threads", 0) > 1:
-        logger.debug("Running with multiprocessing on {} cores."\
-                    .format(config["threads"]))
-        pool = multiprocessing.Pool(config["threads"])
-    else:
-        logger.debug("Running serial...parallel panda is sad :(")
-        pool = None
 
     # determine the output data path
     path = make_path(config)
@@ -313,6 +294,9 @@ def main(config_file, job_name=None):
 
     # sample starting points for the walkers from the prior
     p0 = model.sample(size=Nwalkers)
+
+    # get a pool object given the configuration parameters
+    pool = get_pool(config)
 
     if not os.path.exists(chain_file):
         logger.debug("Initializing sampler...")
