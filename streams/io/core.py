fix units

streams/io/core.py
@@ -98,8 +98,9 @@ class SimulationData(object):
         #   bound particle positions/velocities
         bound = self.table(bound_expr)
         cols = _tbl_to_quantity_list(bound, column_names)
-        return Particle([np.median(np.array(c, copy=True)) for c in cols],
+        return Particle([np.median(np.array(c.value)) for c in cols],
                         names=column_names,
+                        units=[c.unit for c in cols],
                         meta=dict(expr=bound_expr))
 
     def particles(self, N=None, expr=None, frame="galactocentric",