fbb74ab5d91325806c921dcae93dfb541d856b53
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-01-26 22:25:12 -0500

    make plots vs mass

--- scripts/figures/paper2_figures.py
+++ scripts/figures/paper2_figures.py
@@ -23,8 +23,12 @@ from matplotlib.gridspec import GridSpec
 from matplotlib import rc_context, rcParams, cm
 from matplotlib.patches import Rectangle, Ellipse
 import scipy.optimize as so
+from scipy.stats import norm
 
 from streams.util import project_root
+from streams.io.sgr import SgrSimulation
+from streams.integrate import LeapfrogIntegrator
+from streams.potential.lm10 import LawMajewski2010
 
 matplotlib.rc('xtick', labelsize=18)
 matplotlib.rc('ytick', labelsize=18)
@@ -71,6 +75,113 @@ def graphical_model(**kwargs):
     #pgm.figure.savefig("classic.pdf")
     pgm.figure.savefig(filename, dpi=150)
 
+def gaussian(x, mu, sigma):
+    return 1./np.sqrt(2*np.pi*sigma**2) * np.exp(-0.5 * ((x-mu)/sigma)**2)
+
+def rel_dist(**kwargs):
+
+    nbins = 50
+    fig,axes = plt.subplots(4,2,figsize=(8.5,11), sharex='col', sharey=True)
+
+    #_m = kwargs["mass"]
+    for kk,_m in enumerate(["2.5e6","2.5e7","2.5e8","2.5e9"]):
+        m = float(_m)
+        sgr = SgrSimulation(_m)
+        p = sgr.particles(N=5000, expr="tub!=0")
+        p_bound = sgr.particles(N=0, expr="tub==0")
+        v_disp = np.sqrt(np.var(p_bound["vx"]) + \
+                         np.var(p_bound["vy"]) + \
+                         np.var(p_bound["vz"])).value[0]
+        s = sgr.satellite()
+
+        lm10_potential = LawMajewski2010()
+
+        X = np.vstack((s._X[...,:3], p._X[...,:3].copy()))
+        V = np.vstack((s._X[...,3:], p._X[...,3:].copy()))
+        integrator = LeapfrogIntegrator(lm10_potential._acceleration_at,
+                                        np.array(X), np.array(V),
+                                        args=(X.shape[0], np.zeros_like(X)))
+        ts, rs, vs = integrator.run(t1=sgr.t1, t2=sgr.t2, dt=-1.)
+
+        s_orbit = np.vstack((rs[:,0][:,np.newaxis].T, vs[:,0][:,np.newaxis].T)).T
+        p_orbits = np.vstack((rs[:,1:].T, vs[:,1:].T)).T
+        t_idx = np.array([np.argmin(np.fabs(ts - t)) for t in p.tub])
+
+        p_x = np.array([p_orbits[jj,ii] for ii,jj in enumerate(t_idx)])
+        s_x = np.array([s_orbit[jj,0] for jj in t_idx])
+
+        diff = p_x-s_x
+        rel_r = np.sqrt(np.sum(diff[:,:3]**2, axis=-1))
+        rel_v = np.sqrt(np.sum(diff[:,3:]**2, axis=-1))
+
+        r_tide = lm10_potential._tidal_radius(m, s_x)*1.6
+        v_esc = lm10_potential._escape_velocity(m, r_tide)
+
+        mu_r, sig_r = norm.fit(np.log(rel_r))
+        print("effective disruption radius: {}".format(mu_r))
+        print("\t\tspread: {}".format(sig_r))
+        print("\t\tmean tidal radius: {}".format(np.log(r_tide.mean())))
+
+        mu_v, sig_v = norm.fit(np.log(rel_v))
+        print("effective disruption velocity: {}".format(mu_v))
+        print("\t\tspread: {}".format(sig_v))
+        print("\t\tvelocity dispersion: {}".format(np.log(v_disp)))
+        print("\n")
+
+        n,bins,patches = axes[kk,0].hist(np.log(rel_r), bins=nbins,
+                                         alpha=0.4, normed=True)
+        axes[kk,0].plot(bins, gaussian(bins, mu_r, sig_r), lw=2., alpha=0.75)
+        axes[kk,0].axvline(np.log(r_tide.mean()), color='#ca0020', lw=2.,
+                        alpha=0.75, label=r'$\ln \langle R_{\rm tide}\rangle$')
+        axes[kk,0].set_yticks([])
+
+        ylim = axes[kk,0].get_ylim()
+        axes[kk,0].text(-2.75, ylim[1]-(ylim[1]-ylim[0])/5.,
+                        r"{} $M_\odot$".format(_m),
+                        fontsize=16)
+
+        axes[kk,0].text(-2.75, ylim[1]-1.75*(ylim[1]-ylim[0])/5.,
+                        r"$\sigma={:.2f}$".format(sig_r),
+                        fontsize=14)
+
+        ylim = axes[kk,1].get_ylim()
+        axes[kk,1].text(-8.75, ylim[1]-1.75*(ylim[1]-ylim[0])/5.,
+                        r"$\sigma={:.2f}$".format(sig_v),
+                        fontsize=14)
+
+        n,bins,patches = axes[kk,1].hist(np.log(rel_v), bins=nbins,
+                                         alpha=0.4, normed=True)
+        axes[kk,1].plot(bins, gaussian(bins, mu_v, sig_v), lw=2., alpha=0.75)
+        axes[kk,1].axvline(np.log(v_disp), color='#2ca25f', lw=2.,
+                        alpha=0.75, label=r'$\ln \langle \sigma_v\rangle$')
+        axes[kk,1].set_yticks([])
+
+        if kk == 0:
+            axes[kk,0].set_title("relative distances")
+            axes[kk,1].set_title("relative velocities")
+            axes[kk,0].legend(fontsize=12)
+            axes[kk,1].legend(fontsize=12)
+
+        if kk == 3:
+            axes[kk,0].set_xlabel(r"$\ln\vert r_i-r_{\rm sat} \vert$")
+            axes[kk,1].set_xlabel(r"$\ln\vert v_i-v_{\rm sat} \vert$")
+
+    plt.tight_layout()
+    fig.savefig(os.path.join(plot_path, "rel_dists.pdf"))
+
+    # fig,axes = plt.subplots(1,2,figsize=(16,8))
+    # n,bins,patches = axes[0].hist((np.log(rel_r)-np.log(r_tide)),
+    #                               bins=nbins, alpha=0.4, normed=True)
+    # axes[0].set_title("normed distance distribution")
+    # axes[0].set_yticks([])
+
+    # n,bins,patches = axes[1].hist((np.log(rel_v)-np.log(v_disp)),
+    #                               bins=nbins, alpha=0.4, normed=True)
+    # axes[1].set_title("normed velocity distribution")
+    # axes[1].set_yticks([])
+
+    # plt.tight_layout()
+    # fig.savefig(os.path.join(plot_path, "{}_norm_dists.pdf".format(_m)))
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
