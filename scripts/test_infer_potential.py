902739bca974614951a724d80e5d5e92e7677f87
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-02-07 15:02:46 -0500

    test case

--- scripts/test_infer_potential.py
+++ scripts/test_infer_potential.py
@@ -14,6 +14,7 @@ import time
 
 # Third-party
 import astropy.units as u
+from astropy.utils import isiterable
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
@@ -25,6 +26,7 @@ import streams.io as io
 from streams.io.sgr import SgrSimulation
 import streams.inference as si
 import streams.potential as sp
+from streams.util import project_root
 
 # Create logger
 logger = logging.getLogger(__name__)
@@ -32,9 +34,15 @@ logging.basicConfig(level=logging.DEBUG)
 
 minimum_config = """
 name: test
-data_file: data/observed_particles/N1024.hdf5
+data_file: data/observed_particles/2.5e8_N1024.hdf5
+# nparticles: 4
+# particle_idx: [217, 276, 358, 470]
 nparticles: 4
-particle_idx: [0, 1, 512, 513]
+particle_idx: [77, 132, 658, 847]
+# nparticles: 8
+# particle_idx: [25, 77, 132, 140, 359, 658, 693, 847]
+# nparticles: 32
+# particle_idx: [28, 55, 65, 142, 144, 154, 243, 277, 339, 362, 374, 376, 395, 396, 434, 439, 464, 467, 467, 536, 536, 553, 584, 706, 715, 730, 734, 734, 822, 852, 862, 939]
 
 potential:
     class_name: LawMajewski2010
@@ -51,10 +59,10 @@ pot_params = """
     parameters:
         q1:
             a: 1.
-            b: 1.7
+            b: 1.6
         qz:
             a: 1.
-            b: 1.7
+            b: 1.6
         phi:
             a: 80. degree
             b: 120. degree
@@ -65,7 +73,7 @@ pot_params = """
 
 ptc_params = """
     parameters:
-        _X:
+#        _X:
         tub:
 """
 
@@ -78,52 +86,68 @@ lm10_c = minimum_config.format(potential_params=pot_params,
                                particles_params="",
                                satellite_params="")
 
-_configs = dict()
-_configs[0] = minimum_config.format(potential_params=pot_params,
-                                    particles_params="",
-                                    satellite_params="")
-# _configs[1] = minimum_config.format(potential_params="",
-#                                     particles_params=ptc_params,
-#                                     satellite_params="")
-# _configs.append(minimum_config.format(potential_params="",
-#                                       particles_params="",
-#                                       satellite_params=sat_params))
-# _configs.append(minimum_config.format(potential_params=pot_params,
-#                                       particles_params=ptc_params,
-#                                       satellite_params=""))
-# _configs.append(minimum_config.format(potential_params=pot_params,
-#                                       particles_params="",
-#                                       satellite_params=sat_params))
-# _configs.append(minimum_config.format(potential_params=pot_params,
-#                                       particles_params=ptc_params,
-#                                       satellite_params=sat_params))
+_config = minimum_config.format(potential_params=pot_params,
+                                particles_params=ptc_params,
+                                satellite_params=sat_params)
+# particles_params=ptc_params,
+# satellite_params=sat_params
+
+output_path = os.path.join(project_root, 'plots', 'tests', 'infer_potential')
+if not os.path.exists(output_path):
+    os.mkdir(output_path)
+
+def make_plot(model, idx, vals1, vals2):
+    fig,axes = plt.subplots(2,2,figsize=(12,12),sharex='col')
+
+    p = model.truths.copy()
+    Ls = []
+    for val in vals1:
+        p[idx] = val
+        Ls.append(model(p))
+    axes[0,0].plot(vals1, Ls)
+    axes[0,0].set_ylabel("$\ln\mathcal{L}$")
+    axes[1,0].plot(vals1, np.exp(Ls-np.max(Ls)))
+    axes[1,0].set_ylabel("$\mathcal{L}$")
+
+    p = model.truths.copy()
+    Ls = []
+    for val in vals2:
+        p[idx] = val
+        Ls.append(model(p))
+
+    #logger.debug("{} vs truth {}".format(vals[np.argmax(Ls)], truth))
+    #logger.debug("{:.2f}% off".format(abs(vals[np.argmax(Ls)] - truth)/truth*100.))
+
+    axes[0,1].set_title("zoomed")
+    axes[0,1].plot(vals2, Ls)
+    axes[1,1].plot(vals2, np.exp(Ls-np.max(Ls)))
+    for ax in axes.flat:
+        ax.axvline(model.truths[idx])
+
+    return fig
 
 class TestStreamModel(object):
 
     def setup(self):
-        self.configs = dict([(key,io.read_config(_configs[key])) for key in _configs.keys()])
+        config = io.read_config(_config)
+        self.model = si.StreamModel.from_config(config)
 
     def test_simple(self):
 
-        for ii,c in self.configs.items():
-            print()
-            logger.info("Testing config {}".format(ii+1))
-            model = si.StreamModel.from_config(c)
+        # make sure true posterior value is higher than any randomly sampled value
+        logger.debug("Checking posterior values...")
+        true_ln_p = self.model.ln_posterior(self.model.truths, *self.model.lnpargs)
+        true_ln_p2 = self.model(self.model.truths)
+        logger.debug("\t\t At truth: {}".format(true_ln_p))
 
-            # make sure true posterior value is higher than any randomly sampled value
-            logger.debug("Checking posterior values...")
-            true_ln_p = model.ln_posterior(model.truths, *model.lnpargs)
-            true_ln_p2 = model(model.truths)
-            logger.debug("\t\t At truth: {}".format(true_ln_p))
+        p0 = self.model.sample_priors()
+        ln_p = self.model.ln_posterior(p0, *self.model.lnpargs)
+        ln_p2 = self.model(p0)
+        logger.debug("\t\t At random sample: {}".format(ln_p))
 
-            p0 = model.sample_priors()
-            ln_p = model.ln_posterior(p0, *model.lnpargs)
-            ln_p2 = model(p0)
-            logger.debug("\t\t At random sample: {}".format(ln_p))
-
-            assert true_ln_p > ln_p
-            assert true_ln_p == true_ln_p2
-            assert ln_p == ln_p2
+        assert true_ln_p > ln_p
+        assert true_ln_p == true_ln_p2
+        assert ln_p == ln_p2
 
     def test_likelihood(self):
 
@@ -135,7 +159,7 @@ class TestStreamModel(object):
         model = si.StreamModel.from_config(c)
         true_potential = model._potential_class(**model._given_potential_params)
 
-        test_path = os.path.join(c['output_path'], "lm10")
+        test_path = os.path.join(output_path, "likelihood")
         if not os.path.exists(test_path):
             os.mkdir(test_path)
 
@@ -156,7 +180,9 @@ class TestStreamModel(object):
                                                     model.lnpargs[2],
                                                     potential, model.true_particles._X,
                                                     model.true_satellite._X,
-                                                    model.true_particles.tub)
+                                                    model.true_particles.tub,
+                                                    model.true_satellite.mass,
+                                                    model.true_satellite.vdisp)
                 Ls.append(ll)
 
             Ls = np.array(Ls)
@@ -191,46 +217,114 @@ class TestStreamModel(object):
     def test_model(self):
         """ Simple test of posterior """
 
-        for ii,c in self.configs.items():
-            print()
-            logger.info("Testing config {}".format(ii+1))
-            model = si.StreamModel.from_config(c)
+        Ncoarse = 51
+        Nfine = 71
 
-            test_path = os.path.join(c['output_path'], "config{}".format(ii))
-            if not os.path.exists(test_path):
-                os.mkdir(test_path)
+        model = self.model
 
-            for jj,truth in enumerate(model.truths):
-                fig,axes = plt.subplots(1,2,figsize=(12,6))
+        test_path = os.path.join(output_path, "model")
+        if not os.path.exists(test_path):
+            os.mkdir(test_path)
 
-                p = model.truths.copy()
-                vals = truth*np.linspace(0.25, 1.75, 121)
-                Ls = []
-                for val in vals:
-                    p[jj] = val
-                    Ls.append(model(p))
-                axes[0].plot(vals, Ls)
+        truth_dict = model._decompose_vector(model.truths)
 
-                p = model.truths.copy()
-                vals = truth*np.linspace(0.9, 1.1, 121)
-                Ls = []
-                for val in vals:
-                    p[jj] = val
-                    Ls.append(model(p))
+        idx = 0
+        for group_name,group in truth_dict.items():
+            for param_name,truths in group.items():
+                print(group_name, param_name)
+                param = model.parameters[group_name][param_name]
 
-                logger.debug("{} vs truth {}".format(vals[np.argmax(Ls)], truth))
-                logger.debug("{:.2f}% off".format(abs(vals[np.argmax(Ls)] - truth)/truth*100.))
+                if group_name == "potential":
+                    vals1 = np.linspace(param._prior.a,
+                                        param._prior.b,
+                                        Ncoarse)
+                    vals2 = np.linspace(0.9,1.1,Nfine)*truths
+                    fig = make_plot(model, idx, vals1, vals2)
+                    fig.savefig(os.path.join(test_path, "{}_{}.png".format(idx,param_name)))
+                    plt.close('all')
+                    idx += 1
 
-                axes[1].plot(vals, Ls)
-                axes[0].axvline(truth)
-                axes[1].axvline(truth)
+                if group_name == "particles":
+                    if param_name == "_X":
+                        for jj in range(param.value.shape[0]):
+                            prior = param._prior[jj]
+                            truth = truths[jj]
 
-                fig.savefig(os.path.join(test_path, "log_test_{}.png".format(jj)))
+                            for kk in range(param.value.shape[1]):
+                                mu,sigma = truth[kk],prior.sigma[kk]
+                                vals1 = np.linspace(mu-10*sigma,
+                                                    mu+10*sigma,
+                                                    Ncoarse)
+                                vals2 = np.linspace(mu-3*sigma,
+                                                    mu+3*sigma,
+                                                    Nfine)
+                                fig = make_plot(model, idx, vals1, vals2)
+                                fig.savefig(os.path.join(test_path,
+                                        "{}_{}.png".format(idx,param_name)))
+                                plt.close('all')
+                                idx += 1
 
-                # plt.clf()
-                # plt.plot(vals, np.exp(Ls))
-                # plt.axvline(truth)
-                # plt.savefig(os.path.join(test_path, "test_{}.png".format(jj)))
+                    elif param_name =="tub":
+                        for jj in range(param.value.shape[0]):
+                            vals1 = np.linspace(param._prior[jj].a,
+                                                param._prior[jj].b,
+                                                Ncoarse)
+                            vals2 = np.linspace(-500,500,Nfine) + truths[jj]
+                            fig = make_plot(model, idx, vals1, vals2)
+                            fig.savefig(os.path.join(test_path, "{}_{}.png".format(idx,param_name)))
+                            plt.close('all')
+                            idx += 1
+
+                    # fig.savefig(os.path.join(test_path, "{}_{}.png".format(param_name,jj)))
+                    # plt.close('all')
+
+    def test_apw(self):
+        c = io.read_config(_configs[3])
+        model = si.StreamModel.from_config(c)
+
+        p = np.array([1.1515709829358736, 1.3061651589219028, 1.6554345936276273, 0.12323310142846752, 3.0522815366139735, -0.57442121586774952, 36.751323505513533, 0.0066999310241279029, -0.0042394488370680371, -0.19422152398249989, -2.1543975690443853, 1.0121210540890484, 38.821645607755585, 0.0010389207842562375, -0.0062815802063840367, -0.009289155208715285, 5306.9338333741944, 1187.7455521689817])
+
+        print(list(model.truths))
+        print(list(model.parameters['particles']['_X']._prior[0].mu) + \
+              list(model.parameters['particles']['_X']._prior[1].mu))
+        print(list(model.parameters['particles']['_X']._prior[0].sigma) + \
+              list(model.parameters['particles']['_X']._prior[1].sigma))
+
+        #print(model(p))
+
+        return
+
+        for jj,truth in enumerate(model.truths):
+            fig,axes = plt.subplots(1,2,figsize=(12,6))
+
+            p = model.truths.copy()
+            vals = truth*np.linspace(0.25, 1.75, 51)
+            Ls = []
+            for val in vals:
+                p[jj] = val
+                Ls.append(model(p))
+            axes[0].plot(vals, Ls)
+
+            p = model.truths.copy()
+            vals = truth*np.linspace(0.9, 1.1, 121)
+            Ls = []
+            for val in vals:
+                p[jj] = val
+                Ls.append(model(p))
+
+            logger.debug("{} vs truth {}".format(vals[np.argmax(Ls)], truth))
+            logger.debug("{:.2f}% off".format(abs(vals[np.argmax(Ls)] - truth)/truth*100.))
+
+            axes[1].plot(vals, Ls)
+            axes[0].axvline(truth)
+            axes[1].axvline(truth)
+
+            fig.savefig(os.path.join(test_path, "log_test_{}.png".format(jj)))
+
+            # plt.clf()
+            # plt.plot(vals, np.exp(Ls))
+            # plt.axvline(truth)
+            # plt.savefig(os.path.join(test_path, "test_{}.png".format(jj)))
 
 
 def time_likelihood(model, potential, niter=100):
