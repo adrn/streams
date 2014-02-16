ca2137ea5a9825eb1924b83e8f56f63eef3f2f68
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-02-16 17:48:06 -0500

    new figure

--- scripts/figures/paper2_figures.py
+++ scripts/figures/paper2_figures.py
@@ -163,277 +163,51 @@ def potential_contours(**kwargs):
     fig.tight_layout(pad=1.5, h_pad=0.)
     fig.savefig(filename)
 
-def gaussian(x, mu, sigma):
-    return 1./np.sqrt(2*np.pi*sigma**2) * np.exp(-0.5 * ((x-mu)/sigma)**2)
+def simulated_streams(**kwargs):
 
-def rel_dist(**kwargs):
+    filename = os.path.join(plot_path, "simulated_streams.pdf")
+    fig,axes = plt.subplots(2,4,figsize=(14,7.5),
+                            sharex=True, sharey=True)
 
-    nbins = 50
-    fig,axes = plt.subplots(4,2,figsize=(8.5,11), sharex='col', sharey=True)
+    ticks = [-100,-50,0,50]
+    alpha = 0.25
+    rcparams = {'lines.linestyle' : 'none',
+                'lines.linewidth' : 1.,
+                'lines.marker' : '.',
+                'axes.facecolor' : '#ffffff'}
 
-    #_m = kwargs["mass"]
-    for kk,_m in enumerate(["2.5e6","2.5e7","2.5e8","2.5e9"]):
-        m = float(_m)
-        sgr = SgrSimulation(_m)
-        p = sgr.particles(N=5000, expr="tub!=0")
-        p_bound = sgr.particles(N=0, expr="tub==0")
-        v_disp = np.sqrt(np.var(p_bound["vx"]) + \
-                         np.var(p_bound["vy"]) + \
-                         np.var(p_bound["vz"])).value[0]
-        s = sgr.satellite()
+    for ii,_m in enumerate(range(6,9+1)):
+        mass = "2.5e{}".format(_m)
 
-        lm10_potential = LawMajewski2010()
+        m = float(mass)
+        sgr = SgrSimulation(mass)
+        p = sgr.particles(N=5000)
 
-        X = np.vstack((s._X[...,:3], p._X[...,:3].copy()))
-        V = np.vstack((s._X[...,3:], p._X[...,3:].copy()))
-        integrator = LeapfrogIntegrator(lm10_potential._acceleration_at,
-                                        np.array(X), np.array(V),
-                                        args=(X.shape[0], np.zeros_like(X)))
-        ts, rs, vs = integrator.run(t1=sgr.t1, t2=sgr.t2, dt=-1.)
+        with rc_context(rc=rcparams):
+            axes[0,ii].text(0.5, 1.04, r"{}$M_\odot$".format(mass),
+                   horizontalalignment='center',
+                   fontsize=24,
+                   transform=axes[0,ii].transAxes)
 
-        s_orbit = np.vstack((rs[:,0][:,np.newaxis].T, vs[:,0][:,np.newaxis].T)).T
-        p_orbits = np.vstack((rs[:,1:].T, vs[:,1:].T)).T
-        t_idx = np.array([np.argmin(np.fabs(ts - t)) for t in p.tub])
+            axes[0,ii].plot(p["x"].value, p["y"].value,
+                            alpha=alpha)
+            axes[1,ii].plot(p["x"].value, p["z"].value,
+                            alpha=alpha)
+            axes[1,ii].set_xticks(ticks)
+            axes[1,ii].set_xlabel("$X$ [kpc]")
 
-        p_x = np.array([p_orbits[jj,ii] for ii,jj in enumerate(t_idx)])
-        s_x = np.array([s_orbit[jj,0] for jj in t_idx])
+    axes[0,0].set_ylabel("$Y$ [kpc]")
+    axes[1,0].set_ylabel("$Z$ [kpc]")
 
-        diff = p_x-s_x
-        rel_r = np.sqrt(np.sum(diff[:,:3]**2, axis=-1))
-        rel_v = np.sqrt(np.sum(diff[:,3:]**2, axis=-1))
-
-        r_tide = lm10_potential._tidal_radius(m, s_x)*1.6
-        v_esc = lm10_potential._escape_velocity(m, r_tide)
-
-        mu_r, sig_r = norm.fit(np.log(rel_r))
-        print("effective disruption radius: {}".format(mu_r))
-        print("\t\tspread: {}".format(sig_r))
-        print("\t\talpha: {}".format(1/sig_r))
-        print("\t\tmean tidal radius: {}".format(np.log(r_tide.mean())))
-
-        mu_v, sig_v = norm.fit(np.log(rel_v))
-        print("effective disruption velocity: {}".format(mu_v))
-        print("\t\tspread: {}".format(sig_v))
-        print("\t\tbeta: {}".format(1/sig_v))
-        print("\t\tvelocity dispersion: {}".format(np.log(v_disp)))
-        print("\n")
-
-        n,bins,patches = axes[kk,0].hist(np.log(rel_r), bins=nbins,
-                                         alpha=0.4, normed=True)
-        axes[kk,0].plot(bins, gaussian(bins, mu_r, sig_r), lw=2., alpha=0.75)
-        axes[kk,0].axvline(np.log(r_tide.mean()), color='#ca0020', lw=2.,
-                        alpha=0.75, label=r'$\ln \langle R_{\rm tide}\rangle$')
-        axes[kk,0].set_yticks([])
-
-        ylim = axes[kk,0].get_ylim()
-        axes[kk,0].text(-2.75, ylim[1]-(ylim[1]-ylim[0])/5.,
-                        r"{} $M_\odot$".format(_m),
-                        fontsize=16)
-
-        axes[kk,0].text(-2.75, ylim[1]-1.75*(ylim[1]-ylim[0])/5.,
-                        r"$\sigma={:.2f}$".format(sig_r),
-                        fontsize=14)
-
-        ylim = axes[kk,1].get_ylim()
-        axes[kk,1].text(-8.75, ylim[1]-1.75*(ylim[1]-ylim[0])/5.,
-                        r"$\sigma={:.2f}$".format(sig_v),
-                        fontsize=14)
-
-        n,bins,patches = axes[kk,1].hist(np.log(rel_v), bins=nbins,
-                                         alpha=0.4, normed=True)
-        axes[kk,1].plot(bins, gaussian(bins, mu_v, sig_v), lw=2., alpha=0.75)
-        axes[kk,1].axvline(np.log(v_disp), color='#2ca25f', lw=2.,
-                        alpha=0.75, label=r'$\ln \sigma_v$')
-        axes[kk,1].axvline(np.log(v_esc.mean()), color='#2ca25f', lw=2.,
-                        alpha=0.75, label=r'$\ln \sigma_v$')
-        axes[kk,1].set_yticks([])
-
-        if kk == 0:
-            axes[kk,0].set_title("relative distances")
-            axes[kk,1].set_title("relative velocities")
-            axes[kk,0].legend(fontsize=12)
-            axes[kk,1].legend(fontsize=12)
-
-        if kk == 3:
-            axes[kk,0].set_xlabel(r"$\ln\vert r_i-r_{\rm sat} \vert$")
-            axes[kk,1].set_xlabel(r"$\ln\vert v_i-v_{\rm sat} \vert$")
+    axes[0,0].set_yticks(ticks)
+    axes[1,0].set_yticks(ticks)
+    axes[-1,-1].set_xlim(-100,75)
+    axes[-1,-1].set_ylim(-100,75)
 
     fig.tight_layout()
-    fig.savefig(os.path.join(plot_path, "rel_dists.pdf"))
-
-def test(**kwargs):
-
-    nbins = 100
-    fig,axes = plt.subplots(4,2,figsize=(8.5,11), sharex='col', sharey=True)
-
-    #_m = kwargs["mass"]
-    for kk,_m in enumerate(["2.5e6","2.5e7","2.5e8","2.5e9"]):
-        m = float(_m)
-        sgr = SgrSimulation(_m)
-
-        sim_time = sgr.particle_units[0]/sgr.particle_units[-1]
-        selection_expr = "(tub!=0) & (tub<{})".format((6000*u.Myr).to(sim_time).value)
-        p = sgr.particles(N=5000, expr=selection_expr)
-
-        p_bound = sgr.particles(N=0, expr="tub==0")
-        v_disp = np.sqrt(np.var(p_bound["vx"]) + \
-                         np.var(p_bound["vy"]) + \
-                         np.var(p_bound["vz"])).value[0]
-        s = sgr.satellite()
-
-        lm10_potential = LawMajewski2010()
-
-        X = np.vstack((s._X[...,:3], p._X[...,:3].copy()))
-        V = np.vstack((s._X[...,3:], p._X[...,3:].copy()))
-        integrator = LeapfrogIntegrator(lm10_potential._acceleration_at,
-                                        np.array(X), np.array(V),
-                                        args=(X.shape[0], np.zeros_like(X)))
-        ts, rs, vs = integrator.run(t1=sgr.t1, t2=sgr.t2, dt=-1.)
-
-        s_orbit = np.vstack((rs[:,0][:,np.newaxis].T, vs[:,0][:,np.newaxis].T)).T
-        p_orbits = np.vstack((rs[:,1:].T, vs[:,1:].T)).T
-        t_idx = np.array([np.argmin(np.fabs(ts - t)) for t in p.tub])
-
-        p_x = np.array([p_orbits[jj,ii] for ii,jj in enumerate(t_idx)])
-        s_x = np.array([s_orbit[jj,0] for jj in t_idx])
-
-        diff = p_x-s_x
-        rel_r = np.sqrt(np.sum(diff[:,:3]**2, axis=-1))
-        rel_v = np.sqrt(np.sum(diff[:,3:]**2, axis=-1))
-
-        r_tide = lm10_potential._tidal_radius(m, s_x)*1.6
-        v_esc = lm10_potential._escape_velocity(m, r_tide)
-
-        mu_r, sig_r = norm.fit(np.log(rel_r/r_tide))
-        mu_v, sig_v = norm.fit(np.log(rel_v/v_disp))
-        print(mu_r, sig_r)
-        print(mu_v, sig_v)
-        print()
-
-        n,bins,patches = axes[kk,0].hist(rel_r/r_tide,
-                                         bins=nbins, alpha=0.4, normed=True)
-        axes[kk,0].plot(bins, gaussian(bins, mu_r, sig_r), lw=2., alpha=0.75)
-        axes[kk,0].set_yticks([])
-
-        ylim = axes[kk,0].get_ylim()
-        axes[kk,0].text(-2.75, ylim[1]-(ylim[1]-ylim[0])/5.,
-                        r"{} $M_\odot$".format(_m),
-                        fontsize=16)
-
-        axes[kk,0].text(-2.75, ylim[1]-1.75*(ylim[1]-ylim[0])/5.,
-                        r"$\sigma={:.2f}$".format(sig_r),
-                        fontsize=14)
-
-
-        n,bins,patches = axes[kk,1].hist(rel_v/v_disp, bins=nbins,
-                                         alpha=0.4, normed=True)
-        axes[kk,1].plot(bins, gaussian(bins, mu_v, sig_v), lw=2., alpha=0.75)
-        # n,bins,patches = axes[kk,1].hist(np.log(rel_v/v_esc), bins=nbins,
-        #                                  alpha=0.4, normed=True)
-        axes[kk,1].set_yticks([])
-
-        ylim = axes[kk,1].get_ylim()
-        axes[kk,1].text(-2.75, ylim[1]-1.75*(ylim[1]-ylim[0])/5.,
-                        r"$\sigma={:.2f}$".format(sig_v),
-                        fontsize=14)
-
-        if kk == 0:
-            axes[kk,0].set_title("relative distances")
-            axes[kk,1].set_title("relative velocities")
-
-        if kk == 3:
-            axes[kk,0].set_xlabel(r"$\ln\vert r_i-r_{\rm sat} \vert$")
-            axes[kk,1].set_xlabel(r"$\ln\vert v_i-v_{\rm sat} \vert$")
-
-    fig.tight_layout()
-    fig.savefig(os.path.join(plot_path, "norm_dists.pdf".format(_m)))
-
-def norm_dist(**kwargs):
-
-    nbins = 50
-    fig,axes = plt.subplots(4,2,figsize=(8.5,11), sharex='col', sharey=True)
-
-    #_m = kwargs["mass"]
-    for kk,_m in enumerate(["2.5e6","2.5e7","2.5e8","2.5e9"]):
-        m = float(_m)
-        sgr = SgrSimulation(_m)
-
-        sim_time = sgr.particle_units[0]/sgr.particle_units[-1]
-        selection_expr = "(tub!=0) & (tub<{})".format((6000*u.Myr).to(sim_time).value)
-        p = sgr.particles(N=5000, expr=selection_expr)
-
-        p_bound = sgr.particles(N=0, expr="tub==0")
-        v_disp = np.sqrt(np.var(p_bound["vx"]) + \
-                         np.var(p_bound["vy"]) + \
-                         np.var(p_bound["vz"])).value[0]
-        s = sgr.satellite()
-
-        lm10_potential = LawMajewski2010()
-
-        X = np.vstack((s._X[...,:3], p._X[...,:3].copy()))
-        V = np.vstack((s._X[...,3:], p._X[...,3:].copy()))
-        integrator = LeapfrogIntegrator(lm10_potential._acceleration_at,
-                                        np.array(X), np.array(V),
-                                        args=(X.shape[0], np.zeros_like(X)))
-        ts, rs, vs = integrator.run(t1=sgr.t1, t2=sgr.t2, dt=-1.)
-
-        s_orbit = np.vstack((rs[:,0][:,np.newaxis].T, vs[:,0][:,np.newaxis].T)).T
-        p_orbits = np.vstack((rs[:,1:].T, vs[:,1:].T)).T
-        t_idx = np.array([np.argmin(np.fabs(ts - t)) for t in p.tub])
-
-        p_x = np.array([p_orbits[jj,ii] for ii,jj in enumerate(t_idx)])
-        s_x = np.array([s_orbit[jj,0] for jj in t_idx])
-
-        diff = p_x-s_x
-        rel_r = np.sqrt(np.sum(diff[:,:3]**2, axis=-1))
-        rel_v = np.sqrt(np.sum(diff[:,3:]**2, axis=-1))
-
-        r_tide = lm10_potential._tidal_radius(m, s_x)*1.6
-        v_esc = lm10_potential._escape_velocity(m, r_tide)
-
-        mu_r, sig_r = norm.fit(np.log(rel_r/r_tide))
-        mu_v, sig_v = norm.fit(np.log(rel_v/v_disp))
-        print(mu_r, sig_r)
-        print(mu_v, sig_v)
-        print()
-
-        n,bins,patches = axes[kk,0].hist(np.log(rel_r/r_tide),
-                                         bins=nbins, alpha=0.4, normed=True)
-        axes[kk,0].plot(bins, gaussian(bins, mu_r, sig_r), lw=2., alpha=0.75)
-        axes[kk,0].set_yticks([])
-
-        ylim = axes[kk,0].get_ylim()
-        axes[kk,0].text(-2.75, ylim[1]-(ylim[1]-ylim[0])/5.,
-                        r"{} $M_\odot$".format(_m),
-                        fontsize=16)
-
-        axes[kk,0].text(-2.75, ylim[1]-1.75*(ylim[1]-ylim[0])/5.,
-                        r"$\sigma={:.2f}$".format(sig_r),
-                        fontsize=14)
-
-
-        n,bins,patches = axes[kk,1].hist(np.log(rel_v/v_disp), bins=nbins,
-                                         alpha=0.4, normed=True)
-        axes[kk,1].plot(bins, gaussian(bins, mu_v, sig_v), lw=2., alpha=0.75)
-        # n,bins,patches = axes[kk,1].hist(np.log(rel_v/v_esc), bins=nbins,
-        #                                  alpha=0.4, normed=True)
-        axes[kk,1].set_yticks([])
-
-        ylim = axes[kk,1].get_ylim()
-        axes[kk,1].text(-2.75, ylim[1]-1.75*(ylim[1]-ylim[0])/5.,
-                        r"$\sigma={:.2f}$".format(sig_v),
-                        fontsize=14)
-
-        if kk == 0:
-            axes[kk,0].set_title("relative distances")
-            axes[kk,1].set_title("relative velocities")
-
-        if kk == 3:
-            axes[kk,0].set_xlabel(r"$\ln\vert r_i-r_{\rm sat} \vert$")
-            axes[kk,1].set_xlabel(r"$\ln\vert v_i-v_{\rm sat} \vert$")
-
-    fig.tight_layout()
-    fig.savefig(os.path.join(plot_path, "norm_dists.pdf".format(_m)))
+    fig.subplots_adjust(top=0.92, hspace=0.025, wspace=0.1)
+    #fig.subplots_adjust(hspace=0., wspace=0.075)
+    fig.savefig(filename)
 
 def twod_dist(**kwargs):
 
@@ -517,73 +291,6 @@ def twod_dist(**kwargs):
                  fontsize=32)
     fig.subplots_adjust(hspace=0., wspace=0.)
     fig.savefig(os.path.join(plot_path, "twod_{}.pdf".format(kwargs["m"])))
-
-def costheta(**kwargs):
-
-    nbins = 50
-    fig,axes = plt.subplots(4,2,figsize=(8.5,11), sharex='col', sharey=True)
-    fig2,axes2 = plt.subplots(4,1,figsize=(8.5,11), sharex=True, sharey=True)
-
-    #_m = kwargs["mass"]
-    for kk,_m in enumerate(["2.5e6","2.5e7","2.5e8","2.5e9"]):
-        m = float(_m)
-        sgr = SgrSimulation(_m)
-        p = sgr.particles(N=5000, expr="tub!=0")
-        p_bound = sgr.particles(N=0, expr="tub==0")
-        v_disp = np.sqrt(np.var(p_bound["vx"]) + \
-                         np.var(p_bound["vy"]) + \
-                         np.var(p_bound["vz"])).value[0]
-        s = sgr.satellite()
-
-        lm10_potential = LawMajewski2010()
-
-        X = np.vstack((s._X[...,:3], p._X[...,:3].copy()))
-        V = np.vstack((s._X[...,3:], p._X[...,3:].copy()))
-        integrator = LeapfrogIntegrator(lm10_potential._acceleration_at,
-                                        np.array(X), np.array(V),
-                                        args=(X.shape[0], np.zeros_like(X)))
-        ts, rs, vs = integrator.run(t1=sgr.t1, t2=sgr.t2, dt=-1.)
-
-        s_orbit = np.vstack((rs[:,0][:,np.newaxis].T, vs[:,0][:,np.newaxis].T)).T
-        p_orbits = np.vstack((rs[:,1:].T, vs[:,1:].T)).T
-        t_idx = np.array([np.argmin(np.fabs(ts - t)) for t in p.tub])
-
-        p_x = np.array([p_orbits[jj,ii] for ii,jj in enumerate(t_idx)])
-        s_x = np.array([s_orbit[jj,0] for jj in t_idx])
-
-        diff = p_x-s_x
-        normed_rel_r = diff[:,:3] / np.sqrt(np.sum(diff[:,:3]**2, axis=-1))[:,np.newaxis]
-        normed_rel_v = diff[:,3:] / np.sqrt(np.sum(diff[:,3:]**2, axis=-1))[:,np.newaxis]
-        normed_rs = s_x[...,:3] / np.sqrt(np.sum(s_x[...,:3]**2, axis=-1))[:,np.newaxis]
-        normed_vs = s_x[...,3:] / np.sqrt(np.sum(s_x[...,3:]**2, axis=-1))[:,np.newaxis]
-
-        costheta = np.sum(normed_rel_r*normed_vs, axis=-1)
-        cosphi = np.sum(normed_rel_r*normed_rs, axis=-1)
-
-        cos = np.sum(normed_rel_v*normed_vs, axis=-1)
-        sinphi = np.sum(normed_rel_v*normed_rs, axis=-1)
-
-        n,bins,patches = axes[kk,0].hist(costheta, bins=nbins, alpha=0.25)
-        n,bins,patches = axes[kk,1].hist(cosphi, bins=nbins, alpha=0.25)
-        print(np.sum(cosphi < 0.) / np.sum(cosphi > 0.))
-
-        #n,bins,patches = axes2[kk].hist(sintheta, bins=nbins, alpha=0.25)
-        #n,bins,patches = axes2[kk].hist(sinphi, bins=bins, alpha=0.25)
-
-        for ax in [axes[kk,0],axes2[kk]]:
-            xlim = ax.get_xlim()
-            ylim = ax.get_ylim()
-            ax.text(-0.75, ylim[1]-(ylim[1]-ylim[0])/5.,
-                    r"{} $M_\odot$".format(_m),
-                    fontsize=16)
-
-    axes[-1,0].set_xlabel(r"$\cos\theta$")
-    axes[-1,1].set_xlabel(r"$\cos\phi$")
-    axes2[-1].set_xlabel(r"$\sin\theta$")
-    fig.tight_layout()
-    fig.savefig(os.path.join(plot_path, "costheta.pdf"))
-    fig2.tight_layout()
-    fig2.savefig(os.path.join(plot_path, "sintheta.pdf"))
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
