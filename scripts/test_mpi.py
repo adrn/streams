8a7f53415827bb4f7727e79c61508eb4736ee318
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-02-23 09:55:31 -0500

    add mpi test script

---
+++ scripts/test_mpi.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+
+""" Test MPI on a given system. """
+
+from __future__ import division, print_function
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Standard library
+import os, sys
+import time
+
+# Third-party
+import numpy as np
+from emcee.utils import MPIPool
+
+# Initialize the MPI pool
+pool = MPIPool()
+
+# Make sure the thread we're running on is the master
+if not pool.is_master():
+    pool.wait()
+    sys.exit(0)
+
+v = np.random.random(size=100000)
+def f(x):
+    return np.sqrt(x)
+
+a = time.time()
+results = pool.map(f, v)
+pool.close()
+print(time.time() - a, "MPI map")
+
+# now try in serial
+a = time.time()
+map(f, v)
+print(time.time() - a, "map")
