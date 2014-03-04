a57f5f83350aa4f5b23e3765d328e819a4e828cd
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-03-04 16:33:03 -0500

    don't always compute tail_bit

--- streams/io/sgr.py
+++ streams/io/sgr.py
@@ -72,7 +72,7 @@ class SgrSimulation(object):
         tbl = read_table(self.particle_filename, N=N, expr=expr)
         return tbl
 
-    def particles(self, N=None, expr=None, meta_cols=[]):
+    def particles(self, N=None, expr=None, meta_cols=[], tail_bit=False):
         """ Return a Particle object with N particles selected from the
             simulation with expression expr.
 
@@ -84,6 +84,8 @@ class SgrSimulation(object):
                 Use numexpr to select out only rows that match criteria.
             meta_cols : iterable (optional)
                 List of columns to add to meta data.
+            tail_bit : bool (optional)
+                Compute tail bit or not.
         """
         tbl = self.raw_particle_table(N=N, expr=expr)
 
@@ -102,55 +104,58 @@ class SgrSimulation(object):
         ################################################################
         # HACK: Figure out if in leading or trailing tail
         # separate out the orbit of the satellite from the orbit of the stars
-        s = self.satellite()
-        X = np.vstack((s._X[...,:3], p._X[...,:3].copy()))
-        V = np.vstack((s._X[...,3:], p._X[...,3:].copy()))
-        integrator = LeapfrogIntegrator(self.true_potential._acceleration_at,
-                                        np.array(X), np.array(V),
-                                        args=(X.shape[0], np.zeros_like(X)))
-        ts, rs, vs = integrator.run(t1=self.t1, t2=self.t2, dt=-1.)
-        s_orbit = np.vstack((rs[:,0][:,np.newaxis].T, vs[:,0][:,np.newaxis].T)).T
-        p_orbits = np.vstack((rs[:,1:].T, vs[:,1:].T)).T
+        if tail_bit:
+            s = self.satellite()
+            X = np.vstack((s._X[...,:3], p._X[...,:3].copy()))
+            V = np.vstack((s._X[...,3:], p._X[...,3:].copy()))
+            integrator = LeapfrogIntegrator(self.true_potential._acceleration_at,
+                                            np.array(X), np.array(V),
+                                            args=(X.shape[0], np.zeros_like(X)))
+            ts, rs, vs = integrator.run(t1=self.t1, t2=self.t2, dt=-1.)
+            s_orbit = np.vstack((rs[:,0][:,np.newaxis].T, vs[:,0][:,np.newaxis].T)).T
+            p_orbits = np.vstack((rs[:,1:].T, vs[:,1:].T)).T
 
-        # m_t = -s.mdot*ts + s.m0
-        # s_R = np.sqrt(np.sum(s_orbit[...,:3]**2, axis=-1))
-        # s_V = np.sqrt(np.sum(s_orbit[...,3:]**2, axis=-1))
-        # r_tide = self.true_potential._tidal_radius(m_t, s_orbit[...,:3])
-        # v_disp = s_V * r_tide / s_R
+            # m_t = -s.mdot*ts + s.m0
+            # s_R = np.sqrt(np.sum(s_orbit[...,:3]**2, axis=-1))
+            # s_V = np.sqrt(np.sum(s_orbit[...,3:]**2, axis=-1))
+            # r_tide = self.true_potential._tidal_radius(m_t, s_orbit[...,:3])
+            # v_disp = s_V * r_tide / s_R
 
-        t_idx = np.array([np.argmin(np.fabs(ts - t)) for t in p.tub])
-        s_orbit = np.array([s_orbit[jj,0] for jj in t_idx])
-        p_orbits = np.array([p_orbits[jj,ii] for ii,jj in enumerate(t_idx)])
-        # r_tide = np.array([r_tide[jj,0] for jj in t_idx])
-        # v_disp = np.array([v_disp[jj,0] for jj in t_idx])
+            t_idx = np.array([np.argmin(np.fabs(ts - t)) for t in p.tub])
+            s_orbit = np.array([s_orbit[jj,0] for jj in t_idx])
+            p_orbits = np.array([p_orbits[jj,ii] for ii,jj in enumerate(t_idx)])
+            # r_tide = np.array([r_tide[jj,0] for jj in t_idx])
+            # v_disp = np.array([v_disp[jj,0] for jj in t_idx])
 
-        # instantaneous cartesian basis to project into
-        x_hat = s_orbit[...,:3] / np.sqrt(np.sum(s_orbit[...,:3]**2, axis=-1))[...,np.newaxis]
-        y_hat = s_orbit[...,3:] / np.sqrt(np.sum(s_orbit[...,3:]**2, axis=-1))[...,np.newaxis]
-        z_hat = np.cross(x_hat, y_hat)
+            # instantaneous cartesian basis to project into
+            x_hat = s_orbit[...,:3] / np.sqrt(np.sum(s_orbit[...,:3]**2, axis=-1))[...,np.newaxis]
+            y_hat = s_orbit[...,3:] / np.sqrt(np.sum(s_orbit[...,3:]**2, axis=-1))[...,np.newaxis]
+            z_hat = np.cross(x_hat, y_hat)
 
-        # translate to satellite position
-        rel_orbits = p_orbits - s_orbit
-        rel_pos = rel_orbits[...,:3]
-        rel_vel = rel_orbits[...,3:]
+            # translate to satellite position
+            rel_orbits = p_orbits - s_orbit
+            rel_pos = rel_orbits[...,:3]
+            rel_vel = rel_orbits[...,3:]
 
-        # project onto X
-        X = np.sum(rel_pos * x_hat, axis=-1)
-        Y = np.sum(rel_pos * y_hat, axis=-1)
-        Z = np.sum(rel_pos * z_hat, axis=-1)
+            # project onto X
+            X = np.sum(rel_pos * x_hat, axis=-1)
+            Y = np.sum(rel_pos * y_hat, axis=-1)
+            Z = np.sum(rel_pos * z_hat, axis=-1)
 
-        VX = np.sum(rel_vel * x_hat, axis=-1)
-        VY = np.sum(rel_vel * y_hat, axis=-1)
-        VZ = np.sum(rel_vel * z_hat, axis=-1)
+            VX = np.sum(rel_vel * x_hat, axis=-1)
+            VY = np.sum(rel_vel * y_hat, axis=-1)
+            VZ = np.sum(rel_vel * z_hat, axis=-1)
 
-        Phi = np.arctan2(Y, X)
-        tail_bit = np.ones(p.nparticles)
-        tail_bit[:] = np.nan
-        tail_bit[np.cos(Phi) < -0.5] = -1.
-        tail_bit[np.cos(Phi) > 0.5] = 1.
+            Phi = np.arctan2(Y, X)
+            tail_bit = np.ones(p.nparticles)
+            tail_bit[:] = np.nan
+            tail_bit[np.cos(Phi) < -0.5] = -1.
+            tail_bit[np.cos(Phi) > 0.5] = 1.
 
-        p.meta["tail_bit"] = p.tail_bit = tail_bit
+            p.meta["tail_bit"] = p.tail_bit = tail_bit
         ################################################################
+        else:
+            tail_bit = np.ones(p.nparticles)*np.nan
 
         return p
 
