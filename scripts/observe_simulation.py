ae778bf29e10a88825071ca8b76292f5a260517c
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-03-03 19:03:56 -0500

    moved tailbit stuff and added mdot stuff

--- scripts/observe_simulation.py
+++ scripts/observe_simulation.py
@@ -77,9 +77,9 @@ def observe_simulation(class_name, particle_error_model=None, satellite_error_mo
     sim_time = simulation.particle_units[0]/simulation.particle_units[-1]
 
     # HACK HACK HACK
-    selection_expr = "(tub!=0) & (tub>{}) & (tub<{}) & (sqrt((x+8)**2 + y**2 + z**2)<75)"\
-                     .format((1750*u.Myr).to(sim_time).value,
-                             (4700*u.Myr).to(sim_time).value)
+    selection_expr = "(tub!=0) & (tub>{}) & (tub<{}) & (sqrt((x+8)**2 + y**2 + z**2)<60)"\
+                     .format((1400*u.Myr).to(sim_time).value,
+                             (5300*u.Myr).to(sim_time).value)
     particles = simulation.particles(N=N, expr=selection_expr)
 
     logger.debug("Read in {} particles with expr='{}'"\
@@ -88,32 +88,6 @@ def observe_simulation(class_name, particle_error_model=None, satellite_error_mo
     # read the satellite position
     satellite = simulation.satellite()
     logger.debug("Read in present position of satellite...")
-
-    ################################################################
-    # HACK: Figure out if in leading or trailing tail
-    s = satellite
-    p = particles
-    lm10_potential = LawMajewski2010()
-    X = np.vstack((s._X[...,:3], p._X[...,:3].copy()))
-    V = np.vstack((s._X[...,3:], p._X[...,3:].copy()))
-    integrator = LeapfrogIntegrator(lm10_potential._acceleration_at,
-                                    np.array(X), np.array(V),
-                                    args=(X.shape[0], np.zeros_like(X)))
-    ts, rs, vs = integrator.run(t1=simulation.t1, t2=simulation.t2, dt=-1)
-
-    s_orbit = np.vstack((rs[:,0][:,np.newaxis].T, vs[:,0][:,np.newaxis].T)).T
-    p_orbits = np.vstack((rs[:,1:].T, vs[:,1:].T)).T
-    t_idx = np.array([np.argmin(np.fabs(ts - t)) for t in p.tub])
-
-    p_x = np.array([p_orbits[jj,ii] for ii,jj in enumerate(t_idx)])
-    s_x = np.array([s_orbit[jj,0] for jj in t_idx])
-    diff = p_x-s_x
-
-    norm_r = s_x[:,:3] / np.sqrt(np.sum(s_x[:,:3]**2, axis=-1))[:,np.newaxis]
-    norm_diff_r = diff[:,:3] / np.sqrt(np.sum(diff[:,:3]**2, axis=-1))[:,np.newaxis]
-    dot_prod_r = np.sum(norm_diff_r*norm_r, axis=-1)
-    tail_bit = (dot_prod_r > 0.).astype(int)*2 - 1
-    ################################################################
 
     satellite = satellite.to_frame(heliocentric)
     particles = particles.to_frame(heliocentric)
@@ -176,7 +150,7 @@ def observe_simulation(class_name, particle_error_model=None, satellite_error_mo
         grp["coordinate_names"] = o_particles.frame.coord_names
         grp["units"] = [str(x) for x in o_particles._repr_units]
         grp["tub"] = o_particles.tub
-        grp["tail_bit"] = tail_bit
+        grp["tail_bit"] = o_particles.tail_bit
 
         grp = f.create_group("satellite")
         grp["data"] = o_satellite._repr_X
@@ -185,8 +159,8 @@ def observe_simulation(class_name, particle_error_model=None, satellite_error_mo
             grp["true_data"] = satellite._repr_X
         grp["coordinate_names"] = o_satellite.frame.coord_names
         grp["units"] = [str(x) for x in o_satellite._repr_units]
-        grp["m"] = o_satellite.m
-        grp["v_disp"] = o_satellite.v_disp
+        grp["m0"] = o_satellite.m
+        grp["mdot"] = o_satellite.mdot
 
         grp = f.create_group("simulation")
         grp["t1"] = simulation.t1
