aff4ec13dbd4a07f1e1de340b2711899fa15f42e
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-03-23 12:22:57 -0400

    fix whack walkers

--- scripts/infer_potential.py
+++ scripts/infer_potential.py
@@ -41,7 +41,7 @@ def fix_whack_walkers(acc_frac, pos, threshold=0.01):
 
         # resample positions for bad walkers
         pos[ix] = np.random.normal(np.median(pos[~ix], axis=0),
-                                   np.fabs(np.median(pos[~ix], axis=0)/10.),
+                                   np.fabs(np.median(pos[~ix], axis=0)/100.),
                                    size=(sum(ix),pos.shape[1]))
 
     return pos
