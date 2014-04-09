44905f9816d7c6c4e1902d6f5c64d994dd25971d
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-04-08 20:52:59 -0400

    use streamteam stuff, change current data

--- streams/observation/gaia.py
+++ streams/observation/gaia.py
@@ -14,102 +14,27 @@ import copy
 import numpy as np
 import astropy.units as u
 from astropy.utils.misc import isiterable
+from streamteam.observation import apparent_magnitude
+from streamteam.observation.rrlyrae import M_V, gaia_rv_error, gaia_pm_error
 
 from ..dynamics import Particle
 from ..coordinates import gc_to_hel, hel_to_gc
-from .rrlyrae import *
-from .core import *
 
-__all__ = ["parallax_error", "proper_motion_error", "V_to_G", \
-           "gaia_spitzer_errors"]
-
-def V_to_G(V, V_minus_I):
-    """ Convert Johnson V to Gaia G-band.
-
-        Parameters
-        ----------
-        V : numeric or iterable
-            The V-band apparent magnitude of a source.
-        V_minus_I : numeric or iterable
-            The V-I color of the source.
-    """
-    return V - 0.0257 - 0.0924*V_minus_I - 0.1623*V_minus_I**2 + 0.009*V_minus_I**3
-
-def parallax_error(V, V_minus_I):
-    """ Compute the estimated GAIA parallax error as a function of apparent
-        V-band magnitude and V-I color. All equations are taken from the GAIA
-        science performance book:
-
-            http://www.rssd.esa.int/index.php?project=GAIA&page=Science_Performance#chapter1
-
-        Parameters
-        ----------
-        V : numeric or iterable
-            The V-band apparent magnitude of a source.
-        V_minus_I : numeric or iterable
-            The V-I color of the source.
-    """
-
-    # GAIA G mag
-    g = V - 0.0257 - 0.0924*V_minus_I- 0.1623*V_minus_I**2 + 0.0090*V_minus_I**3
-    z = 10**(0.4*(g-15.))
-
-    p = g < 12.
-    if isiterable(V):
-        if sum(p) > 0:
-            z[p] = 10**(0.4*(12. - 15.))
-    else:
-        if p:
-            z = 10**(0.4*(12. - 15.))
-
-    # "end of mission parallax standard"
-    # σπ [μas] = (9.3 + 658.1·z + 4.568·z^2)^(1/2) · [0.986 + (1 - 0.986) · (V-IC)]
-    dp = np.sqrt(9.3 + 658.1*z + 4.568*z**2) * (0.986 + (1 - 0.986)*V_minus_I) * 1E-6 * u.arcsecond
-
-    return dp
-
-def proper_motion_error(V, V_minus_I):
-    """ Compute the estimated GAIA proper motion error as a function of apparent
-        V-band magnitude and V-I color. All equations are taken from the GAIA
-        science performance book:
-
-            http://www.rssd.esa.int/index.php?project=GAIA&page=Science_Performance#chapter1
-
-        Parameters
-        ----------
-        V : numeric or iterable
-            The V-band apparent magnitude of a source.
-        V_minus_I : numeric or iterable
-            The V-I color of the source.
-    """
-
-    dp = parallax_error(V, V_minus_I)
-
-    # assume 5 year baseline, µas/year
-    dmu = dp/u.year
-
-    # too optimistic: following suggests factor 2 more realistic
-    #http://www.astro.utu.fi/~cflynn/galdyn/lecture10.html
-    # - and Sanjib suggests factor 0.526
-    dmu = 0.526*dmu
-
-    return dmu.to(u.radian/u.yr)
+__all__ = ["gaia_spitzer_errors"]
 
 def gaia_spitzer_errors(particles):
     """ Given particles in heliocentric frame, return an array of
         Gaia + Spitzer errors (same shape as particles._X).
     """
 
-    # assuming [Fe/H] = -0.5 for Sgr
-    M_V, dM_V = rrl_M_V(-0.5)
-
     try:
-        V = apparent_magnitude(M_V, particles["d"])
+        # assuming [Fe/H] = -0.5 for Sgr
+        V = apparent_magnitude(M_V(-0.5), particles["d"])
     except AttributeError:
         raise ValueError("Particles in wrong reference frame? Has "
                          "coordinates: {}".format(particles.names))
-    dlb = parallax_error(V, rrl_V_minus_I)
-    dmu = proper_motion_error(V, rrl_V_minus_I)
+    dlb = 100*u.microarcsecond
+    dmu = gaia_pm_error(particles["d"])
 
     # angular position error is negligible
     l = dlb.to(particles["l"].unit)
@@ -130,7 +55,7 @@ def gaia_spitzer_errors(particles):
 def current_data_star(particles):
 
     dlb = 0.1*u.arcsecond
-    dmu = 0.1*u.mas/u.yr # http://adsabs.harvard.edu/abs/2013ApJ...766...79K
+    dmu = 2.*u.mas/u.yr # http://adsabs.harvard.edu/abs/2013ApJ...766...79K for individual stars
 
     # angular position error is negligible
     l = dlb.to(particles["l"].unit)
@@ -141,7 +66,7 @@ def current_data_star(particles):
     mub = dmu.to(particles["mub"].unit)
 
     # distance
-    d = 0.15*particles["d"] # 15% distances, http://adsabs.harvard.edu/cgi-bin/nph-data_query?bibcode=2004AJ....128..245M&db_key=AST&link_type=ABSTRACT&high=51a39ba3d727427
+    d = 0.2*particles["d"] # 20% distances, http://adsabs.harvard.edu/cgi-bin/nph-data_query?bibcode=2004AJ....128..245M&db_key=AST&link_type=ABSTRACT&high=51a39ba3d727427
 
     # radial velocity
     vr = 5.*u.km/u.s * np.ones_like(particles["vr"].value) # M giant, http://adsabs.harvard.edu/cgi-bin/nph-data_query?bibcode=2004AJ....128..245M&db_key=AST&link_type=ABSTRACT&high=51a39ba3d727427
