28db9c266cec1aae0f1ce4ff803f0a33b2b091a9
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-04-28 14:13:23 -0400

    more rigorous tests

--- streams/inference/tests/test_core.py
+++ streams/inference/tests/test_core.py
@@ -18,177 +18,105 @@ import matplotlib.pyplot as plt
 
 from ... import usys
 from ..core import *
-from ...potential.lm10 import LawMajewski2010
-from ...coordinates.frame import galactocentric, heliocentric
 from ..parameter import *
 from ..prior import *
-from ...io import read_hdf5
-
-# TODO: MAJOR TODO HERE!
 
 plot_path = "plots/tests/inference"
 if not os.path.exists(plot_path):
     os.makedirs(plot_path)
 
-class TestStreamModel(object):
+class TestModelParameter(object):
+
+    def test_string(self):
+        p = ModelParameter("m", value=np.nan, truth=1.5,
+                           prior=LogUniformPrior(1.,2.))
+        assert repr(p) == "<ModelParameter 'm' truth=1.5>"
+        assert str(p) == "m"
+
+        p = ModelParameter("m", value=np.nan,
+                           prior=LogUniformPrior(1.,2.))
+        assert repr(p) == "<ModelParameter 'm'>"
+        assert str(p) == "m"
+
+class TestModel(object):
 
     def setup(self):
+        np.random.seed(42)
 
-        self.plot_path = os.path.join(plot_path, "StreamModel")
-        if not os.path.exists(self.plot_path):
-            os.mkdir(self.plot_path)
+        self.flat_model = Model()
+        for name in "abcdefg":
+            p = ModelParameter(name, value=np.nan, truth=np.random.random())
+            self.flat_model.add_parameter(p)
 
-        np.random.seed(52)
-        self.Nparticles = 25
-        self.simulation = SgrSimulation(mass="2.5e8")
-        self.args = (self.simulation.t1,self.simulation.t2,-1.)
+        self.group_model = Model()
+        for group in ["herp","derp"]:
+            for name in "abcd":
+                p = ModelParameter(name, value=np.nan, truth=np.random.random())
+                self.group_model.add_parameter(p, parameter_group=group)
 
-        self.particles = self.simulation.particles(N=self.Nparticles,
-                                                   expr="tub!=0")
-        self.particles = self.particles.to_frame(heliocentric)
+        self.vec_model = Model()
+        for name in "abcd":
+            length = np.random.randint(2,10)
+            p = ModelParameter(name, value=[np.nan]*length, truth=np.random.random(size=length))
+            self.vec_model.add_parameter(p)
 
-        self.potential = LawMajewski2010()
-        self.satellite = self.simulation.satellite()
-        self.satellite = self.satellite.to_frame(heliocentric)
+        self.models = [self.group_model, self.flat_model, self.vec_model]
 
-# # read in the particles
-# d_path = os.path.join(os.environ["STREAMSPATH"], "data/observed_particles/")
-# data_file = "N128.hdf5"
-# d = io.read_hdf5(os.path.join(d_path, data_file))
+    def test_init(self):
+        m = ModelParameter("m", value=np.nan, truth=1.5,
+                           prior=LogUniformPrior(1.,2.))
+        b = ModelParameter("b", value=np.nan, truth=6.7,
+                           prior=LogUniformPrior(0.,10.))
 
-# # integration stuff
-# t1 = float(d["t1"])
-# t2 = float(d["t2"])
-# dt = -1.
+        model = Model()
+        model.add_parameter(m)
+        model.add_parameter(b)
+        model.parameters['main']['m']
+        model.parameters['main']['b']
 
-# # potential
-# q1 = ModelParameter('q1', prior=LogUniformPrior(1.,1.75), truth=1.38)
-# assert q1.prior(1.5) == 0.
-# assert q1.prior(2.5) == -np.inf
+        assert np.all(model.truths == np.array([1.5,6.7]))
 
-# qz = ModelParameter('qz', prior=LogUniformPrior(1.,1.75), truth=1.36)
-# assert qz.prior(1.5) == 0.
-# assert qz.prior(2.5) == -np.inf
+    def test_walk_parameters(self):
+        for model in self.models:
+            for group,name,p in model._walk(model.parameters):
+                assert name == str(p)
 
-# v_halo = ModelParameter('v_halo', prior=LogUniformPrior(0.1, 0.15),
-#                         truth=0.124625659009114)
-# assert v_halo.prior(0.121) == 0.
-# assert v_halo.prior(0.221) == -np.inf
+    def test_decompose_compose(self):
+        for model in self.models:
+            vec = np.random.random(size=model.nparameters)
+            decom = model.vector_to_parameters(vec)
+            for group,name,p in model._walk(model.parameters):
+                decom[group][name]
 
-# phi = ModelParameter('phi', prior=LogUniformPrior(1.5, 1.9),
-#                      truth=1.692969)
-# assert phi.prior(1.69) == 0.
+            com = model.parameters_to_vector(decom)
+            assert np.all((vec-com) == 0.)
 
-# # particles
-# nparticles = 5
-# particles = ObservedParticle(d['particles']._X[:nparticles].T,
-#                              d['particles']._error_X[:nparticles].T,
-#                              frame=d['particles'].frame,
-#                              units=d['particles']._internal_units)
+    def test_flatchain(self):
+        nsteps = 1024
+        for model in self.models:
+            vec = np.random.random(size=(model.nparameters,nsteps))
+            decom = model.vector_to_parameters(vec)
+            for group,name,p in model._walk(model.parameters):
+                print(decom[group][name].shape)
 
-# true_particles = Particle(d['true_particles']._X[:nparticles].T,
-#                           frame=d['true_particles'].frame,
-#                           units=d['true_particles']._internal_units)
-# true_particles.tub = d['true_particles'].tub[:nparticles]
 
-# #X = ModelParameter('_X', truth=true_particles._X)
-# priors = [LogNormalPrior(particles._X[ii],particles._error_X[ii]) for ii in range(nparticles)]
-# X = ModelParameter('_X', value=particles._X, prior=priors,
-#                    truth=true_particles._X)
+# class TestStreamModel(object):
 
-# test_X = np.random.normal(particles._X, particles._error_X)
-# test_X2 = np.random.normal(particles._X, particles._error_X*10.)
-# assert X.prior(test_X).shape == (nparticles,)
-# assert np.sum(X.prior(test_X2)) < np.sum(X.prior(test_X))
+#     def setup(self):
 
-# #truth=true_particles.tub,
-# priors = [LogUniformPrior(t2, t1) for ii in range(nparticles)]
-# tub = ModelParameter('tub', value=np.zeros(nparticles), prior=priors,
-#                      truth=true_particles.tub)
+#         self.plot_path = os.path.join(plot_path, "StreamModel")
+#         if not os.path.exists(self.plot_path):
+#             os.mkdir(self.plot_path)
 
-# test_tubs = np.zeros(nparticles)
-# assert np.sum(tub.prior(test_tubs - 100.)) == -np.inf
-# assert np.sum(tub.prior(test_tubs + 100.)) == 0.
+#         np.random.seed(52)
+#         self.Nparticles = 25
+#         self.simulation = SgrSimulation(mass="2.5e8")
+#         self.args = (self.simulation.t1,self.simulation.t2,-1.)
 
-# # satellite
-# satellite = d['satellite']
-# priors = [LogNormalPrior(satellite._X[0],satellite._error_X[0])]
-# s_X = ModelParameter('_X', value=satellite._X, prior=priors,
-#                      truth=d['true_satellite']._X)
+#         self.particles = self.simulation.particles(N=self.Nparticles,
+#                                                    expr="tub!=0")
+#         self.particles = self.particles.to_frame(heliocentric)
 
-# test_X = np.random.normal(satellite._X, satellite._error_X)
-# test_X2 = np.random.normal(satellite._X, satellite._error_X*10.)
-# assert s_X.prior(test_X).shape == (1,)
-# assert np.sum(s_X.prior(test_X2)) < np.sum(s_X.prior(test_X))
-
-# # MODEL
-# potential = sp.LawMajewski2010()
-# model = StreamModel(potential)
-
-# assert len(model.sample_priors()) == 0
-
-# model.add_parameter('potential', q1)
-# model.add_parameter('potential', qz)
-# model.add_parameter('potential', v_halo)
-# model.add_parameter('potential', phi)
-
-# assert len(model.sample_priors()) == 4
-
-# model.add_parameter('particles', X)
-# assert len(model.sample_priors()) == nparticles*6 + 4
-
-# model.add_parameter('particles', tub)
-# assert len(model.sample_priors()) == nparticles*7 + 4
-
-# model.add_parameter('satellite', s_X)
-# assert len(model.sample_priors()) == nparticles*7 + 4 + 6
-
-# true_ln_p = model.ln_posterior(model.truths, t1, t2, dt)
-# for ii in range(1):
-#     p0 = model.sample_priors()
-#     ln_p = model.ln_posterior(p0, t1, t2, dt)
-#     assert true_ln_p > ln_p
-
-# import time
-# import emcee
-# import multiprocessing
-
-# a = time.time()
-# t = model.truths
-# for ii in range(10):
-#     ln_p = model.ln_posterior(t, t1, t2, dt)
-# print((time.time() - a)/10., "seconds per posterior call")
-
-# nwalkers = model.nparameters*2 + 2
-# print(nwalkers, "walkers for", model.nparameters, "parameters")
-
-# a = time.time()
-# p0 = model.sample_priors(size=nwalkers)
-# print(time.time() - a, "seconds for sampling p0")
-
-# assert p0.shape == (nwalkers, model.nparameters)
-
-# #pickle.dumps(model)
-# #sys.exit(0)
-
-# pool = multiprocessing.Pool(2)
-# sampler = emcee.EnsembleSampler(nwalkers, model.nparameters,
-#                                 model,
-#                                 args=(t1,t2,dt),
-#                                 pool=pool)
-
-# print("running")
-# sampler.run_mcmc(p0, 2)
-# #model.ln_posterior)
-
-# # model.run()
-
-# # model.chain["potential"]["q1"]
-# # model.chain["potential"]["qz"]
-# # model.chain["potential"]["v_halo"]
-# # model.chain["potential"]["phi"]
-
-# # model.chain["particles"]["_X"]
-# # model.chain["particles"]["tub"]
-
-# # model.chain["satellite"]["_X"]
+#         self.potential = LawMajewski2010()
+#         self.satellite = self.simulation.satellite()
+#         self.satellite = self.satellite.to_frame(heliocentric)
