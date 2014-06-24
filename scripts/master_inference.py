6acdc4a5568e11b07c5700d585ac93adf1c9eee6
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-06-24 00:20:18 -0400

    not twice acor time

--- scripts/master_inference.py
+++ scripts/master_inference.py
@@ -45,7 +45,7 @@ def master_inference(path, all=False, outfile="combined_inference.hdf5"):
     print(sum(~ix), "<2% acceptance")
     #tau,mm,xx = acor.acor(np.mean(chain[ix],axis=0).T)
     tau = [acor.acor(np.mean(chain[ix],axis=0).T[i])[0] for i in range(chain.shape[-1])]
-    acor_time = int(2*np.max(tau))
+    acor_time = int(np.max(tau))
     print("Autocorrelation times: ", tau)
     print("Max autocorrelation time: ", acor_time)
 
