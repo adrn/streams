d9085af72f87a08c52de8af5341edb3167cc7be1
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-07-07 16:34:45 +0200

    simulation utilities

---
+++ streams/simulation/util.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+""" Utilities for simulations. """
+
+from __future__ import division, print_function
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Standard library
+import os, sys
+
+# Third-party
+import numpy as np
+from astropy import log as logger
+import astropy.units as u
+
+# Project
+# ...
+
+__all__ = ['']
+
+def identify_tail(potential, ...):
+    pass
