6abfffde3ed67ca99770de32fd573c409a4f869f
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-07-10 14:22:58 +0200

    added simple tests for potentials

---
+++ streams/potential/tests/test_basepotential.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+""" Test the base potential classes. """
+
+from __future__ import division, print_function
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Standard library
+import os, sys
+
+# Third-party
+import numpy as np
+from astropy import log as logger
+import astropy.units as u
+
+# Project
+from ..basepotential import Potential, _Potential
+
+# TODO: anything to test here?
