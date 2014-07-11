a409cc619b76c73fd59d9376b6bf3408eab2f08a
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-07-11 11:54:14 +0200

    skeleton for tests of rewinder

---
+++ streams/inference/tests/test_rewinder.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+""" Test the Rewinder model subclass """
+
+from __future__ import division, print_function
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Standard library
+import os, sys
+
+# Third-party
+import numpy as np
+from astropy import log as logger
+import astropy.units as u
+
+# Project
+from ..rewinder import Rewinder
+
+def test_config():
+    rw = Rewinder.from_config("/Users/adrian/projects/streams/config/test.yml")
