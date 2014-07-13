fa11a2fd34085283331bd042cfa33d0456e0e5fb
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-07-13 08:53:49 +0200

    reworking tests to be more robust

--- streams/inference/tests/test_rewinder.py
+++ streams/inference/tests/test_rewinder.py
@@ -11,9 +11,10 @@ import os, sys
 import logging
 
 # Third-party
-import numpy as np
 from astropy import log as logger
 import astropy.units as u
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Project
 from ..rewinder import Rewinder
@@ -25,5 +26,17 @@ def test_config():
     for p in rw._walk():
         print(p)
 
-    p0 = rw.sample_priors(24)[0]
-    print(rw(p0))
+    # p0 = rw.sample_priors(24)[0]
+    # print(rw(np.array([1.38, 1.6])))
+
+    vals = np.linspace(1., 1.5, 25)
+    ll = []
+    for val in vals:
+        try:
+            ll.append(rw(np.array([val,1.6])))
+        except ValueError:
+            ll.append(np.nan)
+
+    print(ll)
+    plt.plot(vals, ll)
+    plt.show()
