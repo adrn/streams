568ea78ee2235bc5ff1f4e0ecea9ac8c856b4a82
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-07-15 13:19:20 +0200

    typo

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ numpy_base_path = os.path.split(numpy.__file__)[0]
 numpy_incl_path = os.path.join(numpy_base_path, "core", "include")
 
 gal_hel = Extension("streams.coordinates.gal_hel",
-                    ["streams/coordinates/gal.pyx"],
+                    ["streams/coordinates/gal_hel.pyx"],
                      include_dirs=[numpy_incl_path])
 
 rewinder = Extension("streams.inference.rewinder_likelihood",
