35def74acc5dd922ecc411a169e022b4fdfb4974
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-07-15 13:17:45 +0200

    rename gal_hel

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ import os, numpy
 numpy_base_path = os.path.split(numpy.__file__)[0]
 numpy_incl_path = os.path.join(numpy_base_path, "core", "include")
 
-gc_hel = Extension("streams.coordinates._gc_hel",
-                   ["streams/coordinates/_gc_hel.pyx"],
-                    include_dirs=[numpy_incl_path])
+gal_hel = Extension("streams.coordinates.gal_hel",
+                    ["streams/coordinates/gal.pyx"],
+                     include_dirs=[numpy_incl_path])
 
 rewinder = Extension("streams.inference.rewinder_likelihood",
                      ["streams/inference/rewinder_likelihood.pyx"],
@@ -30,5 +30,5 @@ setup(
     author_email="adrn@astro.columbia.edu",
     license="BSD",
     cmdclass = {'build_ext': build_ext},
-    ext_modules=[gc_hel, rewinder, potential, lm10]
+    ext_modules=[gal_hel, rewinder, potential, lm10]
 )
