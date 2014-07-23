92ed656f8ca38efff260af1606c4c00aba2c2458
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-07-23 14:36:56 -0400

    add prior method back

--- streams/inference/starsprogenitor.py
+++ streams/inference/starsprogenitor.py
@@ -65,6 +65,22 @@ class Base(object):
     def __len__(self):
         return self.n
 
+    def ln_prior(self, data):
+        """ """
+
+        data = np.atleast_2d(data)
+        l,b,d,mul,mub,vr = data.T
+
+        ln_p_l = -np.log(2*np.pi)
+        ln_p_b = np.log(np.cos(b) / 2.)
+        # distance range 1-200 kpc
+        ln_p_d = np.log((1 / np.log(200./1.))) - np.log(d)
+        ln_p_mul = log_normal(mul, 0., 0.306814 / d) # 300 km/s
+        ln_p_mub = log_normal(mub, 0., 0.306814 / d) # 300 km/s
+        ln_p_vr = log_normal(vr, 0., 0.306814) # 300 km/s
+
+        return ln_p_l, ln_p_b, ln_p_d, ln_p_mul, ln_p_mub, ln_p_vr
+
 class Stars(Base):
     pass
 
