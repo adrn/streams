4ff8567ea82300873e62243a6d1d59be3410a07d
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-10-15 00:32:25 -0400

    call external c function

--- streams/inference/rewinder_likelihood.pyx
+++ streams/inference/rewinder_likelihood.pyx
@@ -23,6 +23,16 @@ cdef extern from "math.h":
     double exp(double x) nogil
     double pow(double x, double n) nogil
 
+cdef extern from "rewinder.c":
+    void set_basis(double*, double*, double*, double*, double*)
+    double test(double*, long)
+    void ln_likelihood_helper(double, double,
+                              double*, double*, long,
+                              double*, double*, double*,
+                              double*, double*,
+                              double, double*,
+                              double*)
+
 @cython.cdivision(True)
 @cython.wraparound(False)
 @cython.nonecheck(False)
@@ -75,102 +85,82 @@ cdef inline void leapfrog_step(double[:,::1] r, double[:,::1] v,
         v_12[i,1] = v_12[i,1] - dt*acc[i,1]
         v_12[i,2] = v_12[i,2] - dt*acc[i,2]
 
-@cython.cdivision(True)
-@cython.wraparound(False)
-@cython.nonecheck(False)
-cdef inline double dot(double[::1] a, double[::1] b) nogil:
-    return a[0]*b[0] + a[1]*b[1] + a[2]*b[2]
 
 @cython.cdivision(True)
 @cython.wraparound(False)
 @cython.nonecheck(False)
-cdef inline void ln_likelihood_helper(double sat_mass,
-                                      double[:,::1] x, double[:,::1] v, unsigned int nparticles,
-                                      double alpha, double[::1] betas,
-                                      pot._CPotential potential,
-                                      double[::1] x1_hat, double[::1] x2_hat,
-                                      double[::1] x3_hat, double theta,
-                                      double[:,::1] ln_likelihoods, unsigned int ll_idx,
-                                      double[::1] dx, double[::1] dv, double[::1] r_p_hat) nogil:
-    cdef unsigned int i
-    cdef double r_norm, v_norm, over_sigma_r_sq, over_sigma_v_sq, r_p_norm
-    cdef double r_term, v_term
-    cdef double x1,x2,x3
+cpdef _rewinder_likelihood():
+    cdef double [::1] x1 = np.empty(3)
+    cdef double [::1] x2 = np.empty(3)
+    cdef double [::1] x3 = np.empty(3)
+    cdef double [::1] dx = np.empty(3)
+    cdef double [::1] dv = np.empty(3)
 
-    cdef double jac, d, BB, Rsun = 8.
-    cdef double sat_R = sqrt(x[0,0]*x[0,0] + x[0,1]*x[0,1] + x[0,2]*x[0,2])
-    cdef double sat_V = sqrt(v[0,0]*v[0,0] + v[0,1]*v[0,1] + v[0,2]*v[0,2])
+    N = 16
+    cdef double [:,::1] x = np.vstack((np.array([1.,0,0]),np.ones((N,3))))
+    cdef double [:,::1] v = np.vstack((np.array([0.,1,0]),-1*np.ones((N,3))))
+    cdef double alpha = 1.
+    cdef double [::1] betas = np.ones(N)
 
-    cdef double r_tide = potential._tidal_radius(sat_mass, x[0,0], x[0,1], x[0,2])
-    cdef double v_disp = sat_V * r_tide / sat_R
+    cdef double [::1] ln_likelihoods = np.empty(N)
 
-    # normalizations and constants for Gaussians
-    r_norm = -log(r_tide) - 0.91893853320467267 # 0.5*log(2π)
-    over_sigma_r_sq = 1./(r_tide*r_tide)
-    v_norm = -log(v_disp) - 0.91893853320467267 # 0.5*log(2π)
-    over_sigma_v_sq = 1./(v_disp*v_disp)
+    # print(test(&x[0,0], 3))
 
-    # unit vector pointing at the position of the progenitor
-    r_p_norm = sqrt(x[0,0]*x[0,0] + x[0,1]*x[0,1] + x[0,2]*x[0,2])
-    r_p_hat[0] = x[0,0] / r_p_norm
-    r_p_hat[1] = x[0,1] / r_p_norm
-    r_p_hat[2] = x[0,2] / r_p_norm
+    # ln_likelihood_helper(1., 1.,
+    #                      &x[0,0], &v[0,0], N,
+    #                      &x1[0], &x2[0], &x3[0],
+    #                      &dx[0], &dv[0],
+    #                      alpha, &betas[0],
+    #                      &ln_likelihoods[0])
 
-    for i in range(nparticles):
-        # hijacking these vars to use for jacobian calc.
-        #   this is basically the transformation from GC cartesian to
-        #   heliocentric spherical, but just the parts I need for the
-        #   determinant of the jacobian
-        x1 = x[i+1,0] + Rsun
-        x2 = x[i+1,1]
-        x3 = x[i+1,2]
-        d = sqrt(x1*x1 + x2*x2 + x3*x3)
-        # BB = 1.5707963267948966 - acos(x3/d)
-        x2 = x3/d
-        BB = -x2 - x2*x2*x2/6. # TODO: BAD APPROXIMATION
-        jac = 4*log(d) + log(cos(BB))
+    # print(np.array(x1))
+    # print(np.array(x2))
+    # print(np.array(x3))
 
-        x1 = alpha*betas[i]*r_tide
+    import time
+    t1 = time.time()
+    for i in range(1000000):
+        ln_likelihood_helper(1., 1.,
+                             &x[0,0], &v[0,0], N,
+                             &x1[0], &x2[0], &x3[0],
+                             &dx[0], &dv[0],
+                             alpha, &betas[0],
+                             &ln_likelihoods[0])
+    print("{} µs per call".format((time.time()-t1)))
+    print("{} ms per 6000".format((time.time()-t1)/1e6*6000*1000))
 
-        # position likelihood is gaussian at lagrange points
-        dx[0] = x[i+1,0] - x[0,0] - x1*r_p_hat[0]
-        dx[1] = x[i+1,1] - x[0,1] - x1*r_p_hat[1]
-        dx[2] = x[i+1,2] - x[0,2] - x1*r_p_hat[2]
-        r_term = r_norm - 0.5*dot(dx,dx) * over_sigma_r_sq
+    print(np.array(x1))
+    print(np.array(x2))
+    print(np.array(x3))
 
-        # velocity likelihood is isotropic gaussian centered on prog.
-        dv[0] = v[i+1,0] - v[0,0]
-        dv[1] = v[i+1,1] - v[0,1]
-        dv[2] = v[i+1,2] - v[0,2]
-        v_term = v_norm - 0.5*dot(dv,dv) * over_sigma_v_sq
 
-        x1 = r_term + v_term + jac
-        ln_likelihoods[ll_idx,i] = x1
-
+@cython.cdivision(True)
+@cython.wraparound(False)
+@cython.nonecheck(False)
 cpdef rewinder_likelihood(double t1, double t2, double dt,
                           pot._CPotential potential,
-                          np.ndarray[double,ndim=2] prog_gal,
-                          np.ndarray[double,ndim=2] star_gal,
+                          np.ndarray[double,ndim=2] prog_xv,
+                          np.ndarray[double,ndim=2] star_xv,
                           double m0, double mdot,
                           double alpha, np.ndarray[double,ndim=1] _betas,
                           double theta):
-    """ 0th entry of x,v is the satellite position, velocity """
 
     cdef unsigned int i, nsteps, nparticles, ndim
-    nparticles = len(star_gal)
+    nparticles = len(star_xv)
     nsteps = int(fabs((t1-t2)/dt))
 
-    cdef double [:,::1] ln_likelihoods = np.zeros((nsteps, nparticles))
-    cdef double [::1] dx = np.zeros(3)
-    cdef double [::1] dv = np.zeros(3)
-    cdef double [::1] r_p_hat = np.zeros(3)
+    # Containers
+    cdef double [::1] x1 = np.empty(3)
+    cdef double [::1] x2 = np.empty(3)
+    cdef double [::1] x3 = np.empty(3)
+    cdef double [::1] dx = np.empty(3)
+    cdef double [::1] dv = np.empty(3)
+
+    cdef double [::1] ln_likelihood_tmp = np.zeros(nparticles)
     cdef double [:,::1] v_12 = np.zeros((nparticles+1,3))
 
-    cdef double [::1] x1_hat = np.empty(3)
-    cdef double [::1] x2_hat = np.empty(3)
-    cdef double [::1] x3_hat = np.empty(3)
-    cdef double [:,::1] x = np.vstack((prog_gal[:,:3],star_gal[:,:3]))
-    cdef double [:,::1] v = np.vstack((prog_gal[:,3:],star_gal[:,3:]))
+    cdef double [:,::1] x = np.vstack((prog_xv[:,:3],star_xv[:,:3]))
+    cdef double [:,::1] v = np.vstack((prog_xv[:,3:],star_xv[:,3:]))
     cdef double [:,::1] acc = np.empty((nparticles+1,3))
     cdef double [::1] betas = _betas
 
@@ -189,12 +179,20 @@ cpdef rewinder_likelihood(double t1, double t2, double dt,
     # all_x[0] = x
     # all_v[0] = v
     # --
-    ln_likelihood_helper(mass, x, v, nparticles,
-                         alpha, betas, potential,
-                         x1_hat, x2_hat, x3_hat, theta,
-                         ln_likelihoods, 0, dx, dv, r_p_hat)
+
+    # TODO: hack
+    rtide = 1.
+    vdisp = 1.
+
+    ln_likelihood_helper(rtide, vdisp,
+                         &x[0,0], &v[0,0], nparticles,
+                         &x1[0], &x2[0], &x3[0],
+                         &dx[0], &dv[0],
+                         alpha, &betas[0],
+                         &ln_likelihood_tmp[0])
 
     for i in range(1,nsteps):
+        # HACK
         leapfrog_step(x, v, v_12, acc, nparticles+1, dt, potential)
 
         # -- TURN THESE ON FOR DEBUGGING --
@@ -206,15 +204,30 @@ cpdef rewinder_likelihood(double t1, double t2, double dt,
         # mass of the satellite
         mass = -mdot*t1 + m0
 
-        ln_likelihood_helper(mass, x, v, nparticles,
-                             alpha, betas, potential,
-                             x1_hat, x2_hat, x3_hat, theta,
-                             ln_likelihoods, i, dx, dv, r_p_hat)
+        ln_likelihood_helper(rtide, vdisp,
+                             &x[0,0], &v[0,0], nparticles,
+                             &x1[0], &x2[0], &x3[0],
+                             &dx[0], &dv[0],
+                             alpha, &betas[0],
+                             &ln_likelihood_tmp[0])
 
-    return np.array(ln_likelihoods)
+    #return np.array(ln_likelihoods)
     # -- TURN THESE ON FOR DEBUGGING --
     # return np.array(ln_likelihoods), np.array(all_x), np.array(all_v)
     # --
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
