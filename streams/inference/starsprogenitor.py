fa2548c8b09a6bef2234b55faae80bf460a10eff
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-10-18 22:57:34 -0400

    rehashing dumb classes

--- streams/inference/starsprogenitor.py
+++ streams/inference/starsprogenitor.py
@@ -18,70 +18,48 @@ from .. import heliocentric_names
 from streamteam.inference import ModelParameter
 from .util import log_normal
 
-__all__ = ['ObservedParameter', 'Stars', 'Progenitor']
+__all__ = ['Stars', 'Progenitor']
 
-class ObservedParameter(ModelParameter):
+class StreamComponentBase(object):
 
-    def __init__(self, name, value, error, truth=None, prior=None):
-        """ Represents an observed quantity, e.g., distance. """
-        self.value = value
-        self.error = error
-        super(ObservedParameter,self).__init__(name, truth, prior, value.shape)
-
-    def ln_likelihood(self, value):
-        """ """
-        return log_normal(value, self.value, self.error)
-
-    def __str__(self):
-        return "<ObservedParameter '{}'>".format(self.name)
-
-class Base(object):
-
-    def __init__(self, data, errors, truths=None):
+    def __init__(self, data, err=None, **kwargs):
         """ """
 
-        ps = OrderedDict()
+        self.data = data
+        self.err = err
+        self.parameters = kwargs
 
-        # TODO: still need to be able to define mappings for parameters...
-        for name in heliocentric_names:
-            try:
-                truth = np.asarray(truths[name])
-            except:
-                truth = None
-            ps[name] = ObservedParameter(name, np.asarray(data[name]),
-                                         np.asarray(errors[name]), truth=truth)
-        self.parameters = ps
+    # def ln_prior(self, data):
+    #     """ """
 
-        self.data = np.vstack([ps[n].value for n in heliocentric_names]).T.copy()
-        self.errors = np.vstack([ps[n].error for n in heliocentric_names]).T.copy()
-        if truth is None:
-            self.truths = None
+    #     data = np.atleast_2d(data)
+    #     l,b,d,mul,mub,vr = data.T
+
+    #     ln_p_l = -np.log(2*np.pi)  # isotropic
+    #     ln_p_b = np.log(np.cos(b) / 2.)  # isotropic
+
+    #     # distance range 1-200 kpc
+    #     ln_p_d = np.log((1 / np.log(200./1.))) - np.log(d)
+    #     ln_p_mul = log_normal(mul, 0., 0.306814 / d)  # 300 km/s at d
+    #     ln_p_mub = log_normal(mub, 0., 0.306814 / d)  # 300 km/s at d
+    #     ln_p_vr = log_normal(vr, 0., 0.306814)  # 300 km/s
+
+    #     return ln_p_l, ln_p_b, ln_p_d, ln_p_mul, ln_p_mub, ln_p_vr
+
+class Stars(StreamComponentBase):
+
+    def __init__(self, data, err=None, tail=None):
+        """ TODO: """
+
+        if isinstance(tail, ModelParameter):
+
+        elif tail is None:
+
         else:
-            self.truths = np.vstack([ps[n].truth for n in heliocentric_names]).T.copy()
-        self.n = len(self.data)
 
-    def __len__(self):
-        return self.n
 
-    def ln_prior(self, data):
-        """ """
 
-        data = np.atleast_2d(data)
-        l,b,d,mul,mub,vr = data.T
+class Progenitor(StreamComponentBase):
 
-        ln_p_l = -np.log(2*np.pi)  # isotropic
-        ln_p_b = np.log(np.cos(b) / 2.)  # isotropic
-
-        # distance range 1-200 kpc
-        ln_p_d = np.log((1 / np.log(200./1.))) - np.log(d)
-        ln_p_mul = log_normal(mul, 0., 0.306814 / d)  # 300 km/s at d
-        ln_p_mub = log_normal(mub, 0., 0.306814 / d)  # 300 km/s at d
-        ln_p_vr = log_normal(vr, 0., 0.306814)  # 300 km/s
-
-        return ln_p_l, ln_p_b, ln_p_d, ln_p_mul, ln_p_mub, ln_p_vr
-
-class Stars(Base):
-    pass
-
-class Progenitor(Base):
-    pass
+    def __init__(self, data, err=None, m0=None, mdot=0.):
+        pass
