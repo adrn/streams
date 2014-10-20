afd5ec9dbdd13d2f171058efaf55fdf02a73f967
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-10-20 10:04:05 -0400

    use scipy.stats

--- streams/inference/streamcomponent.py
+++ streams/inference/streamcomponent.py
@@ -6,21 +6,14 @@ from __future__ import division, print_function
 
 __author__ = "adrn <adrn@astro.columbia.edu>"
 
-# Standard library
-from collections import OrderedDict
-
 # Third-party
 import numpy as np
 from astropy import log as logger
+from scipy.stats import norm
 
-# Project
-from .. import heliocentric_names
-from streamteam.inference import ModelParameter
-from .util import log_normal
+__all__ = ['StreamComponent']
 
-__all__ = ['Stars', 'Progenitor']
-
-class StreamComponentBase(object):
+class StreamComponent(object):
 
     def __init__(self, data, err=None, **kwargs):
         """ """
@@ -28,38 +21,24 @@ class StreamComponentBase(object):
         self.data = data
         self.err = err
         self.parameters = kwargs
+        self._dist = norm(self.data, self.err)
 
-    # def ln_prior(self, data):
-    #     """ """
+    def ln_data_prob(self, x):
+        """ Compute the (log-)probability of phase-space positions (in heliocentric
+            observed coordinates), given the data and uncertainties.
 
-    #     data = np.atleast_2d(data)
-    #     l,b,d,mul,mub,vr = data.T
+        """
+        return self._dist.logpdf(x).sum(axis=0)
 
-    #     ln_p_l = -np.log(2*np.pi)  # isotropic
-    #     ln_p_b = np.log(np.cos(b) / 2.)  # isotropic
+        # l,b,d,mul,mub,vr = data.T
 
-    #     # distance range 1-200 kpc
-    #     ln_p_d = np.log((1 / np.log(200./1.))) - np.log(d)
-    #     ln_p_mul = log_normal(mul, 0., 0.306814 / d)  # 300 km/s at d
-    #     ln_p_mub = log_normal(mub, 0., 0.306814 / d)  # 300 km/s at d
-    #     ln_p_vr = log_normal(vr, 0., 0.306814)  # 300 km/s
+        # ln_p_l = -np.log(2*np.pi)  # isotropic
+        # ln_p_b = np.log(np.cos(b) / 2.)  # isotropic
 
-    #     return ln_p_l, ln_p_b, ln_p_d, ln_p_mul, ln_p_mub, ln_p_vr
+        # # distance range 1-200 kpc
+        # ln_p_d = np.log((1 / np.log(200./1.))) - np.log(d)
+        # ln_p_mul = log_normal(mul, 0., 0.306814 / d)  # 300 km/s at d
+        # ln_p_mub = log_normal(mub, 0., 0.306814 / d)  # 300 km/s at d
+        # ln_p_vr = log_normal(vr, 0., 0.306814)  # 300 km/s
 
-class Stars(StreamComponentBase):
-
-    def __init__(self, data, err=None, tail=None):
-        """ TODO: """
-
-        if isinstance(tail, ModelParameter):
-
-        elif tail is None:
-
-        else:
-
-
-
-class Progenitor(StreamComponentBase):
-
-    def __init__(self, data, err=None, m0=None, mdot=0.):
-        pass
+        # return ln_p_l, ln_p_b, ln_p_d, ln_p_mul, ln_p_mub, ln_p_vr
