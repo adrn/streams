f5f40a9297589008608cd3653045c7b7da43008a
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-05-31 14:45:41 -0400

    add command line options

--- scripts/serious_hack_for_michigan.py
+++ scripts/serious_hack_for_michigan.py
@@ -34,21 +34,28 @@ true_params = dict(v_c=(200*u.km/u.s).decompose(galactic).value, r_s=20.,
 true_potential = gp.LeeSutoTriaxialNFWPotential(units=galactic, **true_params)
 
 def ln_prior_nfw(p):
-    alpha, v_c = p
+    alpha, v_c, log_r_s = p
+
+    lnp = 0.
 
     if alpha < 0.2 or alpha > 3.:
+        return -np.inf
+    lnp += -((alpha - 1.) / 0.5)**2
+
+    if log_r_s < 0.7 or log_r_s > 4.5:
         return -np.inf
 
     if v_c < 0.1 or v_c > 0.3: # 100 - 300 km/s
         return -np.inf
 
-    return 0.
+    return lnp
 
 def ln_likelihood_nfw(p, dt, nsteps, prog_w, star_w, betas, sat_mass):
-    alpha, v_c = p
+    alpha, v_c, log_r_s = p
 
     params = true_params.copy()
     params['v_c'] = v_c
+    params['r_s'] = np.exp(log_r_s)
     pot = gp.LeeSutoTriaxialNFWPotential(units=galactic, **params)
 
     nstars = star_w.shape[0]
@@ -70,13 +77,13 @@ def ln_posterior_nfw(p, *args):
 
 # ----------------------------------------------------------------------
 def ln_prior_hernq(p):
-    log_sat_mass, alpha, log_m, c = p
+    alpha, log_m, c = p
 
-    if log_sat_mass < 10. or log_sat_mass > 14.7: # 2.5E4 - 2.5E6
-        return -np.inf
+    lnp = 0.
 
     if alpha < 0.2 or alpha > 3.:
         return -np.inf
+    lnp += -((alpha - 1.) / 0.5)**2
 
     if log_m < 23. or log_m > 27.6:
         return -np.inf
@@ -84,10 +91,10 @@ def ln_prior_hernq(p):
     if c < 0.1 or c > 100.:
         return -np.inf
 
-    return 0.
+    return lnp
 
-def ln_likelihood_hernq(p, dt, nsteps, prog_w, star_w, betas):
-    log_sat_mass, alpha, log_m, c = p
+def ln_likelihood_hernq(p, dt, nsteps, prog_w, star_w, betas, sat_mass):
+    alpha, log_m, c = p
 
     params = dict()
     params['m'] = np.exp(log_m)
@@ -97,29 +104,29 @@ def ln_likelihood_hernq(p, dt, nsteps, prog_w, star_w, betas):
     nstars = star_w.shape[0]
     ll = np.zeros((nsteps,nstars), dtype=float) - 9999.
     rewinder_likelihood(ll, dt, nsteps, pot.c_instance, pot.G, prog_w[None].copy(), star_w,
-                        np.exp(log_sat_mass), 0., alpha, betas, 0., True)
+                        sat_mass, 0., alpha, betas, 0., True)
 
     return integrate_tub(ll, dt).sum()
 
-def ln_posterior_hernq(p, dt, nsteps, prog_w, star_w, betas):
+def ln_posterior_hernq(p, *args):
     ln_pri = ln_prior_hernq(p)
     if np.any(~np.isfinite(ln_pri)):
         return -np.inf
 
-    ll = ln_likelihood_hernq(p, dt, nsteps, prog_w, star_w, betas)
+    ll = ln_likelihood_hernq(p, *args)
     if np.any(~np.isfinite(ll)):
         return -np.inf
     return -(ln_pri + ll)
 
 # ----------------------------------------------------------------------
 def ln_prior_bfe(p):
-    log_sat_mass, alpha, log_m, c, c1, c2, c3, c4 = p
+    alpha, log_m, c, c1, c2, c3, c4 = p
 
-    if log_sat_mass < 10. or log_sat_mass > 14.7: # 2.5E4 - 2.5E6
-        return -np.inf
+    lnp = 0.
 
     if alpha < 0.2 or alpha > 3.:
         return -np.inf
+    lnp += -((alpha - 1.) / 0.5)**2
 
     if log_m < 23. or log_m > 27.6:
         return -np.inf
@@ -127,10 +134,10 @@ def ln_prior_bfe(p):
     if c < 0.1 or c > 100.:
         return -np.inf
 
-    return 0.
+    return lnp
 
-def ln_likelihood_bfe(p, dt, nsteps, prog_w, star_w, betas):
-    log_sat_mass, alpha, log_m, c, c1, c2, c3, c4 = p
+def ln_likelihood_bfe(p, dt, nsteps, prog_w, star_w, betas, sat_mass):
+    alpha, log_m, c, c1, c2, c3, c4 = p
 
     params = dict()
     params['m'] = np.exp(log_m)
@@ -142,30 +149,48 @@ def ln_likelihood_bfe(p, dt, nsteps, prog_w, star_w, betas):
     nstars = star_w.shape[0]
     ll = np.zeros((nsteps,nstars), dtype=float) - 9999.
     rewinder_likelihood(ll, dt, nsteps, pot.c_instance, pot.G, prog_w[None].copy(), star_w,
-                        np.exp(log_sat_mass), 0., alpha, betas, 0., True)
+                        sat_mass, 0., alpha, betas, 0., True)
 
     return integrate_tub(ll, dt).sum()
 
-def ln_posterior_bfe(p, dt, nsteps, prog_w, star_w, betas):
+def ln_posterior_bfe(p, *args):
     ln_pri = ln_prior_bfe(p)
     if np.any(~np.isfinite(ln_pri)):
         return -np.inf
 
-    ll = ln_likelihood_bfe(p, dt, nsteps, prog_w, star_w, betas)
+    ll = ln_likelihood_bfe(p, *args)
     if np.any(~np.isfinite(ll)):
         return -np.inf
     return -(ln_pri + ll)
 
 # =====================================================================================
 
-def main():
-    pool = get_pool(mpi=False)
+def sample_dat_ish(sampler, p0, nburn=128, nwalk=1024):
+    pos,_,_ = sampler.run_mcmc(p0, nburn)
+    print("Done burning in.")
+
+    # find MAP sample, re-initialize a small ball around here
+    pvec = sampler.flatchain[sampler.flatlnprobability.argmax()]
+    pos = np.random.normal(pvec, pvec*0.01, size=p0.shape)
+
+    sampler.reset()
+    pos,_,_ = sampler.run_mcmc(pos, nburn)
+    print("Second stage burn complete.")
+
+    sampler.reset()
+    pos,_,_ = sampler.run_mcmc(pos, nwalk)
+    print("Done final walking.")
+
+    return sampler
+
+def main(name, nstars, nburn, nwalk, mpi=False, save=False):
+    pool = get_pool(mpi=mpi)
 
     np.random.seed(42)
 
     # parameters
     true_sat_mass = 2.5E5
-    nstars = 16
+    nstars = 8
     dt = -0.1
 
     # read in the SCF simulation data
@@ -189,79 +214,108 @@ def main():
     dE = (true_potential.total_energy(data_w[:,:3], data_w[:,3:]) - prog_E) / prog_E
     betas = -2*(dE > 0).astype(int) + 1.
 
+    # test true potential
+    # vals = np.linspace(0.195, 0.215, 32)
+    # lls = []
+    # for val in vals:
+    #     p = [1., val, np.log(20.)]
+    #     ll = ln_likelihood_nfw(p, dt, nsteps, prog_w, data_w, betas, true_sat_mass)
+    #     lls.append(ll)
+
+    # plt.figure()
+    # plt.plot(vals, lls)
+    # plt.show()
+
     # ----------------------------
     # Emcee
-    nburn = 2
-    nwalk = 2
 
-    # NFW
-    print("Firing up sampler for NFW")
-    ndim = 2
-    nwalkers = 8*ndim
-    sampler = emcee.EnsembleSampler(nwalkers=nwalkers, dim=ndim, lnpostfn=ln_posterior_nfw,
-                                    args=(dt, nsteps, prog_w, data_w, betas, true_sat_mass),
-                                    pool=pool)
+    if name == 'nfw':
+        # NFW
+        print("Firing up sampler for NFW")
+        ndim = 3
+        nwalkers = 8*ndim
+        sampler = emcee.EnsembleSampler(nwalkers=nwalkers, dim=ndim, lnpostfn=ln_posterior_nfw,
+                                        args=(dt, nsteps, prog_w, data_w, betas, true_sat_mass),
+                                        pool=pool)
 
-    p0 = np.zeros((nwalkers,ndim))
-    p0[:,0] = np.random.normal(1., 0.25, size=nwalkers) # alpha
-    p0[:,1] = np.random.normal(0.2, 0.02, size=nwalkers) # v_c
+        p0 = np.zeros((nwalkers,ndim))
+        p0[:,0] = np.random.normal(1., 0.25, size=nwalkers) # alpha
+        p0[:,1] = np.random.normal(0.2, 0.02, size=nwalkers) # v_c
+        p0[:,2] = np.random.normal(3., 0.2, size=nwalkers) # log_r_s
 
-    pos,_,_ = sampler.run_mcmc(p0, nburn)
-    print("Done burning in.")
-    sampler.reset()
-    pos,_,_ = sampler.run_mcmc(pos, nwalk)
-    print("Done sampling.")
-    # np.save("/vega/astro/users/amp2217/projects/streams/output/michigan_hack/nfw_chain.npy", sampler.chain)
-    # np.save("/vega/astro/users/amp2217/projects/streams/output/michigan_hack/nfw_lnprob.npy", sampler.lnprobability)
+        sampler = sample_dat_ish(sampler, p0, nburn=nburn, nwalk=nwalk)
+        pool.close()
 
-    # Hernquist
-    # print("Firing up sampler for Hernquist")
-    # ndim = 4
-    # nwalkers = 8*ndim
-    # sampler = emcee.EnsembleSampler(nwalkers=nwalkers, dim=ndim, lnpostfn=ln_posterior_hernq,
-    #                                 args=(dt, nsteps, prog_w, data_w, betas), pool=pool)
+        if save:
+            np.save("/vega/astro/users/amp2217/projects/streams/output/michigan_hack/nfw_chain.npy", sampler.chain)
+            np.save("/vega/astro/users/amp2217/projects/streams/output/michigan_hack/nfw_lnprob.npy", sampler.lnprobability)
 
-    # p0 = np.zeros((nwalkers,ndim))
-    # p0[:,0] = np.random.normal(true_log_sat_mass, 0.5, size=nwalkers) # log_sat_mass
-    # p0[:,1] = np.random.normal(1., 0.25, size=nwalkers) # alpha
-    # p0[:,2] = np.random.normal(26., 0.15, size=nwalkers) # log_m
-    # p0[:,3] = np.random.normal(20., 2., size=nwalkers) # c
+    elif name == "hernq":
+        # Hernquist
+        print("Firing up sampler for Hernquist")
+        ndim = 3
+        nwalkers = 8*ndim
+        sampler = emcee.EnsembleSampler(nwalkers=nwalkers, dim=ndim, lnpostfn=ln_posterior_hernq,
+                                        args=(dt, nsteps, prog_w, data_w, betas), pool=pool)
 
-    # pos,_,_ = sampler.run_mcmc(p0, nburn)
-    # print("Done burning in.")
-    # sampler.reset()
-    # pos,_,_ = sampler.run_mcmc(pos, nwalk)
-    # print("Done sampling.")
-    # np.save("/vega/astro/users/amp2217/projects/streams/output/michigan_hack/hernq_chain.npy", sampler.chain)
-    # np.save("/vega/astro/users/amp2217/projects/streams/output/michigan_hack/hernq_lnprob.npy", sampler.lnprobability)
+        p0 = np.zeros((nwalkers,ndim))
+        p0[:,0] = np.random.normal(1., 0.25, size=nwalkers) # alpha
+        p0[:,1] = np.random.normal(26., 0.15, size=nwalkers) # log_m
+        p0[:,2] = np.random.normal(20., 2., size=nwalkers) # c
 
-    # BFE
-    # print("Firing up sampler for BFE")
-    # ndim = 8
-    # nwalkers = 8*ndim
-    # sampler = emcee.EnsembleSampler(nwalkers=nwalkers, dim=ndim, lnpostfn=ln_posterior_bfe,
-    #                                 args=(dt, nsteps, prog_w, data_w, betas), pool=pool)
+        sampler = sample_dat_ish(sampler, p0, nburn=nburn, nwalk=nwalk)
+        pool.close()
 
-    # p0 = np.zeros((nwalkers,ndim))
-    # p0[:,0] = np.random.normal(true_log_sat_mass, 0.5, size=nwalkers) # log_sat_mass
-    # p0[:,1] = np.random.normal(1., 0.25, size=nwalkers) # alpha
-    # p0[:,2] = np.random.normal(26., 0.15, size=nwalkers) # log_m
-    # p0[:,3] = np.random.normal(20., 2., size=nwalkers) # c
-    # p0[:,4] = np.random.uniform(-0.9, -1.1, size=nwalkers) # c1
-    # p0[:,5] = np.random.uniform(-0., -0.1, size=nwalkers) # c2
-    # p0[:,6] = np.random.uniform(-0., -0.01, size=nwalkers) # c3
-    # p0[:,7] = np.random.uniform(-0., -0.01, size=nwalkers) # c4
+        if save:
+            np.save("/vega/astro/users/amp2217/projects/streams/output/michigan_hack/hernq_chain.npy", sampler.chain)
+            np.save("/vega/astro/users/amp2217/projects/streams/output/michigan_hack/hernq_lnprob.npy", sampler.lnprobability)
 
-    # pos,_,_ = sampler.run_mcmc(p0, nburn)
-    # print("Done burning in.")
-    # sampler.reset()
-    # pos,_,_ = sampler.run_mcmc(pos, nwalk)
-    # print("Done sampling.")
-    # #np.save("/vega/astro/users/amp2217/projects/streams/output/michigan_hack/bfe_chain.npy", sampler.chain)
-    # #np.save("/vega/astro/users/amp2217/projects/streams/output/michigan_hack/bfe_lnprob.npy", sampler.lnprobability)
+    elif name == "bfe":
+        # BFE
+        print("Firing up sampler for BFE")
+        ndim = 7
+        nwalkers = 8*ndim
+        sampler = emcee.EnsembleSampler(nwalkers=nwalkers, dim=ndim, lnpostfn=ln_posterior_bfe,
+                                        args=(dt, nsteps, prog_w, data_w, betas), pool=pool)
 
-    # pool.close()
-    # sys.exit(0)
+        p0 = np.zeros((nwalkers,ndim))
+        p0[:,0] = np.random.normal(1., 0.25, size=nwalkers) # alpha
+        p0[:,1] = np.random.normal(26., 0.15, size=nwalkers) # log_m
+        p0[:,2] = np.random.normal(20., 2., size=nwalkers) # c
+        p0[:,3] = np.random.uniform(-0.9, -1.1, size=nwalkers) # c1
+        p0[:,4] = np.random.uniform(-0., -0.1, size=nwalkers) # c2
+        p0[:,5] = np.random.uniform(-0., -0.01, size=nwalkers) # c3
+        p0[:,6] = np.random.uniform(-0., -0.01, size=nwalkers) # c4
+
+        sampler = sample_dat_ish(sampler, p0, nburn=nburn, nwalk=nwalk)
+        pool.close()
+
+        if save:
+            np.save("/vega/astro/users/amp2217/projects/streams/output/michigan_hack/bfe_chain.npy", sampler.chain)
+            np.save("/vega/astro/users/amp2217/projects/streams/output/michigan_hack/bfe_lnprob.npy", sampler.lnprobability)
+    sys.exit(0)
 
 if __name__ == "__main__":
-    main()
+    from argparse import ArgumentParser
+
+    # Define parser object
+    parser = ArgumentParser(description="")
+
+    # name, nstars, nburn, nwalk, mpi=False, save=False
+    parser.add_argument("--name", dest="name", required=True,
+                        type=str, help="Potential code name (nfw, bfe, hernq).")
+    parser.add_argument("--nstars", dest="nstars", required=True,
+                        type=int, help="Number of stars to use.")
+    parser.add_argument("--nburn", dest="nburn", default=128,
+                        type=int, help="Number of steps to burn in.")
+    parser.add_argument("--nwalk", dest="nwalk", default=1024,
+                        type=int, help="Number of steps to walk post-burn-in.")
+    parser.add_argument("--mpi", dest="mpi", action="store_true", default=False,
+                        help="Use MPI.")
+    parser.add_argument("--save", dest="save", action="store_true", default=False,
+                        help="Save chains.")
+
+    args = parser.parse_args()
+
+    main(name=args.name, nstars=args.nstars, nburn=args.nburn, nwalk=args.nwalk,
+         save=args.save, mpi=args.mpi)
